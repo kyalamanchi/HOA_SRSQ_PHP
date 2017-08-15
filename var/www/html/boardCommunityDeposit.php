31b48f513a72a7f8fd84ce1b4e3246187d5ce75c
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-15 23:15:30 +0530

    Deposit

--- var/www/html/boardCommunityDeposit.php
+++ var/www/html/boardCommunityDeposit.php
@@ -458,7 +458,7 @@
 
                             </div>";
 
-                          echo "<tr><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>".$id1."</td><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>$ ".$net_amount."</a></td><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>".$number_of_transactions."</a></td><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>".$status."</a></td><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>".$effective_date."</a></td><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>".$origination_date."</a></td></tr>";
+                          echo "<tr><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>".$id1."</td><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>$ ".$net_amount."</a></td><td><a data-toggle='modal' data-target='#funding_id_".$funding_id."'>".$number_of_transactions."</a></td><td>".$status."</td><td>".$effective_date."</td><td>".$origination_date."</td></tr>";
 
                         }
 
