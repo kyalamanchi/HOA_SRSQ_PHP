8ddfd5e91afe66be7579e1ffd2a5bbd18266130a
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-15 22:04:42 +0530

    Deposit

--- var/www/html/boardCommunityDeposit.php
+++ var/www/html/boardCommunityDeposit.php
@@ -411,10 +411,14 @@
 
                                       <div class='row'>
 
-                                        <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Transactions ID</div>
-                                        <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Status</div>
-                                        <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Amount</div>
-                                        <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Received Date</div>
+                                        <strong>
+
+                                          <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Transactions ID</div>
+                                          <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Status</div>
+                                          <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Amount</div>
+                                          <div class='col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'>Received Date</div>
+
+                                        </strong>
 
                                       </div>
 
