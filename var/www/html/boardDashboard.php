c605110cbbed5a2d343a7e3dff37680f74d9887e
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-18 11:21:26 +0530

    Tiles

--- var/www/html/boardDashboard.php
+++ var/www/html/boardDashboard.php
@@ -667,61 +667,6 @@
 
               <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-xs-6">
                 
-                <a href='https://hoaboardtime.com/boardCurrentMonthPendingPayments.php'>
-
-                  <div style="background:#ffffff;">
-                
-                    <div class="box-header">
-                  
-                      <div class='row container-fluid'><i class="fa fa-hourglass-2 fa-4x pull-left text-aqua"></i>
-                  
-                        <b class="pull-right">
-                          
-                          <?php 
-                        
-                            $pending = 0;
-
-                            $result = pg_query("SELECT * FROM hoaid WHERE hoa_id NOT IN (SELECT hoa_id FROM current_payments WHERE community_id=".$community_id." AND payment_status_id=1 AND process_date>='$year-$month-01' AND process_date<='$year-$month-$end_date') AND community_id=$community_id");
-
-                            while($row = pg_fetch_assoc($result))
-                            {
-                              $r2 = pg_query("SELECT sum(amount) FROM current_charges WHERE hoa_id=".$row['hoa_id']." GROUP BY hoa_id");
-                              $r = pg_fetch_assoc($r2);
-                              $cha = $r['sum'];
-
-                              $r2 = pg_query("SELECT sum(amount) FROM current_payments WHERE payment_status_id=1 AND hoa_id=".$row['hoa_id']." GROUP BY hoa_id");
-                              $r = pg_fetch_assoc($r2);
-                              $pay = $r['sum'];
-
-                              if($cha - $pay > 0)
-                                      $pending++;
-                            }
-
-                            if($pending == 0 || $pending == "")
-                              echo "<h4 class='text-success'><strong>0</strong></h4>";
-                            else
-                              echo "<h4 class='text-orange'><strong>".$pending."</strong></h4>";
-
-                          ?>
-
-                        </b>
-
-                      </div>
-                      
-                      <div class='row container-fluid text-center'><br>Payments Pending</div>
-                
-                    </div>
-
-                  </div>
-
-                </a>
-
-                <br>
-
-              </div>
-
-              <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-xs-6">
-                
                 <?php if(date('d') >= 16) echo "<a href='https://hoaboardtime.com/boardCurrentMonthLatePayments.php'>"; ?>
 
                   <div style="background:#ffffff;">
@@ -1011,6 +956,71 @@
                   <div class="row container-fluid text-left">
 
                     <h4><strong>Pending Agreemnts</strong></h4>
+
+                  </div>
+
+                  <br>
+
+                </div>
+
+              </a>
+
+            </div>
+
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-6">
+
+              <a href='https://hoaboardtime.com/boardCurrentMonthPendingPayments.php'>
+
+                <div class="row container-fluid text-left">
+
+                  <br>
+
+                  <div class="row container-fluid">
+
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
+
+                      <img src="pending_payments.png" height=75 width=75 alt='Pending Agreements'>
+
+                    </div>
+
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
+
+                      <?php 
+
+                        $pending = 0;
+
+                        $result = pg_query("SELECT * FROM hoaid WHERE hoa_id NOT IN (SELECT hoa_id FROM current_payments WHERE community_id=".$community_id." AND payment_status_id=1 AND process_date>='$year-$month-01' AND process_date<='$year-$month-$end_date') AND community_id=$community_id");
+
+                        while($row = pg_fetch_assoc($result))
+                        {
+                          
+                          $r2 = pg_query("SELECT sum(amount) FROM current_charges WHERE hoa_id=".$row['hoa_id']." GROUP BY hoa_id");
+                          $r = pg_fetch_assoc($r2);
+                          $cha = $r['sum'];
+
+                          $r2 = pg_query("SELECT sum(amount) FROM current_payments WHERE payment_status_id=1 AND hoa_id=".$row['hoa_id']." GROUP BY hoa_id");
+                          $r = pg_fetch_assoc($r2);
+                          $pay = $r['sum'];
+
+                          if($cha - $pay > 0)
+                            $pending++;
+
+                        }
+
+                        if($pending > 0)
+                          echo "<h2 class='text-orange'><strong>".$pending."</strong></h2>"; 
+                        else
+                          echo "<h2 class='text-green'><strong>".$pending."</strong></h2>";
+
+                      ?>
+
+                    </div>
+
+                  </div>
+
+                  <div class="row container-fluid text-left">
+
+                    <h4><strong>Pending Payments</strong></h4>
 
                   </div>
 
