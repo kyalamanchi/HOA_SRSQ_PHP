1ae8b1b49df8b8bb96a75d49795240b466c5a523
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-18 17:33:20 +0530

    Dashboard

--- var/www/html/boardDashboard.php
+++ var/www/html/boardDashboard.php
@@ -993,7 +993,7 @@
 
             <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-6">
 
-              <a href='https://hoaboardtime.com/boardCurrentMonthPendingPayments.php'>
+              <a href='https://hoaboardtime.com/boardCurrentMonthPrePaidMembers.php'>
 
                 <div class="row container-fluid text-left">
 
