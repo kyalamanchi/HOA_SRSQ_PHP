721df4ade89a78c28f64d829434ade4b86e27960
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-18 11:22:21 +0530

    Tile

--- var/www/html/boardDashboard.php
+++ var/www/html/boardDashboard.php
@@ -979,7 +979,7 @@
 
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
 
-                      <img src="pending_payments.png" height=75 width=75 alt='Pending Agreements'>
+                      <!--img src="pending_payments.png" height=75 width=75 alt='Pending Agreements'-->
 
                     </div>
 
