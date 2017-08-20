f527e5df0d1c55e0eea1d50b4aba18d40b3092aa
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-21 00:41:06 +0530

    testing

--- var/www/html/testing.php
+++ var/www/html/testing.php
@@ -4,7 +4,16 @@
     require("fpdf/fpdf.php");
 
 
+    header ( "Content-Type: application/vnd.x-pdf" );
+    header ( "Content-disposition: attachment; filename=Testing_PDF-_".date('m-d-Y H:i:s').".pdf" );
+    header ( "Content-Type: application/force-download" );
+    header ( "Content-Transfer-Encoding: binary" );
+    header ( "Pragma: no-cache" );
+    header ( "Expires: 0" );
+
+
     $pdf = new FPDF();
+    $pdf->AddPage();
 
 
     $pdf->SetFont("Arial", "", 12);
@@ -27,9 +36,9 @@
     $pdf->Cell(85, 6, "Due Date : 08-15-17", 0, 1, R);
 
 
-    $pdf->Cell(189, 6, " ", 0, 1);
-    $pdf->Cell(189, 6, " ", 0, 1);
-    $pdf->Cell(189, 6, " ", 0, 1);
+    $pdf->Cell(85, 6, " ", 0, 1, R);
+    $pdf->Cell(85, 6, " ", 0, 1, R);
+    $pdf->Cell(85, 6, " ", 0, 1, R);
 
 
     $pdf->Cell(100, 6, "To :", 0, 1, L);
@@ -42,21 +51,6 @@
 
     $pdf->Cell(100, 6, "2751 Chocolate Street", 0, 1, L);
     $pdf->Cell(100, 6, "Pleasanton, CA 94588", 0, 1, L);
-
-
-    $pdf->Cell(189, 6, " ", 0, 1);
-    $pdf->Cell(189, 6, " ", 0, 1);
-    $pdf->Cell(189, 6, " ", 0, 1);
-
-
-    $pdf->SetFont("Arial", "B", 12);
-    $pdf->Cell(20, 5, "Month", 0, 0);
-    $pdf->Cell(20, 5, "Document ID", 0, 0);
-    $pdf->Cell(20, 5, "Description", 0, 0);
-    $pdf->Cell(20, 5, "Charge", 0, 0);
-    $pdf->Cell(20, 5, "Payment", 0, 0);
-    $pdf->Cell(20, 5, "Balance", 0, 1);
-    $pdf->SetFont("Arial", "", 12);
 
 
     $pdf->output();
