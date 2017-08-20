cdb0ccc339fc7327ee46ba8486abb609940b3f75
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-20 15:31:26 +0530

    testing

--- var/www/html/testing.php
+++ var/www/html/testing.php
@@ -4,7 +4,11 @@
     require("fpdf/fpdf.php");
 
     header ( "Content-Type: application/vnd.x-pdf" );
+    header ( "Content-disposition: attachment; filename=Testing_PDF-_".date('m-d-Y H:i:s').".php" );
     header ( "Content-Type: application/force-download" );
+    header ( "Content-Transfer-Encoding: binary" );
+    header ( "Pragma: no-cache" );
+    header ( "Expires: 0" );
 
     $pdf = new FPDF();
     $pdf->AddPage();
