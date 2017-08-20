a66347f9d9538f765f923f3ceb7f54bd1cadcf68
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-20 15:22:28 +0530

    testing

--- var/www/html/testing.php
+++ var/www/html/testing.php
@@ -1,34 +1,10 @@
 <?php
 
 	
-    header ( "Content-Type: application/vnd.x-pdf" );
-    header ( "Content-Type: application/force-download" );
+    require("fpdf/fpdf.php");
 
-    class myExtension fpdi {
-
-		public function addContent($pdffile) {
-
-			$this->setSourceFile($pdffile);
-			$page = $this->ImportPage(1);
-			$this->AddPage();
-			$this->useTemplate($page);
-			$this->$this->Image('my_profile.png', 150, 5, 40, 20, 'PNG');
-			$this->createTextField($string, 25, 50, 40);
-
-		}
-
-		public function createTextField($string, $x, $y, $width, $height = 2) {
-
-			$this->SetXY($x,$y);
-			$this->Setfont("Arial","",10);
-			$this->Cell($width, $height, $string, 0, 0, "");
-
-		}
-
-	}
-
-	$pdf = new myExtension();
-	$pdf->addContent('origpdffile.php');
-	$pdf->Output('newpdffile.pdf', 'I');
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->output();
 
 ?>
