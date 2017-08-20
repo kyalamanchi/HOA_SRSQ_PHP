66c1c220918533fd878832cc2345974b450b8a49
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-21 00:28:48 +0530

    testing

--- var/www/html/testing.php
+++ var/www/html/testing.php
@@ -53,6 +53,21 @@
     $pdf->Cell(100, 6, "Pleasanton, CA 94588", 0, 1, L);
 
 
+    $pdf->Cell(85, 6, " ", 0, 1, R);
+    $pdf->Cell(85, 6, " ", 0, 1, R);
+    $pdf->Cell(85, 6, " ", 0, 1, R);
+
+
+    $pdf->SetFont("Arial", "B", 12);
+    $pdf->Cell(20, 5, "Month", 0, 0);
+    $pdf->Cell(20, 5, "Document ID", 0, 0);
+    $pdf->Cell(20, 5, "Description", 0, 0);
+    $pdf->Cell(20, 5, "Charge", 0, 0);
+    $pdf->Cell(20, 5, "Payment", 0, 0);
+    $pdf->Cell(20, 5, "Balance", 0, 1);
+    $pdf->SetFont("Arial", "", 12);
+
+
     $pdf->output();
 
 
