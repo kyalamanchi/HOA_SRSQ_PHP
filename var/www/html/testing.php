5ad05d4d5a2a4bc516ae07800b75daec65ffb6ec
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-20 12:40:53 +0530

    testing

--- var/www/html/testing.php
+++ var/www/html/testing.php
@@ -7,7 +7,7 @@
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
 	header ( "Content-Type: application/vnd.x-pdf" );
-    header ( "Content-disposition: attachment; filename=".$_SESSION['hoa_community_code']."-Mailing_List-_".date('m-d-Y H:i:s').".csv" );
+    header ( "Content-disposition: attachment; filename=".$_SESSION['hoa_community_code']."-Mailing_List-_".date('m-d-Y H:i:s').".pdf" );
     header ( "Content-Type: application/force-download" );
     header ( "Content-Transfer-Encoding: binary" );
     header ( "Pragma: no-cache" );
