d842be815d6ec7009fc4a2a54a0874b3c8cebde9
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-20 15:25:12 +0530

    testing

--- var/www/html/testing.php
+++ var/www/html/testing.php
@@ -3,6 +3,9 @@
 	
     require("fpdf/fpdf.php");
 
+    header ( "Content-Type: application/vnd.x-pdf" );
+    header ( "Content-Type: application/force-download" );
+
     $pdf = new FPDF();
     $pdf->AddPage();
     $pdf->output();
