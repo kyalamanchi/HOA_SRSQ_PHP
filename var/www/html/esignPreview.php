c9b0a4e3918e08a1dbd884bce4943784b1989178
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-21 11:01:42 +0530

    esign

--- var/www/html/esignPreview.php
+++ var/www/html/esignPreview.php
@@ -4,7 +4,7 @@
 
 	header("Content-type: application/pdf");
 	
-	$agreementID  = '3AAABLblqZhBIQ7hPSRUAC1JwGZP-uirYJ6Qqwvol7mWel8A538YmDDdhTeSmh8GcabLHzfAnLjT4r5HC0d6r3nMwGYM-Ya-i';#$_GET['id'];
+	$agreementID  = $_GET['id'];
 	$url  = 'https://api.na1.echosign.com:443/api/rest/v5/agreements/';
 	$url  = $url.$agreementID."/combinedDocument";
 	$ch = curl_init($url);
