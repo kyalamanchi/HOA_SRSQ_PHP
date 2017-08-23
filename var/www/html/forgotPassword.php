af5be91ec6fdd8ac75d79967de7852d7df05d45b
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-23 08:32:48 +0530

    reset

--- var/www/html/forgotPassword.php
+++ var/www/html/forgotPassword.php
@@ -240,7 +240,7 @@
                   else
                   {
 
-                    $to = 'geethchadalawada@gmail.com';#$reset_email;
+                    $to = 'geethchadalawada@gmail';#$reset_email;
 
                     $otp = rand(100000,1000000);
 
