6c57f69cae141ba1eb97f10830ed86d9fbb025b5
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-27 19:53:41 +0530

    tags

--- var/www/html/residentParkingTags.php
+++ var/www/html/residentParkingTags.php
@@ -38,7 +38,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
 
     <script type="text/javascript">
       
