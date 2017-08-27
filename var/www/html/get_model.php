8d41297adb462927220df70b7bb50b5c63a6287e
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-27 19:33:08 +0530

    tags

--- var/www/html/get_model.php
+++ var/www/html/get_model.php
@@ -4,7 +4,7 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	$result = pg_query("SELECT * FROM car_model WHERE car_make_id=".$_POST['make']);
+	$result = pg_query("SELECT * FROM car_model WHERE car_make_id=1");
 
 	$output = "";
 
