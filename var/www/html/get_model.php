cb828ce9993c70d64b3a9ec5f1bcf2322582b9e6
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-27 22:38:37 +0530

    tag

--- var/www/html/get_model.php
+++ var/www/html/get_model.php
@@ -2,7 +2,7 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	$make = $_GET['make'];
+	$make = $_REQUEST['make'];
 	$output = "<option disabled>Select Model</option>";
 
 	$result = pg_query("SELECT * FROM car_model WHERE car_make_id=$make");
