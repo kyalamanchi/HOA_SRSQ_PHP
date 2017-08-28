b0f3d036f397d60ad3e0666072f075d2094e47b5
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-28 20:34:39 +0530

    tags

--- var/www/html/boardApproveParkingTag.php
+++ var/www/html/boardApproveParkingTag.php
@@ -1,7 +1,16 @@
 <?php
 
-	echo $_POST['tag_id'];
+	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	echo "<center><a href='https://hoaboardtime.com/boardParkingTags.php'>Click here</a> if this page doesnot redirect automatically in 5 seconds.</center><script>setTimeout(function(){window.location.href='https://hoaboardtime.com/boardParkingTags.php'},2000);</script>";
+	$tag_id = $_POST['tag_id'];
+
+	$result = pg_query("UPDATE home_tags SET status='APPROVED' WHERE id=$tag_id");
+
+	if($result)
+		echo "<br><br><br><center><h3>Parking tag approved.</h3></center>";
+	else
+		echo "<br><br><br><center><h3>Some error occured. Please try again later.</h3></center>";
+
+	echo "<center><a href='https://hoaboardtime.com/boardParkingTags.php'>Click here</a> if this page doesnot redirect automatically in 5 seconds.</center><script>setTimeout(function(){window.location.href='https://hoaboardtime.com/boardParkingTags.php'},1000);</script>";
 
 ?>
