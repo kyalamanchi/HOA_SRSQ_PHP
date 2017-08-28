2ac00a58b7fef6b0b475be122b85ef018d04d30a
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-28 18:30:52 +0530

    tags

--- var/www/html/residentParkingTags.php
+++ var/www/html/residentParkingTags.php
@@ -584,7 +584,7 @@
                                   $id = $row1['id'];
                                   $name = $row1['name'];
 
-                                  echo "<option value='$id'>$name - $id</option>";
+                                  echo "<option value='$id'>$name</option>";
 
                                 }
 
@@ -610,7 +610,7 @@
                                   $id = $row1['id'];
                                   $name = $row1['name'];
 
-                                  echo "<option value='$id'>$name - $id</option>";
+                                  echo "<option value='$id'>$name</option>";
 
                                 }
 
