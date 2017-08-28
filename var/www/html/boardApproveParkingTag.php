678d6a1e2627174a5457b0d189161eab4a9d049b
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-28 20:31:06 +0530

    tags

--- var/www/html/boardApproveParkingTag.php
+++ var/www/html/boardApproveParkingTag.php
@@ -2,4 +2,6 @@
 
 	echo $_POST['tag_id'];
 
+	echo "<center><a href='https://hoaboardtime.com/boardParkingTags.php'>Click here</a> if this page doesnot redirect automatically in 5 seconds.</center><script>setTimeout(function(){window.location.href='https://hoaboardtime.com/boardParkingTags.php'},2000);</script>";
+
 ?>
