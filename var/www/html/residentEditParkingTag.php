176d67bc2f570df3c8ae4d911a01262a955473cc
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-28 18:52:29 +0530

    tags

--- var/www/html/residentEditParkingTag.php
+++ var/www/html/residentEditParkingTag.php
@@ -2,12 +2,36 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
+	function encrypt_string($input)
+	{
+									 
+		$inputlen = strlen($input);// Counts number characters in string $input
+		$randkey = rand(1, 9); // Gets a random number between 1 and 9
+									 
+		$i = 0;
+		while ($i < $inputlen)
+		{
+									 
+			$inputchr[$i] = (ord($input[$i]) - $randkey);//encrpytion 
+									 
+			$i++; // For the loop to function
+
+		}
+									 
+		//Puts the $inputchr array togtheir in a string with the $randkey add to the end of the string
+		$encrypted = implode('.', $inputchr) . '.' . (ord($randkey)+50);
+		return $encrypted;
+	}
+
 	$car_id = $_POST['car_id'];
 	$make = $_POST['edit_make'];
 	$model = $_POST['edit_model'];
 	$color = $_POST['edit_color'];
 	$year = $_POST['edit_year'];
 	$plate = $_POST['edit_plate'];
+
+	$plate = encrypt_string($plate);
+	$plate = base64_encode($plate);
 
 	$result = pg_query("UPDATE car_detail SET car_make_id=$make, car_model_id=$model, car_color_id=$color, year=$year, notes='$plate' WHERE id=$car_id");
 
