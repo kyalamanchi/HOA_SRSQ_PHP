d5ecbe15087acec16b16a061d82a2edb1ee76e77
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-31 15:59:13 +0530

    mail

--- var/www/html/residentSendInspectionNotes.php
+++ var/www/html/residentSendInspectionNotes.php
@@ -17,76 +17,86 @@
 	$home = $_POST['home'];
 	$owner = $_POST['owner'];
 	$notice_summary = $_POST['notice_summary'];
-	$status_requested = $_POST['status_requested'];
+	$status_requested_id = $_POST['status_requested'];
 
-	$row = pg_fetch_assoc(pg_query("SELECT * FROM inspection_status WHERE id=".$status_requested));
+	$row = pg_fetch_assoc(pg_query("SELECT * FROM inspection_status WHERE id=".$status_requested_id));
 	$status_requested = $row['inspection_status'];
 
-	$to = "geethchadalawada@gmail.com";#"project-13225888-c32e66904296f8f2b2f5c19f@basecamp.com";
+	if()
+	{
 
-	switch ($community_id) {
-        case 1:
-            $community = 'SRP';
-            $cnote = "Stoneridgeplace HOA";
-            $api_key = 'NRqC1Izl9L8aU-lgm_LS2A';
-            $from = 'info@stoneridgeplace.org';
-            break;
+		$to = "geethchadalawada@gmail.com";#"project-13225888-c32e66904296f8f2b2f5c19f@basecamp.com";
 
-        case 2:
-            $community = 'SRSQ';
-            $cnote = "Stoneridge Square HOA";
-            $api_key = 'MO3K0X3fhNe4qFMX6jOTOw';
-            $from = 'info@stoneridgesquare.org';
-            break;
-    }
+		switch ($community_id) {
+	        case 1:
+	            $community = 'SRP';
+	            $cnote = "Stoneridgeplace HOA";
+	            $api_key = 'NRqC1Izl9L8aU-lgm_LS2A';
+	            $from = 'info@stoneridgeplace.org';
+	            break;
 
-                
-                
-    $content = 'Date : '.$date.'<br>Inspection Notice : '.$inspection_notice.'<br>Inspection Initial Notice : '.$initial_notice.'<br>Compliance Date : '.$compliance_date.'<br>Viewed From : '.$viewed_from.'<br>Item : '.$item.'<br>Observation : '.$observation.'<br>Home : '.$home.'<br>Owner : '.$owner.'<br>Inspection Notes Summary : '.$notice_summary.'.';
-    $subject = $home.' requesting '.$status_requested;
-    $uri = 'https://mandrillapp.com/api/1.0/messages/send.json';
-    $content_text = strip_tags($content);
+	        case 2:
+	            $community = 'SRSQ';
+	            $cnote = "Stoneridge Square HOA";
+	            $api_key = 'MO3K0X3fhNe4qFMX6jOTOw';
+	            $from = 'info@stoneridgesquare.org';
+	            break;
+	    }
+
+	                
+	                
+	    $content = 'Date : '.$date.'<br>Inspection Notice : '.$inspection_notice.'<br>Inspection Initial Notice : '.$initial_notice.'<br>Compliance Date : '.$compliance_date.'<br>Viewed From : '.$viewed_from.'<br>Item : '.$item.'<br>Observation : '.$observation.'<br>Home : '.$home.'<br>Owner : '.$owner.'<br>Inspection Notes Summary : '.$notice_summary.'.';
+	    $subject = $home.' requesting '.$status_requested;
+	    $uri = 'https://mandrillapp.com/api/1.0/messages/send.json';
+	    $content_text = strip_tags($content);
 
 
-    $params = array(
-        "key" => $api_key,
-        "message" => array(
-            "html" => $content,
-            "text" => $content_text,
-            "subject" => $subject,
-            "from_email" => $from,
-            "from_name" => $from,
-            "to" => array(
-                array("email" => $to, "name" => $to)
-            ),
-            "track_opens" => true,
-            "track_clicks" => true,
-            "auto_text" => true,
-            "url_strip_qs" => true,
-            "preserve_recipients" => true
-        ),
-        "async" => false
-   	);
+	    $params = array(
+	        "key" => $api_key,
+	        "message" => array(
+	            "html" => $content,
+	            "text" => $content_text,
+	            "subject" => $subject,
+	            "from_email" => $from,
+	            "from_name" => $from,
+	            "to" => array(
+	                array("email" => $to, "name" => $to)
+	            ),
+	            "track_opens" => true,
+	            "track_clicks" => true,
+	            "auto_text" => true,
+	            "url_strip_qs" => true,
+	            "preserve_recipients" => true
+	        ),
+	        "async" => false
+	   	);
 
-    if($to)
-    {
-        $ch = curl_init();
-        $postString = json_encode($params);
+	    if($to)
+	    {
+	        $ch = curl_init();
+	        $postString = json_encode($params);
 
-        curl_setopt($ch, CURLOPT_URL, $uri);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+	        curl_setopt($ch, CURLOPT_URL, $uri);
+	        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	        curl_setopt($ch, CURLOPT_POST, true);
+	        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
 
-        $result = curl_exec($ch);
+	        $result = curl_exec($ch);
 
-        echo "<br><br><center><h3>Email Sent.</h3></center>";
-    }
-    else
+	        echo "<br><br><center><h3>Email Sent.</h3></center>";
+	    }
+	    else
+		{
+			$result = pg_query("DELETE FROM inspection_notes WHERE inspection_notices_id=$id AND notes_date='$date' AND status_requested_id=$status_requested_id");
+
+			echo "<br><br><center><h3>Some error occured. Please try again.</h3></center>";
+		}
+
+	}
+	else
 		echo "<br><br><center><h3>Some error occured. Please try again.</h3></center>";
-
 
 	echo "<center><a href='https://hoaboardtime.com/residentViolationCitation.php'>Click here</a> if this page doesnot redirect automatically within 5 seconds.</center><script>setTimeout(function(){window.location.href='https://hoaboardtime.com/residentViolationCitation.php'},3000);</script>";
 ?>
