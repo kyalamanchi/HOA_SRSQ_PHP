5e5af0b6b8778eb6b73c8bf19e99157bb2df03dc
GeethChadalawada <geethchadalawada@gmail.com>
2017-08-31 16:05:43 +0530

    asdfhgjk

--- var/www/html/residentSendInspectionNotes.php
+++ var/www/html/residentSendInspectionNotes.php
@@ -27,7 +27,7 @@
 	if($result)
 	{
 
-		$to = "geethchadalawada@gmail";#"project-13225888-c32e66904296f8f2b2f5c19f@basecamp.com";
+		$to = "geethchadalawada";#"project-13225888-c32e66904296f8f2b2f5c19f@basecamp.com";
 
 		switch ($community_id) {
 	        case 1:
