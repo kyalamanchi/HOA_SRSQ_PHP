90eb242dee952416a615f0735dd117987da3878a
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-01 06:07:05 +0530

    Inspection Update

--- var/www/html/testV.php
+++ var/www/html/testV.php
@@ -15,8 +15,10 @@
 </head>
 <body>
 	<?php
+
 require('fpdf/mc_table.php');
 date_default_timezone_set('America/Los_Angeles');
+echo "Message";
 $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
 if ( $connection ){
 	$inspectionNoticeQuery= "SELECT * FROM INSPECTION_NOTICES WHERE ID=".$_GET['id'];
