c9c3d30592e33fcf2a399b76276bdab02af1a5f2
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-01 13:52:05 +0530

    in

--- var/www/html/testV.php
+++ var/www/html/testV.php
@@ -2,95 +2,95 @@
 ob_start();
 require('fpdf/mc_table.php');
 date_default_timezone_set('America/Los_Angeles');
-$connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
-if ( $connection ){
-    $inspectionNoticeQuery= "SELECT * FROM INSPECTION_NOTICES WHERE ID=".$_GET['id'];
-    $inspectionNoticeQueryResult = pg_query($inspectionNoticeQuery);
-    while ($row = pg_fetch_assoc($inspectionNoticeQueryResult)) {
-        $insideData = array();
-        array_push($insideData, $row);  
-    }
-    $inspectionTypeDetails = array();
-    $inspectionTypeQuery = "SELECT * FROM INSPECTION_NOTICE_TYPE";
-    $inspectionTypeQueryResult = pg_query($inspectionTypeQuery);
-    while ($row = pg_fetch_assoc($inspectionTypeQueryResult)) {
-        $inspectionTypeDetails[$row['id']] = $row['name'];
-    }
-    $homeQuery = "SELECT * FROM HOMEID";
-    $homeQueryResult = pg_query($homeQuery);
-    $homeDetails = array();
-    while ($row = pg_fetch_assoc($homeQueryResult)) {
-        $homeDetails[$row['home_id']] = $row['address1'];
-        $livingStatus = $row['living_status'];
-    }
-    $inspectionNoticeInfo = array("InspectionData"=>$insideData);
-    $inspectionNoticeInfo = json_encode($inspectionNoticeInfo);
-    $inspectionNoticeInfo = json_decode($inspectionNoticeInfo);
-    foreach ($inspectionNoticeInfo->InspectionData as $key) {
-        $accountNumber = $key->hoa_id;
-        $communityID = $key->community_id;
-        $propertyAddress = $homeDetails[$key->home_id];
-        $violationID = $key->id;
-        $homeIDValue = $key->home_id;
-        $inspectionType = $inspectionTypeDetails[$key->inspection_notice_type_id];
-        $inspectionDoneDate = $key->inspection_date;
-        $locationFound = $key->location_id;
+// $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
+// if ( $connection ){
+//     $inspectionNoticeQuery= "SELECT * FROM INSPECTION_NOTICES WHERE ID=".$_GET['id'];
+//     $inspectionNoticeQueryResult = pg_query($inspectionNoticeQuery);
+//     while ($row = pg_fetch_assoc($inspectionNoticeQueryResult)) {
+//         $insideData = array();
+//         array_push($insideData, $row);  
+//     }
+//     $inspectionTypeDetails = array();
+//     $inspectionTypeQuery = "SELECT * FROM INSPECTION_NOTICE_TYPE";
+//     $inspectionTypeQueryResult = pg_query($inspectionTypeQuery);
+//     while ($row = pg_fetch_assoc($inspectionTypeQueryResult)) {
+//         $inspectionTypeDetails[$row['id']] = $row['name'];
+//     }
+//     $homeQuery = "SELECT * FROM HOMEID";
+//     $homeQueryResult = pg_query($homeQuery);
+//     $homeDetails = array();
+//     while ($row = pg_fetch_assoc($homeQueryResult)) {
+//         $homeDetails[$row['home_id']] = $row['address1'];
+//         $livingStatus = $row['living_status'];
+//     }
+//     $inspectionNoticeInfo = array("InspectionData"=>$insideData);
+//     $inspectionNoticeInfo = json_encode($inspectionNoticeInfo);
+//     $inspectionNoticeInfo = json_decode($inspectionNoticeInfo);
+//     foreach ($inspectionNoticeInfo->InspectionData as $key) {
+//         $accountNumber = $key->hoa_id;
+//         $communityID = $key->community_id;
+//         $propertyAddress = $homeDetails[$key->home_id];
+//         $violationID = $key->id;
+//         $homeIDValue = $key->home_id;
+//         $inspectionType = $inspectionTypeDetails[$key->inspection_notice_type_id];
+//         $inspectionDoneDate = $key->inspection_date;
+//         $locationFound = $key->location_id;
 
-    }
-    $homeIDQuery = "SELECT * FROM HOMEID WHERE HOME_ID=".$homeIDValue;
-    $homeIDQueryResult = pg_query($homeIDQuery);
+//     }
+//     $homeIDQuery = "SELECT * FROM HOMEID WHERE HOME_ID=".$homeIDValue;
+//     $homeIDQueryResult = pg_query($homeIDQuery);
 
-    while ($row = pg_fetch_assoc($homeIDQueryResult)) {
-        $homeAddress1 = $row['address1'];
-        $homeAddress2 = $row['city_id'];
-        $homeAddress3 = $row['state_id'];
-        $homeAddress4 = $row['zip_id'];
-    }
-    $hoaIDQuery = "SELECT * FROM HOAID WHERE HOA_ID=".$accountNumber;
-    $hoaIDQueryResult = pg_query($hoaIDQuery);
-    while ($row = pg_fetch_assoc($hoaIDQueryResult)) {
-        $firstName  = $row['firstname'];
-        $lastName = $row ['lastname'];
-    }
-    $communityQuery = "SELECT * FROM COMMUNITY_INFO WHERE COMMUNITY_ID=".$communityID;
-    $communityQueryResult = pg_query($communityQuery);
-    $cityInfoQuery = "SELECT * FROM CITY";
-    $cityInfoQueryResult = pg_query($cityInfoQuery);
-    $cityDetails = array();
-    while ($row = pg_fetch_assoc($cityInfoQueryResult)) {
-        $cityDetails[$row['city_id']] = $row['city_name'];
-    }
-    $stateDetails = array();
-    $stateQuery= "  SELECT * FROM STATE";
-    $stateQueryResult = pg_query($stateQuery);
-    while ($row = pg_fetch_assoc($stateQueryResult)) {
-        $stateDetails[$row['state_id']] =  $row['state_code'];
-    }
-    $zipDetails = array();
-    $zipQuery = "SELECT * FROM ZIP";
-    $zipQueryResult = pg_query($zipQuery);
-    while ($row = pg_fetch_assoc($zipQueryResult)) {
-        $zipDetails[$row['zip_id']] = $row['zip_code'];
-    }
-    while ($row = pg_fetch_assoc($communityQueryResult)) {
-        $communityCode = $row['community_code'];
-        $communityLegalName = $row['legal_name'];
-        $communityMailingAddress = $row['mailing_address'];
-        $communityMailingAddress2 = $cityDetails[$row['mailing_addr_city']];
-        $communityMailingAddress3 = $stateDetails[$row['mailing_addr_state']];
-        $communityMailingAddress4 = $zipDetails[$row['mailing_addr_zip']];
-    }
-    $locationQuery = "SELECT * FROM LOCATIONS_IN_COMMUNITY ";
-    $locationQueryResult = pg_query($locationQuery);
-    $locationDetails = array();
-    while ($row = pg_fetch_assoc($locationQueryResult)) {
-            $locationDetails[$row['location_id']] = $row['location'];
-    }
-    $inspectionSubjectQuery = "SELECT * FROM INSPECTION_NOTICE_SUBJECT WHERE ID=1";
-    $inspectionSubjectQueryResult = pg_query($inspectionSubjectQuery);
-    $row = pg_fetch_assoc($inspectionSubjectQueryResult);
-    $inspectionSubjectFinal =$row['desc'];
-}
+//     while ($row = pg_fetch_assoc($homeIDQueryResult)) {
+//         $homeAddress1 = $row['address1'];
+//         $homeAddress2 = $row['city_id'];
+//         $homeAddress3 = $row['state_id'];
+//         $homeAddress4 = $row['zip_id'];
+//     }
+//     $hoaIDQuery = "SELECT * FROM HOAID WHERE HOA_ID=".$accountNumber;
+//     $hoaIDQueryResult = pg_query($hoaIDQuery);
+//     while ($row = pg_fetch_assoc($hoaIDQueryResult)) {
+//         $firstName  = $row['firstname'];
+//         $lastName = $row ['lastname'];
+//     }
+//     $communityQuery = "SELECT * FROM COMMUNITY_INFO WHERE COMMUNITY_ID=".$communityID;
+//     $communityQueryResult = pg_query($communityQuery);
+//     $cityInfoQuery = "SELECT * FROM CITY";
+//     $cityInfoQueryResult = pg_query($cityInfoQuery);
+//     $cityDetails = array();
+//     while ($row = pg_fetch_assoc($cityInfoQueryResult)) {
+//         $cityDetails[$row['city_id']] = $row['city_name'];
+//     }
+//     $stateDetails = array();
+//     $stateQuery= "  SELECT * FROM STATE";
+//     $stateQueryResult = pg_query($stateQuery);
+//     while ($row = pg_fetch_assoc($stateQueryResult)) {
+//         $stateDetails[$row['state_id']] =  $row['state_code'];
+//     }
+//     $zipDetails = array();
+//     $zipQuery = "SELECT * FROM ZIP";
+//     $zipQueryResult = pg_query($zipQuery);
+//     while ($row = pg_fetch_assoc($zipQueryResult)) {
+//         $zipDetails[$row['zip_id']] = $row['zip_code'];
+//     }
+//     while ($row = pg_fetch_assoc($communityQueryResult)) {
+//         $communityCode = $row['community_code'];
+//         $communityLegalName = $row['legal_name'];
+//         $communityMailingAddress = $row['mailing_address'];
+//         $communityMailingAddress2 = $cityDetails[$row['mailing_addr_city']];
+//         $communityMailingAddress3 = $stateDetails[$row['mailing_addr_state']];
+//         $communityMailingAddress4 = $zipDetails[$row['mailing_addr_zip']];
+//     }
+//     $locationQuery = "SELECT * FROM LOCATIONS_IN_COMMUNITY ";
+//     $locationQueryResult = pg_query($locationQuery);
+//     $locationDetails = array();
+//     while ($row = pg_fetch_assoc($locationQueryResult)) {
+//             $locationDetails[$row['location_id']] = $row['location'];
+//     }
+//     $inspectionSubjectQuery = "SELECT * FROM INSPECTION_NOTICE_SUBJECT WHERE ID=1";
+//     $inspectionSubjectQueryResult = pg_query($inspectionSubjectQuery);
+//     $row = pg_fetch_assoc($inspectionSubjectQueryResult);
+//     $inspectionSubjectFinal =$row['desc'];
+// }
 $pdf=new PDF_MC_Table();
 $pdf->AddPage();
 $pdf->SetTextColor(0,0,128);
