d17b289e89b7128b81cc9e16099df60814943705
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-01 13:03:49 +0530

    script

--- var/www/html/violationManagementUpload.php
+++ var/www/html/violationManagementUpload.php
@@ -5,6 +5,16 @@
 
 	#echo $json['Inspection_Data_Input_Form'][0]['Viewed_From'];
 
-	echo sizeof($json['Inspection_Data_Input_Form']);
+	$count = sizeof($json['Inspection_Data_Input_Form']);
+
+	for($i = 0; $i < $count; $i++)
+	{
+		$home = $json['Inspection_Data_Input_Form'][$i]['Address1'];
+		$item = $json['Inspection_Data_Input_Form'][$i]['Lookup_1'];
+		$category = $json['Inspection_Data_Input_Form'][$i]['Category'];
+		$compliance_date = $json['Inspection_Data_Input_Form'][$i]['Compliance_Date'];
+
+		echo $home." - - - ".$item." - - - ".$category." - - - ".$compliance_date;
+	}
 
 ?>
