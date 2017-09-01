2946f91507418407d4fac92d6bdfd26b7754003a
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-01 14:13:57 +0530

    #5

--- var/www/html/testV.php
+++ var/www/html/testV.php
@@ -10,9 +10,9 @@
 $pdf->Ln();
 $pdf->SetTextColor(0,0,0);
 $pdf->SetFont('Arial','B',8);
-$pdf->MultiCell(0,6,'CommunityName',0,'0',false);
+// $pdf->MultiCell(0,6,'CommunityName',0,'0',false);
 $pdf->SetFont('Arial','',8);
-$pdf->MultiCell(0,3,'mailing address'."\n".'Mailing address'."\n".'mailing address'." ".'Mailing address',0,'0',false);
+// $pdf->MultiCell(0,3,'mailing address'."\n".'Mailing address'."\n".'mailing address'." ".'Mailing address',0,'0',false);
 $pdf->Ln();
 
     $pdf->SetFont("Arial", "", 12);
