304597cbac5684f673a7f3f3f8958c57971ffb34
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-01 13:48:32 +0530

    Inspection Update

--- var/www/html/testV.php
+++ var/www/html/testV.php
@@ -1,5 +1,5 @@
 <?php
-ini_set("session.auto_start", 0);
+ob_start();
 require('fpdf/mc_table.php');
 date_default_timezone_set('America/Los_Angeles');
 $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
