8bafa9ce32c02843b56f5aa345006355cabe1674
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-01 12:58:01 +0530

    script

--- var/www/html/violationManagementUpload.php
+++ var/www/html/violationManagementUpload.php
@@ -3,6 +3,8 @@
 	$jsonData = file_get_contents("HOAView.json");
 	$json = json_decode($jsonData, true);
 
-	echo $json['Inspection_Data_Input_Form'][0]['Viewed_From'];
+	#echo $json['Inspection_Data_Input_Form'][0]['Viewed_From'];
+
+	echo sizeof($json['Inspection_Data_Input_Form']);
 
 ?>
