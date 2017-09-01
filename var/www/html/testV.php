2be79aec217816f6e8fdf3cb126fe0e674499373
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-01 06:18:11 +0530

    Inspection Update

--- var/www/html/testV.php
+++ var/www/html/testV.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>
-        
-    </title>
-    <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-
-</head>
-<body>
-    <?php
+<?php
 require('fpdf/mc_table.php');
 date_default_timezone_set('America/Los_Angeles');
 $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
@@ -145,46 +129,5 @@ $pdf->WriteHTML('<br><i>Trash cans, refuse containers and recycling containers s
 $pdf->Rect($pdf->w,$pdf->h,100,1);
 $pdf->Output();
 ?>
-<br>
-<br>
-<center>
-<div>
-<button type="button" class="btn btn-primary" onclick="generateForSouthData();">Generate for South Data</button><br><br>
-</div>
-</center>
-<embed src="data.pdf" width="100%" height="750"></embed>
-<script type="text/javascript">
-    function generateForSouthData(){
-        <?php
-        echo 'window.alert("Heelooo");';
 
-        $handler = fopen('data.tab', 'w');
-        $finalWriteData = "1"."\t".$firstName.' '.$lastName."\t".$homeAddress1."\t".''."\t".$cityDetails[$homeAddress2].' '.$stateDetails[$homeAddress3].' '.$zipDetails[$homeAddress4]."\t".''."\t1\t"."1"."\t"."data.pdf\t".$communityLegalName."\t".$communityMailingAddress."\t".$communityMailingAddress2." ".$communityMailingAddress3.' '.$communityMailingAddress4."\t"."\t".$communityLegalName;
-        fwrite($handler, $finalWriteData);
-        fclose($handler);
-        $zip = new ZipArchive;
-if ($zip->open($violationID.'.zip',  ZipArchive::CREATE)) {
-$zip->addFile('data.pdf', 'data.pdf');
-$zip->addFile('data.tab', 'data.tab');
-$zip->close();
-$url = 'https://content.dropboxapi.com/2/files/upload';
-$fileContents = file_get_contents($violationID.'.zip');
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer n-Bgs_XVPEAAAAAAAAEQYgvfkzJWzxx59jqgvKQeXbtsYt-eXdZ6BNRYivEGKVGB','Content-Type:application/octet-stream','Dropbox-API-Arg: {"path": "/Inspection_Notices_New/'.date('Y').'/'.$violationID.'.zip'.'","mode": "overwrite","autorename": false,"mute": false}'));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $fileContents); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$response = curl_exec($ch);
-curl_close($ch);
-echo 'window.alert('.(json_decode($response)).')';
-
-unlink($violationID.'.zip');
-unlink('data.pdf');
-unlink('data.tab');
-    }
-    ?>
-}
-</script>
-</body>
-</html>
 
