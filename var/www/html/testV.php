7224a753478130f60676c0c4eb181194d4f340ab
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-01 14:01:59 +0530

    #3

--- var/www/html/testV.php
+++ var/www/html/testV.php
@@ -1,134 +1,260 @@
 <?php
-ob_start();
-require('fpdf/mc_table.php');
-date_default_timezone_set('America/Los_Angeles');
-// $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
-// if ( $connection ){
-//     $inspectionNoticeQuery= "SELECT * FROM INSPECTION_NOTICES WHERE ID=".$_GET['id'];
-//     $inspectionNoticeQueryResult = pg_query($inspectionNoticeQuery);
-//     while ($row = pg_fetch_assoc($inspectionNoticeQueryResult)) {
-//         $insideData = array();
-//         array_push($insideData, $row);  
-//     }
-//     $inspectionTypeDetails = array();
-//     $inspectionTypeQuery = "SELECT * FROM INSPECTION_NOTICE_TYPE";
-//     $inspectionTypeQueryResult = pg_query($inspectionTypeQuery);
-//     while ($row = pg_fetch_assoc($inspectionTypeQueryResult)) {
-//         $inspectionTypeDetails[$row['id']] = $row['name'];
-//     }
-//     $homeQuery = "SELECT * FROM HOMEID";
-//     $homeQueryResult = pg_query($homeQuery);
-//     $homeDetails = array();
-//     while ($row = pg_fetch_assoc($homeQueryResult)) {
-//         $homeDetails[$row['home_id']] = $row['address1'];
-//         $livingStatus = $row['living_status'];
-//     }
-//     $inspectionNoticeInfo = array("InspectionData"=>$insideData);
-//     $inspectionNoticeInfo = json_encode($inspectionNoticeInfo);
-//     $inspectionNoticeInfo = json_decode($inspectionNoticeInfo);
-//     foreach ($inspectionNoticeInfo->InspectionData as $key) {
-//         $accountNumber = $key->hoa_id;
-//         $communityID = $key->community_id;
-//         $propertyAddress = $homeDetails[$key->home_id];
-//         $violationID = $key->id;
-//         $homeIDValue = $key->home_id;
-//         $inspectionType = $inspectionTypeDetails[$key->inspection_notice_type_id];
-//         $inspectionDoneDate = $key->inspection_date;
-//         $locationFound = $key->location_id;
 
-//     }
-//     $homeIDQuery = "SELECT * FROM HOMEID WHERE HOME_ID=".$homeIDValue;
-//     $homeIDQueryResult = pg_query($homeIDQuery);
+    
+    session_start();
 
-//     while ($row = pg_fetch_assoc($homeIDQueryResult)) {
-//         $homeAddress1 = $row['address1'];
-//         $homeAddress2 = $row['city_id'];
-//         $homeAddress3 = $row['state_id'];
-//         $homeAddress4 = $row['zip_id'];
-//     }
-//     $hoaIDQuery = "SELECT * FROM HOAID WHERE HOA_ID=".$accountNumber;
-//     $hoaIDQueryResult = pg_query($hoaIDQuery);
-//     while ($row = pg_fetch_assoc($hoaIDQueryResult)) {
-//         $firstName  = $row['firstname'];
-//         $lastName = $row ['lastname'];
-//     }
-//     $communityQuery = "SELECT * FROM COMMUNITY_INFO WHERE COMMUNITY_ID=".$communityID;
-//     $communityQueryResult = pg_query($communityQuery);
-//     $cityInfoQuery = "SELECT * FROM CITY";
-//     $cityInfoQueryResult = pg_query($cityInfoQuery);
-//     $cityDetails = array();
-//     while ($row = pg_fetch_assoc($cityInfoQueryResult)) {
-//         $cityDetails[$row['city_id']] = $row['city_name'];
-//     }
-//     $stateDetails = array();
-//     $stateQuery= "  SELECT * FROM STATE";
-//     $stateQueryResult = pg_query($stateQuery);
-//     while ($row = pg_fetch_assoc($stateQueryResult)) {
-//         $stateDetails[$row['state_id']] =  $row['state_code'];
-//     }
-//     $zipDetails = array();
-//     $zipQuery = "SELECT * FROM ZIP";
-//     $zipQueryResult = pg_query($zipQuery);
-//     while ($row = pg_fetch_assoc($zipQueryResult)) {
-//         $zipDetails[$row['zip_id']] = $row['zip_code'];
-//     }
-//     while ($row = pg_fetch_assoc($communityQueryResult)) {
-//         $communityCode = $row['community_code'];
-//         $communityLegalName = $row['legal_name'];
-//         $communityMailingAddress = $row['mailing_address'];
-//         $communityMailingAddress2 = $cityDetails[$row['mailing_addr_city']];
-//         $communityMailingAddress3 = $stateDetails[$row['mailing_addr_state']];
-//         $communityMailingAddress4 = $zipDetails[$row['mailing_addr_zip']];
-//     }
-//     $locationQuery = "SELECT * FROM LOCATIONS_IN_COMMUNITY ";
-//     $locationQueryResult = pg_query($locationQuery);
-//     $locationDetails = array();
-//     while ($row = pg_fetch_assoc($locationQueryResult)) {
-//             $locationDetails[$row['location_id']] = $row['location'];
-//     }
-//     $inspectionSubjectQuery = "SELECT * FROM INSPECTION_NOTICE_SUBJECT WHERE ID=1";
-//     $inspectionSubjectQueryResult = pg_query($inspectionSubjectQuery);
-//     $row = pg_fetch_assoc($inspectionSubjectQueryResult);
-//     $inspectionSubjectFinal =$row['desc'];
-// }
-$pdf=new PDF_MC_Table();
-$pdf->AddPage();
-$pdf->SetTextColor(0,0,128);
-$pdf->Ln();
-$pdf->SetTextColor(0,0,0);
-$pdf->SetFont('Arial','B',8);
-$pdf->MultiCell(0,6,'name',0,'0',false);
-$pdf->SetFont('Arial','',8);
-$pdf->MultiCell(0,3,'name'."\n".'name'."\n".'name'." ".'name',0,'0',false);
-$pdf->Ln();
-$pdf->SetX(113);
-$pdf->SetWidths(array(40,50));
-$pdf->SetTextColor(0,0,0);
-$pdf->SetFont('Arial','B',9);
-$pdf->Row(array('Account Number','123'));
-$pdf->SetX(113);
-$pdf->Row(array('Community ID','123'));
-$pdf->SetX(113);
-$pdf->Row(array('Property Address','123'));
-$pdf->SetX(113);
-$pdf->Row(array('Violation Notice ID','123'));
-$pdf->SetX(113);
-$pdf->Row(array('Notice Type', '123'));
-$pdf->Ln();
-$pdf->SetY(52.5);
-$pdf->MultiCell(0,6,'name'." ".'name'." OR Current Resident",0,'0',false);
-$pdf->SetFont('','',9);
-$pdf->MultiCell(0,3.5,'name'."\n".'name'.", ".'name'.",".'name'."\n\n\n".date('M d,Y',strtotime('name'))."",0,'0',false);
-$pdf->SetFont('','B',9);
-$pdf->MultiCell(0,3.5,"\n\nRE: ".'name'." ",0,'0',false);
-$pdf->SetFont('','',9);
-$pdf->MultiCell(0,3.5,"\n\nDear ".'name'." ".'name'." OR Current Resident:\n\n".'name'." is a planned community governed by covenants, conditions and restrictions. Compliance with these rules benefits the entire community and all property owners are responsible for protecting the aesthetics and harmony of the neighborhood.
-\n\nBy now you have probably already corrected the following issue at ".'name'.". If not, then this is a courtesy reminder from Stoneridge Square.\n\nIt has been reported or observed during a routine site inspection on ".date('m/d/y',strtotime('name'))." that the property was out of compliance with the community rules and regulations.",0,'0',false);
-$pdf->WriteHTML("<br><b> This violation specifically regards the following item(s): ".'name'."</b>. It was noted that this violation occurred in the following location: ".'name'.".<br><br>The Governing Documents, specifically<b> Rules and Regulations, Section 5.13 (3) </b>of the Declaration of Covenants, Conditions, and Restrictions (Deed Restrictions) for Stoneridge Square Association states, in part:");
-$pdf->Ln();
-$pdf->WriteHTML('<br><i>Trash cans, refuse containers and recycling containers shall be stored within the garage or screened from view by an appropriate fence in a side yard area except on pick-up day when such containers may be relocated to pick-up areas designated by the City for such purposes. All such containers shall be removed from public view by the end of the day of refuse pick-up provided by the waste disposal service company.</i><br><br>If you have already corrected the issue noted above, please disregard this courtesy notice, since no further action is required.<br><br>Thank you for your cooperation in maintaining the appearance and value of Stoneridge Square. If you have any questions, please contact us via our Resident Portal at <a href="https://hoaboardtime.com">https://hoaboardtime.com</a><br><br>'.'name');
-$pdf->Rect($pdf->w,$pdf->h,100,1);
-$pdf->Output();
+    $community_id = $_SESSION['hoa_community_id'];
+
+    pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
+    
+    $year = date("Y");
+    $month = date("m");
+    $end_date = date("t");
+
+    $home_id = $_REQUEST['home_id'];#$_SESSION['hoa_home_id'];
+    $hoa_id = $_REQUEST['hoa_id'];#$_SESSION['hoa_hoa_id'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM hoaid WHERE hoa_id=$hoa_id"));
+
+    $cus_name = $row['firstname'];
+    $cus_name .= " ";
+    $cus_name .= $row['lastname'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM community_info WHERE community_id=$community_id"));
+
+    $city = $row['payment_city'];
+    $c_name = $row['legal_name'];
+    $pobox = $row['remit_payment_address'];
+    $state = $row['payment_addr_state'];
+    $zip = $row['payment_addr_zip'];
+    $tax_id = $row['taxid'];
+    $c_email = $row['email'];
+    $c_website = $row['community_website_url'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM state WHERE state_id=$state"));
+    $state = $row['state_code'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM city WHERE city_id=$city"));
+    $city = $row['city_name'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM zip WHERE zip_id=$zip"));
+    $zip = $row['zip_code'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM homeid WHERE home_id=$home_id"));
+
+    $living_status = $row['living_status'];
+
+    if($living_status == 't')
+    {
+        $cus_addr = $row['address1'];
+        $cus_state = $row['state_id'];
+        $cus_city = $row['city_id'];
+        $cus_zip = $row['zip_id'];
+    }
+    else
+    {
+            
+        $result = pg_query("SELECT * FROM home_mailing_address WHERE home_id=$home_id");
+
+        $row = pg_fetch_assoc($result);
+
+        $cus_addr = $row['address1'];
+        $cus_state = $row['state_id'];
+        $cus_city = $row['city_id'];
+        $cus_zip = $row['zip_id'];
+          
+    }
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM state WHERE state_id=$cus_state"));
+    $cus_state = $row['state_code'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM city WHERE city_id=$cus_city"));
+    $cus_city = $row['city_name'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT * FROM zip WHERE zip_id=$cus_zip"));
+    $cus_zip = $row['zip_code'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT amount FROM assessment_amounts WHERE community_id=$community_id"));
+    $assessment_amount = $row['amount'];
+
+    $result = pg_query("SELECT * FROM current_charges WHERE home_id=".$home_id." ORDER BY assessment_date DESC LIMIT 1");
+
+    $row = pg_fetch_assoc($result);
+    $adate = $row['assessment_date'];
+
+    $adate = date("m-d-y", strtotime($adate));
+
+    if(date("m", strtotime($adate)) == date("m"))
+        $month = date("m"); 
+    else if(date("m", strtotime($adate)) < date("m")) 
+        $month = date("m")-1; 
+    else if(date("m", strtotime($adate)) > date("m")) 
+        $month = date("m")+1; 
+
+    $ddate = $month."-15-".date("y");
+
+    $row = pg_fetch_assoc(pg_query("SELECT sum(amount) FROM current_charges WHERE home_id=$home_id AND hoa_id=$hoa_id"));
+    $total_charges = $row['sum'];
+
+    $row = pg_fetch_assoc(pg_query("SELECT sum(amount) FROM current_payments WHERE home_id=$home_id AND hoa_id=$hoa_id AND payment_status_id=1"));
+    $total_payments = $row['sum'];
+
+    $total = $total_charges - $total_payments;
+
+
+    require("fpdf/fpdf.php");
+
+
+    header ( "Content-Type: application/vnd.x-pdf" );
+    header ( "Content-disposition: attachment; filename=Testing_PDF-_".date('m-d-Y H:i:s').".pdf" );
+    header ( "Content-Type: application/force-download" );
+    header ( "Content-Transfer-Encoding: binary" );
+    header ( "Pragma: no-cache" );
+    header ( "Expires: 0" );
+
+
+    $pdf = new FPDF();
+    $pdf->AddPage();
+
+
+    $pdf->SetFont("Arial", "", 12);
+
+
+    $pdf->Cell(100, 6, "From :", 0, 1, L);
+
+
+    $pdf->SetFont("Arial", "B", 12);
+    $pdf->Cell(100, 6, $c_name, 0, 0, L);
+    $pdf->SetFont("Arial", "", 12);
+    $pdf->Cell(85, 6, "Invoice No : ".$community_id."-".$home_id."-".$hoa_id."-".$year, 0, 1, R);
+
+
+    $pdf->Cell(100, 6, $pobox, 0, 0, L);
+    $pdf->Cell(85, 6, "Invoice Date : ".$adate, 0, 1, R);
+
+
+    $pdf->Cell(100, 6, $city.", ".$state." ".$zip, 0, 0, L);
+    $pdf->Cell(85, 6, "Due Date : ".$ddate, 0, 1, R);
+
+
+    $pdf->Cell(189, 6, " ", 0, 1);
+    $pdf->Cell(189, 6, " ", 0, 1);
+    $pdf->Cell(189, 6, " ", 0, 1);
+
+
+    $pdf->Cell(100, 6, "To :", 0, 1, L);
+
+
+    $pdf->SetFont("Arial", "B", 12);
+    $pdf->Cell(100, 6, $cus_name, 0, 1, L);
+    $pdf->SetFont("Arial", "", 12);
+
+
+    $pdf->Cell(100, 6, $cus_addr, 0, 1, L);
+    $pdf->Cell(100, 6, $cus_city.", ".$cus_state." ".$cus_zip, 0, 1, L);
+
+
+    $pdf->Cell(189, 6, " ", 0, 1);
+
+
+    $pdf->SetFont("Arial", "B", 10);
+    $pdf->Cell(20, 6, "Month", 0, 0);
+    $pdf->Cell(30, 6, "Document ID", 0, 0);
+    $pdf->Cell(80, 6, "Description", 0, 0);
+    $pdf->Cell(20, 6, "Charge", 0, 0);
+    $pdf->Cell(20, 6, "Payment", 0, 0);
+    $pdf->Cell(40, 6, "Total", 0, 1);
+    $pdf->SetFont("Arial", "", 10);
+
+
+    for($m = 1; $m <= 12; $m++)
+    {
+
+        $last_date = date("Y-m-t", strtotime("$year-$m-1"));
+                          
+        $charges_results = pg_query("SELECT * FROM current_charges WHERE home_id=$home_id AND hoa_id=$hoa_id AND assessment_date>='$year-$m-1' AND assessment_date<='$last_date' ORDER BY assessment_date");
+
+        $payments_results = pg_query("SELECT * FROM current_payments WHERE home_id=$home_id AND hoa_id=$hoa_id AND process_date>='$year-$m-1' AND process_date<='$last_date' ORDER BY process_date");
+
+        $pdf->SetFillColor(247,248,249);
+        $pdf->SetTextColor(0);
+
+        $month_charge = 0.0;
+
+        while($charges_row = pg_fetch_assoc($charges_results))
+        {
+
+            $month_charge += $charges_row['amount'];
+            $tdate = $charges_row['assessment_date'];
+            $desc = $charges_row['assessment_rule_type_id'];
+
+            $r = pg_fetch_assoc(pg_query("SELECT * FROM assessment_rule_type WHERE assessment_rule_type_id=$desc"));
+            $desc = $r['name'];
+
+            $pdf->Cell(20, 5, date('F', strtotime($tdate)), 0, 0, 'L');
+            $pdf->Cell(30, 5, $charges_row['id']."-".$charges_row['assessment_rule_type_id'], 0, 0, 'L');
+            $pdf->Cell(80, 5, date('m-d-y', strtotime($tdate))." | ".$desc, 0, 0, 'L');
+            $pdf->Cell(20, 5, "$ ".$charges_row['amount'], 0, 0, 'L');
+            $pdf->Cell(20, 5, " ", 0, 0, 'L');
+            $pdf->Cell(40, 5, "$ ".$month_charge, 0, 1, 'L');
+
+        }       
+        
+        $month_payment = 0.0;
+        $fill = true;
+
+        while($payments_row = pg_fetch_assoc($payments_results))
+        {
+
+            $month_payment += $payments_row['amount'];
+            $tdate = $payments_row['process_date'];
+
+            $pdf->Cell(20, 5, date('F', strtotime($tdate)), 0, 0, 'L');
+            $pdf->Cell(30, 5, $payments_row['id']."-".$payments_row['payment_type_id'], 0, 0, 'L');
+            $pdf->Cell(80, 5, date('m-d-y', strtotime($tdate))." | Payment Received # ".$payments_row['document_num'], 0, 0, 'L');
+            $pdf->Cell(20, 5, " ", 0, 0, 'L');
+            $pdf->Cell(20, 5, "$ ".$payments_row['amount'], 0, 0, 'L');
+            $pdf->Cell(40, 5, "$ ".$month_payment, 0, 1, 'L');
+        }
+
+    }
+
+    $pdf->SetFont("Arial", "B", 10);
+    $pdf->Cell(20, 6, "", 0, 0);
+    $pdf->Cell(30, 6, "", 0, 0);
+    $pdf->Cell(80, 6, "Total", 0, 0);
+    $pdf->Cell(20, 6, "$ ".$total_charges, 0, 0);
+    $pdf->Cell(20, 6, "$ ".$total_payments, 0, 0);
+    $pdf->Cell(40, 6, "$ ".$total, 0, 1);
+    $pdf->SetFont("Arial", "", 10);
+
+
+    $pdf->Cell(189, 6, " ", 0, 1);
+
+
+    $pdf->SetFont("Arial", "B", 10);
+    $pdf->Cell(100, 6, "Note", 0, 1, L);
+    $pdf->SetFont("Arial", "", 10);
+
+
+    $pdf->Cell(100, 6, "BillPay Address : ", 0, 1, L);
+    $pdf->SetFont("Arial", "B", 10);
+    $pdf->Cell(100, 6, $c_name, 0, 1, L);
+    $pdf->SetFont("Arial", "", 10);
+    $pdf->Cell(100, 6, $pobox, 0, 1, L);
+    $pdf->Cell(100, 6, $city.", ".$state." ".$zip, 0, 1, L);
+    $pdf->Cell(100, 6, "EIN : ".$tax_id, 0, 1, L);
+
+
+    $pdf->Cell(189, 6, " ", 0, 1);
+    
+
+    $pdf->Cell(100, 6, "Send an email to ".$c_email." for HOA related queries", 0, 1, L);
+    $pdf->Cell(100, 6, "All updates will be posted at ".$c_website, 0, 1, L);
+
+
+    $pdf->output();
+
+
 ?>
-
-
