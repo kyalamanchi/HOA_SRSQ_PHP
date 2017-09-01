db32e897df10d9e0e0c536d0a4b62cc123582a74
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-01 17:04:53 +0530

    inspections

--- var/www/html/residentViolationCitation.php
+++ var/www/html/residentViolationCitation.php
@@ -401,15 +401,15 @@
 
                                       <br>
                                         
-                                      <button type='submit' name='submit' id='submit' class='btn btn-success btn-xs'><i class='fa fa-check'></i> Send to Board</button>
+                                      <button type='submit' name='submit' id='submit' class='btn btn-success btn-xs'>Send to Board</button>
 
-                                      <button type='submit' name='submit' id='submit' class='btn btn-success btn-xs'><i class='fa fa-check'></i> Send to Vendor</button>
+                                      <button type='submit' name='submit' id='submit' class='btn btn-success btn-xs'>Send to Vendor</button>
 
-                                      <button type='submit' name='submit' id='submit' class='btn btn-success btn-xs'><i class='fa fa-check'></i> Send to Board & Vendor</button>
+                                      <button type='submit' name='submit' id='submit' class='btn btn-success btn-xs'>Send to Board & Vendor</button>
 
                                       <br><br>
 
-                                      <button type='button' class='btn btn-warning btn-xs' data-dismiss='modal'><i class='fa fa-close'></i> Cancel</button>
+                                      <button type='button' class='btn btn-warning btn-xs' data-dismiss='modal'>Cancel</button>
 
                                     </div>
 
