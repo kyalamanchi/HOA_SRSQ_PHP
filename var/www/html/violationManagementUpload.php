e5099a7c18535a90962d8fb8f6b2dff13112b75d
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-01 16:04:38 +0530

    script

--- var/www/html/violationManagementUpload.php
+++ var/www/html/violationManagementUpload.php
@@ -44,7 +44,7 @@
 
 		$date = date('Y-m-d');
 
-		echo $inspection_date." - - - ".$description." - - - ".$home_id." - - - ".$date." - - - ($location) ".$location_id." - - - ($category) ".$category_id." - - - ($status) ".$inspection_status_id." - - - ".$compliance_date." - - - ".$item."<br>";
+		echo $inspection_date." - - - ".$description." - - - ".$home_id." - - - ".$date." - - - ".$location_id." - - - ".$category_id." - - - ".$inspection_status_id." - - - ".$compliance_date." - - - ".$item."<br>";
 
 		$result = pg_query("INSERT INTO inspection_notices (inspection_date, description, community_id, home_id, date_of_upload, location_id, inspection_category_id, hoa_id, inspection_status_id, compliance_date, updated_date, updated_by, item) VALUES ('$inspection_date', '$description', 2, $home_id, '$date', $location_id, $category_id, $hoa_id, $inspection_status_id, '$compliance_date', '$date', 258, '$item'");
 
