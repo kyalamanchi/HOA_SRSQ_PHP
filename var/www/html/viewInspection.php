496bd0be4e3a660aaca992bf05606546ac639a63
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-04 01:39:17 +0530

    #4

--- var/www/html/viewInspection.php
+++ var/www/html/viewInspection.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
         $cityQuery = "SELECT * FROM CITY";
         $cityQueryResult = pg_query($cityQuery);
