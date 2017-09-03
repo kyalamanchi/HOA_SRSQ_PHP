4a4203f2a9f532322c91af57f88543da82d2d6cb
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-03 10:12:48 +0530

    fileUpload

---
+++ var/www/html/file.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>File Upload</title>
+</head>
+<body>
+<form>
+	<input type="file" name='' id='' placeholder='Upload File' required />
+
+	<input type="submit" name="submit" />
+</form>
+</body>
+</html>
