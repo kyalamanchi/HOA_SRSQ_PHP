7608f0f2436a02395f1509edf252e16879cd412f
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-03 13:46:22 +0530

    file

--- var/www/html/file.php
+++ var/www/html/file.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>File Upload</title>
-</head>
-<body>
-<form>
-	<input type="file" name='' id='' placeholder='Upload File' required />
+	<head>
+		<title>File Upload</title>
+	</head>
+	<body>
+		<form action="uploadFile.php" method="POST" enctype="multipart/form-data">
+			<label>File : </label>
+			<input type="file" name='upload' id='upload' required />
 
-	<input type="submit" name="submit" />
-</form>
-</body>
+			<input type="submit" name="submit" />
+		</form>
+	</body>
 </html>
--- var/www/html/uploadFile.php
+++ var/www/html/uploadFile.php
@@ -1,5 +1,10 @@
 <?php
 
-	$file = 
+	$name = $_FILES['upload']['name'];
+	$temp = $_FILES['upload']['tmp_name'];
+	$type = $_FILES['upload']['type'];
+	$size = $_FILES['upload']['size'];
+
+	echo $name." - - - ".$temp." - - - ".$type." - - - ".$size;
 
 ?>
