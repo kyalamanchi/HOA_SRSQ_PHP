6edd170673c31938968ac9995fa998acedb99ff8
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-03 10:15:17 +0530

    14

--- var/www/html/viewInspection.php
+++ var/www/html/viewInspection.php
@@ -14,8 +14,8 @@
 date_default_timezone_set('America/Los_Angeles');
 $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
 try{
-    unlink('data.tab');
-    unlink('data.pdf');
+    // unlink('data.tab');
+    // unlink('data.pdf');
 if ( $connection ){
     $inspectionNoticeQuery= "SELECT * FROM INSPECTION_NOTICES WHERE ID=".$_GET['id'];
     $inspectionNoticeQueryResult = pg_query($inspectionNoticeQuery);
