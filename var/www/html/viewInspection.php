133027cbb171ff9863c958522557a7ef74d9d083
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-04 01:33:04 +0530

    #3

--- var/www/html/viewInspection.php
+++ var/www/html/viewInspection.php
@@ -4,9 +4,8 @@
     <title></title>
 </head>
 <body>
-
-
-<?php    
+<?php 
+    unlink('data.pdf');
     $connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
         $cityQuery = "SELECT * FROM CITY";
         $cityQueryResult = pg_query($cityQuery);
@@ -137,8 +136,7 @@ $pdf->WriteHTML("<br><b>This violation specifically regards the following item(s
 $pdf->Ln();
 $pdf->WriteHTML('<br>If you have already corrected the issue noted above, please disregard this courtesy notice, since no further action is required.<br><br>Thank you for your cooperation in maintaining the appearance and value of '.$communityLegalName.'. If you have any questions, please contact us via our Resident Portal at <a href="https://hoaboardtime.com">https://hoaboardtime.com</a><br><br>'.$communityLegalName);
 $pdf->Rect($pdf->w,$pdf->h,100,1);
-$pdf->Output('data.pdf','F');
+$pdf->Output();
 ?>
-<embed src="data.pdf" width="100%" height="750" />
 </body>
 </html>
