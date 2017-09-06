3739d3e5cb07cdc9ff0f4049d77221b7124f223e
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-06 13:03:18 +0530

    changes

--- var/www/html/mailChimpTesting.php
+++ var/www/html/mailChimpTesting.php
@@ -2,7 +2,7 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	$ch = curl_init('https://us12.api.mailchimp.com/3.0/lists/09692e90bd/');
+	$ch = curl_init('https://us12.api.mailchimp.com/3.0/lists/09692e90bd');
 
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey af5b50b9f714f9c2cb81b91281b84218-us12'));
@@ -11,9 +11,8 @@
     $result = curl_exec($ch);
     $json_decode = json_decode($result,TRUE);
 
-    #
-    print_r($json_decode);
-    die();
+    #print_r($json_decode);
+
     $total = sizeof($json_decode['members']);
 
     echo "<table border=2>";
