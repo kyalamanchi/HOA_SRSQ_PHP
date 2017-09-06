ac7e0927e78c0cc781763375860dd171faf2f397
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-06 13:00:23 +0530

    changes

--- var/www/html/mailChimpTesting.php
+++ var/www/html/mailChimpTesting.php
@@ -2,7 +2,7 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	$ch = curl_init('https://us12.api.mailchimp.com/3.0/lists/09692e90bd/subscribers');
+	$ch = curl_init('https://us12.api.mailchimp.com/3.0/lists/09692e90bd/');
 
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey af5b50b9f714f9c2cb81b91281b84218-us12'));
