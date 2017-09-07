f349b604aa80372e15b6b1ee977ec0038c39ad62
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-07 21:41:33 +0530

    testing

---
+++ var/www/html/dropboxTesting.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Unprocessed HOA Docs</title>
+</head>
+<body>
+
+	<br><br>
+
+	<center><h3>UNPROCESSED HOA DOCS</h3></center>
+
+	<br><br>
+
+	<?php
+
+		$cheaders = array('Authorization: Bearer QwUjEm5GAkAAAAAAAAAADocHK4CgCJoBl2A8-fe9Fs42E06qkDqJA2S9YPwGbZyF',
+                    'Content-Type: application/octet-stream',
+                    'Dropbox-API-Arg: {"path":"/'.$path.'", "mode":"add"}');
+
+		$ch = curl_init('https://content.dropboxapi.com/2/files/upload');
+		
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $cheaders);
+		curl_setopt($ch, CURLOPT_PUT, true);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+		curl_setopt($ch, CURLOPT_INFILE, $fp);
+		curl_setopt($ch, CURLOPT_INFILESIZE, $size);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		$response = curl_exec($ch);
+
+		echo $response;
+
+	    $json_decode = json_decode($response,TRUE);
+
+	?>
+
+</body>
+</html>
