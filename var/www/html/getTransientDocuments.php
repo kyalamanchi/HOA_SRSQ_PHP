50ec6a6da9d220ece4a1bea6b3f32708328263a9
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-07 14:36:43 +0530

    #19

---
+++ var/www/html/getTransientDocuments.php
@@ -0,0 +1,11 @@
+<?php
+$data = file_get_contents('php://input');
+$connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
+$hoaidquery = "SELECT * FROM community_transient_documents WHERE community_id=2";
+$query = pg_query($hoaidquery);
+$libraryDocuments = array();
+while ($row = pg_fetch_assoc($query)) {
+	array_push($libraryDocuments, $row['document_name']);
+}
+echo json_encode($libraryDocuments);
+?>
