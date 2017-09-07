2d18d7d7dae49784fdc4a37668e82f8b6b5bc160
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-07 17:17:49 +0530

    #22

--- var/www/html/agreement.php
+++ var/www/html/agreement.php
@@ -346,7 +346,7 @@ updateName();
         <input type="checkbox" id="enablePassword" onclick="calc();" disabled="disabled">
         <span class="slider round"></span>
       </label>
-      <input type="password" class="form-control" id="authPassword" aria-describedby="passwordhelp" placeholder="Enter password" disabled="disabled" style="width: 35%">
+      <input type="password" class="form-control" id="authPassword" aria-describedby="passwordhelp" placeholder="" disabled="disabled" style="width: 35%">
       <small id="passwordhelp" class="form-text text-muted">Signer needs to enter this password berfore signing</small>
       </div>
       <div style="clear: both;"></div>
