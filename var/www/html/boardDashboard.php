8916e25a7342686e4c04b25d6d4d3cd218b48eb5
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-10 10:09:40 +0530

    icons

--- var/www/html/boardDashboard.php
+++ var/www/html/boardDashboard.php
@@ -897,7 +897,7 @@
 
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
 
-                      <img src="inspections.png" height=75 width=75 alt='Inspection Notices'>
+                      <img src="pending_payments.png" height=75 width=75 alt='Inspection Notices'>
 
                     </div>
 
