9227663cfb2f13e9de731f25e5908b74d2f18990
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-12 05:18:02 +0530

    #7

--- var/www/html/modifyForteCustomerSRSQ.php
+++ var/www/html/modifyForteCustomerSRSQ.php
@@ -50,7 +50,7 @@ function hidePleaseWait() {
         $result = curl_exec($ch);
         $jsonResult = json_decode($result);
         echo '<h4 style="float:left;">'.$jsonResult->display_name.'</h2>';
-        echo '<h4 style="float:right;">Last Updated Date: '.$jsonResult->updated_date.'</h2>';
+        echo '<h4 style="float:right;">Last Updated Date: '.date('Y-m-d',strtotime($jsonResult->updated_date)).'</h2>';
         echo '<hr style="clear:both;">';
         echo '<div class="form-group">';
         echo '<label for="fname">First Name:</label>';
