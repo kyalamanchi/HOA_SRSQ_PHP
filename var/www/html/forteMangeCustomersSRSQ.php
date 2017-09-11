459f3c45782ddd4329123b336901a5db0486718b
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-12 02:49:19 +0530

    Forte Customers

--- var/www/html/forteMangeCustomersSRSQ.php
+++ var/www/html/forteMangeCustomersSRSQ.php
@@ -10,8 +10,6 @@
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
-  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script> -->
   <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
     <style>
 
@@ -163,7 +161,7 @@ function modifyCustomer(obj){
   <div class="container">
     <div class="row">
       <h2 style="text-align: left;float: left;">Forte Manage Customers</h2>
-      <h2 style="text-align: right;float: right;">Forte Manage Customers</h2>
+      <h2 style="text-align: right;float: right;">SRSQ</h2>
       <hr style="clear: both;">
     </div>
     <div>
