45dd5df1cb76515d289cb168317f5c4922386cfc
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-12 15:36:13 +0530

    #2

--- var/www/html/updateHomePayMethodSRP.php
+++ var/www/html/updateHomePayMethodSRP.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/Los_Angeles');
 $connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
 
-$homeidquery = "SELECT * FROM hoaid";
+$homeidquery = "SELECT * FROM hoaid WHERE community_id = 1";
 $homeresult = pg_query($homeidquery);
 $homeIDSARRAY = array();
 
