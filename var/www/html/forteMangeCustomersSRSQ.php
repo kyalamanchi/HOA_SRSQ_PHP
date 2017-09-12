c2c4bb0a1b555478f92edc3ddf68effe00e6c5e0
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-13 03:16:34 +0530

    #3

--- var/www/html/forteMangeCustomersSRSQ.php
+++ var/www/html/forteMangeCustomersSRSQ.php
@@ -70,7 +70,7 @@ function createCustomer(){
   <hr>
 </div>
 <div class="container"> 
-<button type="button" class="btn btn-primary" onclick="createCustomer();" style="float: left;">Create Customer</button>
+<button type="button" class="btn btn-primary" onclick="createCustomer();" style="float: right;">Create Customer</button>
 <br style="clear: both;">
         <table id="example" class="table table-striped" cellspacing="0" width="100%" >
                                 <thead>
