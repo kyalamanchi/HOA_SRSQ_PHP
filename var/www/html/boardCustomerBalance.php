e9cc54b3b38b366f0f40daa18e7c4b5c44a3b1e1
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-13 19:39:22 +0530

    changes

--- var/www/html/boardCustomerBalance.php
+++ var/www/html/boardCustomerBalance.php
@@ -497,14 +497,14 @@
 
                                       <div class='row container-fluid'>
 
-                                        <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 date'>
+                                        <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
 
                                           <label>Open Date</label>
                                           <input class='form-control' type='date' name='edit_reminder_open_date' id='edit_reminder_open_date' value='$o_date' readonly>
 
                                         </div>
 
-                                        <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 date'>
+                                        <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
 
                                           <label>Due Date</label>
                                           <input class='form-control' type='date' name='edit_reminder_due_date' id='edit_reminder_due_date' value='$d_date' required>
