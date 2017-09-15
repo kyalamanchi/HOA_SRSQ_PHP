c623edc58693c7bb4fca385ff0aa79e32e4bbd86
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-15 16:14:05 +0530

    reminders

--- var/www/html/boardSendPendingPaymentReminder2.php
+++ var/www/html/boardSendPendingPaymentReminder2.php
@@ -1,7 +1,11 @@
 <?php
 
+$mes = $_POST['message'];
+
 echo $_POST['no_of_recipients']."<br><br>";
-echo $_POST['message']."<br><br><br><br>";
+echo $mes."<br><br><br><br>";
+
+nl2br($mes."\n"."Ravindra");
 
 for($i = 0; $i < $_POST['no_of_recipients']; $i++)
 	echo $_POST['home_id_'.$i]."<br>";
