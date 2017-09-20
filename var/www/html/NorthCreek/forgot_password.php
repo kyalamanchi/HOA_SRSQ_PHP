3d36c2427281101540c4fea8d2364f7b8c512465
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-20 07:46:29 +0530

    Core

---
+++ var/www/html/NorthCreek/forgot_password.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<meta charset='UTF-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<meta name='description' content=''>
+		<meta name='author' content=''>
+		
+		<title>The Reserve at North Creek</title>
+		
+		<!-- Bootstrap core CSS-->
+		<link href='assets/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+		<!-- Icon Fonts-->
+		<link href='assets/css/font-awesome.min.css' rel='stylesheet'>
+		<link href='assets/css/linea-arrows.css' rel='stylesheet'>
+		<link href='assets/css/linea-icons.css' rel='stylesheet'>
+		<!-- Template core CSS-->
+		<link href='assets/css/template.min.css' rel='stylesheet'>
+	</head>
+	<body>
+
+		<!-- Layout-->
+		<div class='layout'>
+
+			<!-- Header-->
+			<?php include "home_header.php"; ?>
+
+			<!-- Wrapper-->
+			<div class='wrapper'>
+
+				<section class='module'>
+					
+					<div class='container'>
+						
+						<div class='row'>
+							
+							<div class='col-md-4 offset-md-4'>
+								
+								<div class='up-logo'>
+									
+									<p class='text-center m-b-50'><img src='assets/images/logo.png' width='100' alt='Logo'></p>
+								
+								</div>
+								
+								<div class='up-form'>
+									
+									<form method='post' action=''>
+										
+										<div class='form-group'>
+											
+											<input class='form-control form-control-lg' type='email' name='reset_email' id='reset_email' placeholder='Email' required>
+
+										</div>
+
+										<div class='form-group'>
+
+											<button class='btn btn-block btn-lg btn-round btn-brand' type='submit'>Reset password</button>
+
+										</div>
+
+									</form>
+
+								</div>
+
+								<div class='up-help'><a href='login_page.php'>Back to Sign In page</a></div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<!-- Footer -->
+				<?php include "footer.php"; ?>
+
+				<a class='scroll-top' href='#top'><i class='fa fa-angle-up'></i></a>
+
+			</div>
+			<!-- Wrapper end-->
+
+		</div>
+		<!-- Layout end-->
+
+		<!-- Scripts-->
+		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.1.1/js/tether.min.js'></script>
+		<script src='assets/bootstrap/js/bootstrap.min.js'></script>
+		<script src='assets/js/plugins.min.js'></script>
+		<script src='assets/js/custom.min.js'></script>
+
+		<!-- Color Switcher (Remove these lines)-->
+		<!--script src='assets/js/style-switcher.min.js'></script-->
+
+	</body>
+
+</html>
---
+++ var/www/html/NorthCreek/home_header.php
@@ -0,0 +1,62 @@
+
+<header class='header header-center undefined'>
+
+		<div class='container-fluid'>
+
+			<!-- Logos-->
+			<div class='inner-header'>
+
+			<a class='inner-brand' href='index.php'>
+
+				<img class='brand-dark' src='assets/images/logo.png' width='77px' alt=''>
+				<img class='brand-light' src='assets/images/logo-light.png' width='77px' alt=''>
+				<!-- Or simple text-->
+				<!-- Core-->
+
+			</a>
+
+		</div>
+
+		<!-- Navigation-->
+		<div class='inner-navigation collapse'>
+
+			<div class='inner-navigation-inline'>
+
+				<div class='inner-nav'>
+
+					<ul>
+									
+						<!-- Home -->
+						<li><a href='index.php'>Home</a></li>
+
+						<!-- About Us -->
+						<li><a href='about_us.php'>About Us</a></li>
+									
+						<!-- Contact Us -->
+						<li><a href='contact_us.php'>Contact Us</a></li>
+
+						<!-- Sign In -->
+						<li><a href='login_page.php'>Sign In / Sign Up</a></li>
+
+					</ul>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<!-- Mobile menu-->
+		<div class='nav-toggle'>
+						
+			<a href='#' data-toggle='collapse' data-target='.inner-navigation'>
+
+				<span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span>
+
+			</a>
+
+		</div>
+
+	</div>
+
+</header>
