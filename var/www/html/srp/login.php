bae34f8366a0d42caa850b506b6de82c1f4c0d62
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 02:19:48 +0530

    changes

--- var/www/html/srp/login.php
+++ var/www/html/srp/login.php
@@ -47,7 +47,7 @@
 			else
 			{
 
-				echo "<br /><br /><br /><br /><br /><br /><br /><br /><div class='row'><div class='col-xl-3 col-lg-3 col-md-2 col-sm-1 col-xs-1'> </div><div class='col-xl-6 col-lg-6 col-md-8 col-sm-10 col-xs-10'><div class='alert alert-danger'><center><br /><strong style='font-size: 15pt;'>Invalid user!</strong><br /><br />Please check the email and try again.<br /></center></div></div></div>";
+				echo "<br /><br /><br /><br /><br /><br /><br /><br /><div class='row'><div class='col-xl-3 col-lg-3 col-md-2 col-sm-1 col-xs-1'> </div><div class='col-xl-6 col-lg-6 col-md-8 col-sm-10 col-xs-10'><div class='alert alert-danger'><center><br /><strong style='font-size: 15pt;'>Invalid user!</strong><br /><br />Please check the email and try again.<br /><br /></center></div></div></div>";
 
 			}
 
