ab313ee0068e6e7a4c0547b2ce415552187116a5
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 01:48:43 +0530

    password

--- var/www/html/srp/login.php
+++ var/www/html/srp/login.php
@@ -1,5 +1,9 @@
 <?php
 
+	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
+	include 'password.php';
+
 	$login_email = $_POST['login_email'];
 	$login_password = $_POST['login_password'];
 
