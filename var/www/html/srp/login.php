509514f796334aff16a3a5615b681323d043bfa0
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 01:57:53 +0530

    chnages

--- var/www/html/srp/login.php
+++ var/www/html/srp/login.php
@@ -5,34 +5,60 @@
 
 		<title>Log In Page</title>
 
+		<!-- Web Fonts-->
+		<link href='https://fonts.googleapis.com/css?family=Poppins:500,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Hind:400,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Lora:400i' rel='stylesheet'>
+		<!-- Bootstrap core CSS-->
+		<link href='assets/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+		<!-- Icon Fonts-->
+		<link href='assets/css/font-awesome.min.css' rel='stylesheet'>
+		<link href='assets/css/linea-arrows.css' rel='stylesheet'>
+		<link href='assets/css/linea-icons.css' rel='stylesheet'>
+		<!-- Plugins-->
+		<link href='assets/css/magnific-popup.css' rel='stylesheet'>
+		<link href='assets/css/vertical.min.css' rel='stylesheet'>
+		<link href='assets/css/pace-theme-minimal.css' rel='stylesheet'>
+		<link href='assets/css/animate.css' rel='stylesheet'>
+		<!-- Template core CSS-->
+
 	</head>
 
 	<body>
 
 		<?php
 
-	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+			pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	include 'password.php';
+			include 'password.php';
 
-	$login_email = $_POST['login_email'];
-	$login_password = $_POST['login_password'];
+			$login_email = $_POST['login_email'];
+			$login_password = $_POST['login_password'];
 
-	$result = pg_query("SELECT * FROM usr WHERE email='$login_email'");
+			$result = pg_query("SELECT * FROM usr WHERE email='$login_email'");
 
-	$users = pg_num_rows($result);
+			$users = pg_num_rows($result);
 
-	if($users)
-	{
+			if($users)
+			{
+				echo "user found";
+			}
+			else
+			{
 
-	}
-	else
-	{
+				echo "<div class='alert alert-danger'><strong>Invalid user!</strong> Please check the email and try again.</div>";
 
-		echo "<div class='alert alert-danger'><strong>Invalid user!</strong> Please check the email and try again.</div>";
-	}
+			}
 
-?>
+		?>
+
+		<!-- Scripts-->
+		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.1.1/js/tether.min.js'></script>
+		<script src='assets/bootstrap/js/bootstrap.min.js'></script>
+		<script src='http://maps.googleapis.com/maps/api/js?key=AIzaSyA0rANX07hh6ASNKdBr4mZH0KZSqbHYc3Q'></script>
+		<script src='assets/js/plugins.min.js'></script>
+		<script src='assets/js/custom.min.js'></script>
 
 	</body>
 
