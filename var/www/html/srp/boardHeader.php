dc7b2cfd1b102b05fe9c6862ff43c2f8d0976e74
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 17:34:36 +0530

    srp

---
+++ var/www/html/srp/boardHeader.php
@@ -0,0 +1,59 @@
+<header class='header header-center undefined'>
+
+	<div class='container-fluid'>
+
+		<div class='inner-header'>
+						
+			<a class='inner-brand'><h5 style='color: green;'><?php echo $_SESSION['hoa_community_name']; ?></h5></a>
+
+		</div>
+
+		<div class='inner-navigation collapse'>
+
+			<div class='inner-navigation-inline'>
+
+				<div class='inner-nav'>
+
+					<ul>
+
+						<li><a href='boardDashboard.php'>Home</a></li>
+						<li><a class='smoothscroll' href='#pay_online'>Pay Online</a></li>
+						<li><a class='smoothscroll' href='#budget'>2017 Budget</a></li>
+						<li><a class='smoothscroll' href='#r_p'>Rule &amp; Policies</a></li>
+						<li><a class='smoothscroll' href='#contact'>Contact Us</a></li>
+						<li><a style='color: green;' href='residentDashboard.php'>Resident Dashboard</a></li>
+
+					</ul>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="extra-nav">
+						
+			<ul>
+				
+				<li><a href="https://hoaboardtime.com/srp/logout.php"><span>Log Out</span></a></li>
+						
+			</ul>
+
+		</div>
+
+		<!-- Mobile menu-->
+		<div class='nav-toggle'>
+						
+			<a href='#' data-toggle='collapse' data-target='.inner-navigation'>
+							
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+
+			</a>
+
+		</div>
+
+	</div>
+
+</header>
---
+++ var/www/html/srp/residentHeader.php
@@ -0,0 +1,74 @@
+<?php
+
+	session_start();
+
+	$user_id = $_SESSION['hoa_user_id'];
+
+	$board = pg_num_rows(pg_query("SELECT * FROM board_committee_details WHERE user_id=$user_id"));
+
+?>
+
+<header class='header header-center undefined'>
+
+	<div class='container-fluid'>
+
+		<div class='inner-header'>
+						
+			<a class='inner-brand'><h5 style='color: green;'><?php echo $_SESSION['hoa_community_name']; ?></h5></a>
+
+		</div>
+
+		<div class='inner-navigation collapse'>
+
+			<div class='inner-navigation-inline'>
+
+				<div class='inner-nav'>
+
+					<ul>
+
+						<li><a href='index.php'>Home</a></li>
+						<li><a class='smoothscroll' href='#pay_online'>Pay Online</a></li>
+						<li><a class='smoothscroll' href='#budget'>2017 Budget</a></li>
+						<li><a class='smoothscroll' href='#r_p'>Rule &amp; Policies</a></li>
+						<li><a class='smoothscroll' href='#contact'>Contact Us</a></li>
+						<?php
+
+							if($board != 0)
+								echo "<li><a style='color: green;' href='boardDashboard.php'>Board Dashboard</a></li>";
+
+						?>
+
+					</ul>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="extra-nav">
+						
+			<ul>
+				
+				<li><a href="https://hoaboardtime.com/srp/logout.php"><span>Log Out</span></a></li>
+						
+			</ul>
+
+		</div>
+
+		<!-- Mobile menu-->
+		<div class='nav-toggle'>
+						
+			<a href='#' data-toggle='collapse' data-target='.inner-navigation'>
+							
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+				<span class='icon-bar'></span>
+
+			</a>
+
+		</div>
+
+	</div>
+
+</header>
