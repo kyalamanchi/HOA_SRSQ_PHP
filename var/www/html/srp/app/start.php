4dcfb7b5fa8af70750148756206b5f3ec7e78172
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-22 02:16:32 +0530

    documents

---
+++ var/www/html/srp/app/start.php
@@ -0,0 +1,28 @@
+<?php 
+	
+	session_start();
+
+	$_SESSION['hoa_hoa_id'] = 1259;
+
+	require __DIR__.'/../vendor/autoload.php';
+
+	if($_SESSION['hoa_community_id'] == 1)
+	{
+		$dropboxKey = 'nlwoqs7bfr5tiwc';
+		$dropboxSecret = '6l9cjhz6cw7ocy8';
+		$appName = 'SRP';
+	}
+	else if($_SESSION['hoa_community_id'] == 2)
+	{
+		$dropboxKey = '0t0ibm426a3zwhj';
+		$dropboxSecret = 'vuxox96m6rts0lz';
+		$appName = 'SRSQ';
+	}
+
+	$appInfo = new Dropbox\AppInfo($dropboxKey, $dropboxSecret);
+
+	$csrfTokenStore = new Dropbox\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
+
+	$webAuth = new Dropbox\WebAuth($appInfo, $appName, 'http://localhost/dropbox/dropbox_finish.php', $csrfTokenStore);
+
+	
