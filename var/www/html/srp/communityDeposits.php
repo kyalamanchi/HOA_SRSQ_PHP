e92b4331f3e5d78172d304e13547fc1a4ec824c9
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 23:25:15 +0530

    changes

--- var/www/html/srp/communityDeposits.php
+++ var/www/html/srp/communityDeposits.php
@@ -73,9 +73,9 @@
 						
 						</div>
 
-						<div class='module module-white row container'>
+						<div class='module module-white'>
 
-							<table id='example1' class='table table-bordered table-striped container' style='width: 100%;'>
+							<table id='example1' class='table table-bordered table-striped' style='width: 100%;'>
 								
 								<thead>
 									
