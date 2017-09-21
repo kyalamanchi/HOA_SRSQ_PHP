7bbb3265afcc9991edf06fcd213d456446d61d06
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 19:00:46 +0530

    tabs

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -79,10 +79,10 @@
 						<!-- Tabs-->
 						<ul class="nav nav-tabs">
 									
-							<li class="nav-item"><a class="nav-link active" href="#tab-1" data-toggle="tab"><i class="fa fa-anchor"></i> Board Dashboard</a></li>
-							<li class="nav-item"><a class="nav-link" href="#tab-2" data-toggle="tab"><i class="fa fa-area-chart"></i> Communications Dashboard</a></li>
+							<li class="nav-item"><a class="nav-link active" href="#tab-1" data-toggle="tab"><i class="fa fa-dashboard"></i> Board Dashboard</a></li>
+							<li class="nav-item"><a class="nav-link" href="#tab-2" data-toggle="tab"><i class="fa fa-envelope"></i> Communications Dashboard</a></li>
 							<li class="nav-item"><a class="nav-link" href="#tab-3" data-toggle="tab"><i class="fa fa-support"></i> Reserves Dashboard</a></li>
-							<li class="nav-item"><a class="nav-link" href="#tab-4" data-toggle="tab"><i class="fa fa-support"></i> Quickbooks Reports</a></li>
+							<li class="nav-item"><a class="nav-link" href="#tab-4" data-toggle="tab"><i class="fa fa-dollar"></i> Quickbooks Reports</a></li>
 							<li class="nav-item"><a class="nav-link" href="#tab-5" data-toggle="tab"><i class="fa fa-support"></i> Yearly Reports - <?php echo date('Y'); ?></a></li>
 						
 						</ul>
