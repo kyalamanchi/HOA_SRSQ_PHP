ca5185a5af119c325203a246c09223d2beb20aff
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 11:07:57 +0530

    changes

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -8,6 +8,9 @@
 
 			session_start();
 
+			if(!$_SESSION['hoa_username'])
+				header("Location: logout.php");
+
 			pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
 			$community_id = 1;
