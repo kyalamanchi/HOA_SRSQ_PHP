4097d896f7cc6636d120715c10d7b335972b033d
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 22:19:06 +0530

    changes

---
+++ var/www/html/srp/communityDeposits.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+
+	<head>
+
+		<?php
+
+			session_start();
+
+			ini_set("session.gc_maxlifetime", 1000000000);
+			ini_set("session.cache_expire", 1000000000);
+
+			if(!$_SESSION['hoa_username'])
+				header("Location: logout.php");
+
+			pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
+			$user_id = $_SESSION['hoa_user_id'];
+			$board = pg_num_rows(pg_query("SELECT * FROM board_committee_details WHERE user_id=$user_id"));
+
+			if($board == 0)
+				header("Location: residentDashboard.php");
+
+			$community_id = $_SESSION['hoa_community_id'];
+			$days90 = date('Y-m-d', strtotime("-90 days"));
+
+			$res_dir = pg_num_rows(pg_query("SELECT * FROM member_info WHERE community_id=$community_id"));
+			$email_homes = pg_num_rows(pg_query("SELECT * FROM hoaid WHERE email!='' AND community_id=$community_id"));
+			$total_homes = pg_num_rows(pg_query("SELECT * FROM homeid WHERE community_id=$community_id"));
+			$tenants = pg_num_rows(pg_query("SELECT * FROM home_mailing_address WHERE community_id=$community_id"));
+			$newly_moved_in = pg_num_rows(pg_query("SELECT * FROM hoaid WHERE community_id=$community_id AND valid_from>='".$days90."' AND valid_from<='".date('Y-m-d')."'"));
+
+		?>
+
+		<meta charset='UTF-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<meta name='description' content='Stoneridge Place At Pleasanton HOA'>
+		<meta name='author' content='Geeth'>
+
+		<title><?php echo $_SESSION['hoa_community_code']; ?> | Board Dashboard</title>
+
+		<!-- Web Fonts-->
+		<link href='https://fonts.googleapis.com/css?family=Poppins:500,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Hind:400,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Lora:400i' rel='stylesheet'>
+		<!-- Bootstrap core CSS-->
+		<link href='assets/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+		<!-- Icon Fonts-->
+		<link href='assets/css/font-awesome.min.css' rel='stylesheet'>
+		<link href='assets/css/linea-arrows.css' rel='stylesheet'>
+		<link href='assets/css/linea-icons.css' rel='stylesheet'>
+		<!-- Plugins-->
+		<link href='assets/css/magnific-popup.css' rel='stylesheet'>
+		<link href='assets/css/vertical.min.css' rel='stylesheet'>
+		<link href='assets/css/pace-theme-minimal.css' rel='stylesheet'>
+		<link href='assets/css/animate.css' rel='stylesheet'>
+		<!-- Template core CSS-->
+		<link href='assets/css/template.min.css' rel='stylesheet'>
+
+	</head>
+
+	<body>
+
+		<!-- Layout-->
+		<div class='layout' style='background-color: blue;'>
+
+			<!-- Header-->
+			<?php include "boardHeader.php"; ?>
+
+			<!-- Wrapper-->
+			<div class='wrapper'>
+
+				<!-- Counters -->
+				<section class='module module-gray p-b-0'>
+
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+						<div class="special-heading m-b-40">
+									
+							<h4><i class="fa fa-dashboard"></i> Board Dashboard</h4>
+						
+						</div>
+					
+					</div>
+
+					<br /><br /><br />
+
+				</section>
+
+				<!-- Footer-->
+				<?php include "footer.php"; ?>
+
+				<a class='scroll-top' href='#top'><i class='fa fa-angle-up'></i></a>
+
+			</div>
+			<!-- Wrapper end-->
+
+		</div>
+		<!-- Layout end-->
+
+		<!-- Scripts-->
+		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.1.1/js/tether.min.js'></script>
+		<script src='assets/bootstrap/js/bootstrap.min.js'></script>
+		<script src='http://maps.googleapis.com/maps/api/js?key=AIzaSyA0rANX07hh6ASNKdBr4mZH0KZSqbHYc3Q'></script>
+		<script src='assets/js/plugins.min.js'></script>
+		<script src='assets/js/custom.min.js'></script>
+
+		<!-- Color Switcher (Remove these lines)-->
+		<!--script src='assets/js/style-switcher.min.js'></script-->
+	</body>
+
+</html>
