128988aff40914a0472d3c8d16bb0a7292a2ae95
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 18:16:27 +0530

    header

--- var/www/html/srp/boardHeader.php
+++ var/www/html/srp/boardHeader.php
@@ -17,6 +17,85 @@
 					<ul>
 
 						<li><a href='boardDashboard.php'>Home</a></li>
+						<li class="menu-item-has-children menu-item-has-mega-menu"><a href="#">Home</a>
+							<div class="mega-menu">
+								<ul class="sub-menu mega-menu-row">
+									<!-- Column 1-->
+									<li class="menu-item-has-children mega-menu-col"><a href="#">Basic</a>
+										
+										<ul class="sub-menu">
+														
+											<li><a href="index.html">Parallax Dark</a></li>
+											<li><a href="index-2.html">Parallax Light</a></li>
+											<li><a href="index-3.html">Parallax Gradient</a></li>
+											<li><a href="index-4.html">Full Screen Slider</a></li>
+											<li><a href="index-5.html">Text Rotator</a></li>
+											<li><a href="index-23.html">Scale</a></li>
+											<li><a href="index-24.html">Scale & opacity</a></li>
+											<li><a href="index-27.html">Flexslider</a></li>
+											<li><a href="index-28.html">Ken burns Slider</a></li>
+											<li><a href="index-29.html">Ripples Full Screen</a></li>
+											<li><a href="index-31.html">Particles</a></li>
+												
+										</ul>
+
+									</li>
+
+									<!-- Column 2-->
+									<li class="menu-item-has-children mega-menu-col"><a href="#">Special</a>
+										
+										<ul class="sub-menu">
+														
+											<li><a href="index-18.html">One Page</a></li>
+											<li><a href="index-26.html">One Page 2</a></li>
+											<li><a href="index-21.html">Business</a></li>
+											<li><a href="index-22.html">Startup</a></li>
+											<li><a href="index-19.html">Portfolio Masonry</a></li>
+											<li><a href="index-20.html">Freelance Designer</a></li>
+											<li><a href="index-25.html">New Agency</a></li>
+										
+										</ul>
+
+									</li>
+
+									<!-- Column 3-->
+									<li class="menu-item-has-children mega-menu-col"><a href="#">Video</a>
+										
+										<ul class="sub-menu">
+														
+											<li><a href="index-7.html">Youtube Dark</a></li>
+											<li><a href="index-8.html">Youtube Light</a></li>
+											<li><a href="index-9.html">Youtube Gradient</a></li>
+											<li><a href="index-10.html">Vimeo Dark</a></li>
+											<li><a href="index-11.html">Vimeo Light</a></li>
+											<li><a href="index-12.html">Vimeo Gradient</a></li>
+											<li><a href="index-6.html">Local Video</a></li>
+										
+										</ul>
+										
+									</li>
+
+									<!-- Column 4-->
+									<li class="menu-item-has-children mega-menu-col"><a href="#">Small</a>
+										
+										<ul class="sub-menu">
+
+											<li><a href="index-13.html">Dark Small</a></li>
+											<li><a href="index-14.html">Light Small</a></li>
+											<li><a href="index-15.html">Gradient Small</a></li>
+											<li><a href="index-16.html">Youtube Small</a></li>
+											<li><a href="index-17.html">Vimeo Small</a></li>
+											<li><a href="index-30.html">Ripples</a></li>
+										
+										</ul>
+
+									</li>
+								
+								</ul>
+
+							</div>
+
+						</li>
 						<li><a style='color: green;' href='residentDashboard.php'>Resident Dashboard</a></li>
 
 					</ul>
