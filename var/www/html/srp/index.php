418fb877ad5e3f68efc274214cc624bf7a31e68f
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-21 11:04:13 +0530

    changes

--- var/www/html/srp/index.php
+++ var/www/html/srp/index.php
@@ -9,7 +9,7 @@
 			session_start();
 
 			if($_SESSION['hoa_username'])
-				echo "Location: logout.php";
+				header("Location: logout.php");
 
 			pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
--- var/www/html/srp/logout.php
+++ var/www/html/srp/logout.php
@@ -3,13 +3,13 @@
 	session_start();
 
 	$_SESSION = array();
-	
+
     session_unset();
     session_commit();
 	session_destroy();
 	session_write_close();
     session_regenerate_id(true);
 
-	echo "Location: https://hoaboardtime.com/srp/index.php";
+	header("Location: index.php");
 
 ?>
