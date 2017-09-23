866d5a8f853e70c1bc9a6f3dc9247f4d165ff53c
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-24 02:59:14 +0530

    Inspection Notices Update

---
+++ var/www/html/getInspectionData.php
@@ -0,0 +1,75 @@
+<?php
+$connection = pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
+
+if ( $connection){
+    $query = "SELECT   * FROM LOCATIONS_IN_COMMUNITY";
+    $queryRes = pg_query($query);
+    $locationsArray = array();
+    while ($row = pg_fetch_assoc($queryRes)) {
+        $locationsArray[$row['location_id']] = $row['location'];
+    }
+    $categoryArray  = array();
+
+    $query = "SELECT * FROM INSPECTION_CATEGORY";
+    $queryRes  = pg_query($query);
+
+    while ($row = pg_fetch_assoc($queryRes)) {
+        $categoryArray[$row['id']] = $row['name'];
+    }
+
+    $inspectionStatusArray = array();
+    $query = "SELECT * FROM INSPECTION_STATUS";
+    $queryRes  = pg_query($query);
+    while ($row = pg_fetch_assoc($queryRes)) {
+        $inspectionStatusArray[$row['id']] = $row['inspection_status'];
+    }
+    $inspectionSubCategoryArray  = array();
+    $query = "SELECT * FROM INSPECTION_SUB_CATEGORY";
+    $queryResult = pg_query($query);
+    while ($row = pg_fetch_assoc($queryResult)) {
+        $inspectionSubCategoryArray[$row['id']] = $row['name'];
+    }
+
+    if( $_GET['id']){
+        $query =  "SELECT * FROM INSPECTION_NOTICES WHERE INSPECTION_STATUS_ID=".$_GET['id'];
+        $queryResult = pg_query($query);
+        $sendData = array();
+        while ($row = pg_fetch_assoc($queryResult)) {
+            $data = array();
+            $data['id']  = $row['id'];
+            $data['inspection_date'] = $row['inspection_date'];
+            $data['description'] = $row['description'];
+            $data['community_id'] = $row['community_id'];
+            $data['home_id'] = $row['home_id'];
+            $data['location_id'] = $locationsArray[$row['location_id']];
+            $data['inspection_category_id'] = $categoryArray[$row['inspection_category_id']];
+            $data['inspection_sub_category_id'] = $inspectionSubCategoryArray[$row['inspection_sub_category_id']];
+            $data['hoa_id'] = $row['hoa_id'];
+            $data['inspection_notice_type_id'] = $row['inspection_notice_type_id'];
+            $data['inspection_status'] = $inspectionStatusArray[$row['inspection_status_id']];
+            array_push($sendData, $data);
+        }
+    }
+    else {
+        $query =  "SELECT * FROM INSPECTION_NOTICES";
+        $queryResult = pg_query($query);
+        $sendData = array();
+        while ($row = pg_fetch_assoc($queryResult)) {
+            $data = array();
+            $data['id']  = $row['id'];
+            $data['inspection_date'] = $row['inspection_date'];
+            $data['description'] = $row['description'];
+            $data['community_id'] = $row['community_id'];
+            $data['home_id'] = $row['home_id'];
+            $data['location_id'] = $locationsArray[$row['location_id']];
+            $data['inspection_category_id'] = $categoryArray[$row['inspection_category_id']];
+            $data['inspection_sub_category_id'] = $inspectionSubCategoryArray[$row['inspection_sub_category_id']];
+            $data['hoa_id'] = $row['hoa_id'];
+            $data['inspection_notice_type_id'] = $row['inspection_notice_type_id'];
+            $data['inspection_status'] = $inspectionStatusArray[$row['inspection_status_id']];
+            array_push($sendData, $data);
+        }
+    }
+}
+echo json_encode($sendData);
+?>
