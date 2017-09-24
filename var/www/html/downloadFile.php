7bb2f541b10e3b7bf7acd6d1f8fc750d79558837
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-25 01:30:38 +0530

    Update

--- var/www/html/downloadFile.php
+++ var/www/html/downloadFile.php
@@ -4,7 +4,7 @@ $file_url = $_GET['id'].'.zip';
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
-readfile($file_url)
+readfile($file_url);
 unlink($_GET['id'].'.zip');
 }
 else {
