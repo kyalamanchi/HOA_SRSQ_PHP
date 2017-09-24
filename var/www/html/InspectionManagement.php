bbf752f7ee5c9b641ea392c00d726c4c83dff57e
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-25 01:27:51 +0530

    Inspection Management Fix

--- var/www/html/InspectionManagement.php
+++ var/www/html/InspectionManagement.php
@@ -113,7 +113,7 @@ function mailStatement(docID){
     };
 }
 function generateForSouthData(docID){
-    var mailingInformation = docID.split(" ");
+    var mailingInformation = docID.id.split(" ");
     var pleaseWaitData = '<div class="progress">\
                       <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
                       aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
