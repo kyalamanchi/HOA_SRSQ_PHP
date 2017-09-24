362ad44edaed68505d1da8712e1eaa8b29f8f4d3
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-25 01:51:49 +0530

    Updates

--- var/www/html/InspectionManagement.php
+++ var/www/html/InspectionManagement.php
@@ -122,6 +122,8 @@ function generateForSouthData(docID){
                     </div>';
     $("#pleaseWaitDialog2").find('.modal-body').html(pleaseWaitData);
     var source = new EventSource("https://hoaboardtime.com/generateSouthData.php?id="+mailingInformation[1]+"&doc_id="+mailingInformation[0]);
+    alert(mailingInformation[1]);
+    alert(mailingInformation[0]);
     source.onmessage = function(event){
         $("#pleaseWaitDialog2").find('.modal-header').html('<h4 class="modal-title">'+event.data+'</h4>');
         if ( (event.data == "File will be downloaded shortly.")  ){
