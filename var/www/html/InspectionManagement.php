71a8e882f9472eb4d4811fbd85f2926e4b6e4aa3
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-24 22:26:04 +0530

    Inspection Management

--- var/www/html/InspectionManagement.php
+++ var/www/html/InspectionManagement.php
@@ -96,7 +96,13 @@ source.onmessage = function(event) {
 };
 }
 function mailStatement(docID){
-     $("#pleaseWaitDialog2").find('.modal-body').html('');
+    var pleaseWaitData = '<div class="progress">\
+                      <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
+                      aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
+                      </div>\
+                    </div>';
+    $("#pleaseWaitDialog2").find('.modal-body').html(pleaseWaitData);
+    
     var mailingInformation =  docID.id.split(" ");
     var source = new EventSource("https://hoaboardtime.com/sendInspectionNotice.php?id="+mailingInformation[1]+"&doc_id="+mailingInformation[0]);
     source.onmessage = function(event){
