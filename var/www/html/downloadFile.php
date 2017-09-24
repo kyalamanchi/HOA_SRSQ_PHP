b9ec4f02918ffa0ac309e9b2bbb91029bceaaee2
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-25 01:40:27 +0530

    Updates

--- var/www/html/downloadFile.php
+++ var/www/html/downloadFile.php
@@ -1,6 +1,10 @@
 <?php
 if ( $_GET['id'] ){
 $file_url = $_GET['id'].'.zip';
+if( !file_exists($file_url)){
+	echo "Cannot find file";
+	exit(0);
+}
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary"); 
 header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
