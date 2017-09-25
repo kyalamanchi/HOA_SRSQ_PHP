e656c75dd808108429a4d559676c60838512fc10
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-25 15:29:21 +0530

    charges

--- var/www/html/srp/processPayment2.php
+++ var/www/html/srp/processPayment2.php
@@ -96,20 +96,11 @@
 
 							<thead>
 								
-								<th>Name<br>(HOA ID)</th>
-								<th>Address<br>(Home ID)</th>
-								<th>Jan</th>
-								<th>Feb</th>
-								<th>Mar</th>
-								<th>Apr</th>
-								<th>May</th>
-								<th>Jun</th>
-								<th>Jul</th>
-								<th>Aug</th>
-								<th>Sep</th>
-								<th>Oct</th>
-								<th>Nov</th>
-								<th>Dec</th>
+								<th>Charge Date</th>
+								<th>Month</th>
+								<th>Year</th>
+								<th>Assessment Type</th>
+								<th>Amount</th>
 
 							</thead>
 
@@ -117,49 +108,30 @@
 								
 								<?php
 
-									$result = pg_query("SELECT * FROM homeid WHERE community_id=$community_id");
+									$result = pg_query("SELECT * FROM current_charges WHERE community_id=$community_id AND home_id=$home_id AND hoa_id=$hoa_id");
 
 									while($row = pg_fetch_assoc($result))
 									{
 
 										$year = date('Y');
-										$home_id = $row['home_id'];
-										$address = $row['address1'];
-										$living_status = $row['living_status'];
+										$assessment_rule_type = $row['assessment_rule_type_id'];
+										$assessment_month = $row['assessment_month'];
+										$assessment_year = $row['assessment_year'];
+										$amount = $row['amount'];
+										$assessment_date = $row['assessment_date'];
 
-										$row1 = pg_fetch_assoc(pg_query("SELECT * FROM hoaid WHERE home_id=$home_id AND valid_until>='$today'"));
+										if($assessment_date != '')
+											$assessment_date = date('m-d-Y', strtotime($assessment_date));
 
-										$name = $row1['firstname'];
-										$name .= " ";
-										$name .= $row1['lastname'];
-										$hoa_id = $row1['hoa_id'];
+										if($amount != '')
+											$amount = "$ ".$amount;
 
-										$row1 = pg_fetch_assoc(pg_query("SELECT * FROM current_year_payments_processed WHERE community_id=$community_id AND hoa_id=$hoa_id AND home_id=$home_id AND year=$year"));
+										$assessment_month = date('F', $assessment_month);
 
-										$m[1] = $row1['m1_pmt_processed'];
-                          				$m[2] = $row1['m2_pmt_processed'];
-                          				$m[3] = $row1['m3_pmt_processed'];
-                          				$m[4] = $row1['m4_pmt_processed'];
-                          				$m[5] = $row1['m5_pmt_processed'];
-                          				$m[6] = $row1['m6_pmt_processed'];
-                          				$m[7] = $row1['m7_pmt_processed'];
-                          				$m[8] = $row1['m8_pmt_processed'];
-                          				$m[9] = $row1['m9_pmt_processed'];
-                         				$m[10] = $row1['m10_pmt_processed'];
-                          				$m[11] = $row1['m11_pmt_processed'];
-                          				$m[12] = $row1['m12_pmt_processed'];
+										$row1 = pg_fetch_assoc(pg_query("SELECT * FROM assessment_rule_type WHERE assessment_rule_type_id=$assessment_rule_type"));
+										$assessment_rule_type = $row1['name'];
 
-                          				for ($i = 1; $i <= 12; $i++)
-                          				{
-                            
-                            				if($m[$i] == 't')
-                              					$m[$i] = "<center style='color: green;'><i class='fa fa-check-square'></i></center>";
-                            				else
-                              					$m[$i] = "<center style='color: orange;'><i class='fa fa-square-o'></i></center>";
-
-                          				}
-
-                          				echo "<tr><td><a href='processPayment2.php?hoa_id=$hoa_id&home_id=$home_id' style='color: blue;'>$name<br>($hoa_id)</td><td><a href='processPayment2.php?hoa_id=$hoa_id&home_id=$home_id' style='color: blue;'>$address<br>($home_id)</td><td>$m[1]</td><td>$m[2]</td><td>$m[3]</td><td>$m[4]</td><td>$m[5]</td><td>$m[6]</td><td>$m[7]</td><td>$m[8]</td><td>$m[9]</td><td>$m[10]</td><td>$m[11]</td><td>$m[12]</td></tr>";
+                          				echo "<tr><td>$assessment_date</td><td>$assessment_month</td><td>$assessment_year</td><td>$assessment_rule_type</td><td>$amount</td></tr>";
 
 									}
 
