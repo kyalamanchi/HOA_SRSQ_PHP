c11c9326814ea5043bc006f700889df53bd52344
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-25 22:29:42 +0530

    Pdf Update

--- var/www/html/mc_table.php
+++ var/www/html/mc_table.php
@@ -49,137 +49,103 @@ function __construct($orientation='P', $unit='mm', $format='A4')
     $this->issetcolor=false;
 }
 
-function WriteHTML($html)
-{
-    //HTML parser
-    $html=strip_tags($html,"<b><u><i><a><img><p><br><strong><em><font><tr><blockquote>"); //supprime tous les tags sauf ceux reconnus
-    $html=str_replace("\n",' ',$html); //remplace retour à la ligne par un espace
-    $a=preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE); //éclate la chaîne avec les balises
-    foreach($a as $i=>$e)
+ function WriteHTML($html)
     {
-        if($i%2==0)
+        //HTML parser
+        $html=str_replace("\n",' ',$html);
+        $a=preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE);
+        foreach($a as $i=>$e)
         {
-            //Text
-            if($this->HREF)
-                $this->PutLink($this->HREF,$e);
-            else
-                $this->Write(5,stripslashes(txtentities($e)));
-        }
-        else
-        {
-            //Tag
-            if($e[0]=='/')
-                $this->CloseTag(strtoupper(substr($e,1)));
+            if($i%2==0)
+            {
+                //Text
+                if($this->HREF)
+                    $this->PutLink($this->HREF,$e);
+                elseif($this->ALIGN=='center')
+                    $this->Cell(0,5,$e,0,1,'C');
+                else
+                    $this->Write(5,$e);
+            }
             else
             {
-                //Extract attributes
-                $a2=explode(' ',$e);
-                $tag=strtoupper(array_shift($a2));
-                $attr=array();
-                foreach($a2 as $v)
+                //Tag
+                if($e[0]=='/')
+                    $this->CloseTag(strtoupper(substr($e,1)));
+                else
                 {
-                    if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
-                        $attr[strtoupper($a3[1])]=$a3[2];
+                    //Extract properties
+                    $a2=explode(' ',$e);
+                    $tag=strtoupper(array_shift($a2));
+                    $prop=array();
+                    foreach($a2 as $v)
+                    {
+                        if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
+                            $prop[strtoupper($a3[1])]=$a3[2];
+                    }
+                    $this->OpenTag($tag,$prop);
                 }
-                $this->OpenTag($tag,$attr);
             }
         }
     }
-}
 
-function OpenTag($tag, $attr)
-{
-    //Opening tag
-    switch($tag){
-        case 'STRONG':
-            $this->SetStyle('B',true);
-            break;
-        case 'EM':
-            $this->SetStyle('I',true);
-            break;
-        case 'B':
-        case 'I':
-        case 'U':
-            $this->SetStyle($tag,true);
-            break;
-        case 'A':
-            $this->HREF=$attr['HREF'];
-            break;
-        case 'IMG':
-            if(isset($attr['SRC']) && (isset($attr['WIDTH']) || isset($attr['HEIGHT']))) {
-                if(!isset($attr['WIDTH']))
-                    $attr['WIDTH'] = 0;
-                if(!isset($attr['HEIGHT']))
-                    $attr['HEIGHT'] = 0;
-                $this->Image($attr['SRC'], $this->GetX(), $this->GetY(), px2mm($attr['WIDTH']), px2mm($attr['HEIGHT']));
-            }
-            break;
-        case 'TR':
-        case 'BLOCKQUOTE':
-        case 'BR':
-            $this->Ln(5);
-            break;
-        case 'P':
-            $this->Ln(10);
-            break;
-        case 'FONT':
-            if (isset($attr['COLOR']) && $attr['COLOR']!='') {
-                $coul=hex2dec($attr['COLOR']);
-                $this->SetTextColor($coul['R'],$coul['V'],$coul['B']);
-                $this->issetcolor=true;
-            }
-            if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist)) {
-                $this->SetFont(strtolower($attr['FACE']));
-                $this->issetfont=true;
-            }
-            break;
-    }
-}
-
-function CloseTag($tag)
-{
-    //Closing tag
-    if($tag=='STRONG')
-        $tag='B';
-    if($tag=='EM')
-        $tag='I';
-    if($tag=='B' || $tag=='I' || $tag=='U')
-        $this->SetStyle($tag,false);
-    if($tag=='A')
-        $this->HREF='';
-    if($tag=='FONT'){
-        if ($this->issetcolor==true) {
-            $this->SetTextColor(0);
-        }
-        if ($this->issetfont) {
-            $this->SetFont('arial');
-            $this->issetfont=false;
-        }
-    }
-}
-
-function SetStyle($tag, $enable)
-{
-    //Modify style and select corresponding font
-    $this->$tag+=($enable ? 1 : -1);
-    $style='';
-    foreach(array('B','I','U') as $s)
+function OpenTag($tag,$prop)
     {
-        if($this->$s>0)
-            $style.=$s;
+        //Opening tag
+        if($tag=='B' || $tag=='I' || $tag=='U')
+            $this->SetStyle($tag,true);
+        if($tag=='A')
+            $this->HREF=$prop['HREF'];
+        if($tag=='BR')
+            $this->Ln(5);
+        if($tag=='P')
+            $this->ALIGN=$prop['ALIGN'];
+        if($tag=='HR')
+        {
+            if( !empty($prop['WIDTH']) )
+                $Width = $prop['WIDTH'];
+            else
+                $Width = $this->w - $this->lMargin-$this->rMargin;
+            $this->Ln(2);
+            $x = $this->GetX();
+            $y = $this->GetY();
+            $this->SetLineWidth(0.4);
+            $this->Line($x,$y,$x+$Width,$y);
+            $this->SetLineWidth(0.2);
+            $this->Ln(2);
+        }
     }
-    $this->SetFont('',$style);
-}
 
-function PutLink($URL, $txt)
-{
-    //Put a hyperlink
-    $this->SetTextColor(0,0,255);
-    $this->SetStyle('U',true);
-    $this->Write(5,$txt,$URL);
-    $this->SetStyle('U',false);
-    $this->SetTextColor(0);
-}
+    function CloseTag($tag)
+    {
+        //Closing tag
+        if($tag=='B' || $tag=='I' || $tag=='U')
+            $this->SetStyle($tag,false);
+        if($tag=='A')
+            $this->HREF='';
+        if($tag=='P')
+            $this->ALIGN='';
+    }
+
+function SetStyle($tag,$enable)
+    {
+        //Modify style and select corresponding font
+        $this->$tag+=($enable ? 1 : -1);
+        $style='';
+        foreach(array('B','I','U') as $s)
+            if($this->$s>0)
+                $style.=$s;
+        $this->SetFont('',$style);
+    }
+
+    function PutLink($URL,$txt)
+    {
+        //Put a hyperlink
+        $this->SetTextColor(0,0,255);
+        $this->SetStyle('U',true);
+        $this->Write(5,$txt,$URL);
+        $this->SetStyle('U',false);
+        $this->SetTextColor(0);
+    }
 
 function SetWidths($w)
 {
