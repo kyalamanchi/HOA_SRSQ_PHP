d4e6ffe4595d5a677c31a4637ad923cb97536529
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-25 12:09:58 +0530

    changes

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -81,7 +81,7 @@
 
 						<div class="row">
 
-							<div class="col-md-8 offset-md-2">
+							<div class="col-md-10 offset-md-1">
 								
 								<!-- Tabs-->
 								<ul class="nav nav-tabs">
