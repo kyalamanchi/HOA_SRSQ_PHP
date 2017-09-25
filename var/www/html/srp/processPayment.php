39390b03e5606b3a6616d0b3671ff344b15bfba0
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-25 13:21:25 +0530

    changes

---
+++ var/www/html/srp/processPayment.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+
+<html lang='en'>
+
+	<head>
+
+		<?php
+
+			session_start();
+
+			if(!$_SESSION['hoa_username'])
+				header("Location: logout.php");
+
+			pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
+			$community_id = $_SESSION['hoa_community_id'];
+			$mode = $_SESSION['hoa_mode'];
+
+		?>
+
+		<meta charset='UTF-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<meta name='description' content='Stoneridge Place At Pleasanton HOA'>
+		<meta name='author' content='Geeth'>
+
+		<title><?php echo $_SESSION['hoa_community_code']; if($mode == 1) echo " | Board Dashboard"; else if($mode == 2) echo " | Resident Dashboard"; ?></title>
+
+		<!-- Web Fonts-->
+		<link href='https://fonts.googleapis.com/css?family=Poppins:500,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Hind:400,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Lora:400i' rel='stylesheet'>
+		<!-- Bootstrap core CSS-->
+		<link href='assets/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+		<!-- Icon Fonts-->
+		<link href='assets/css/font-awesome.min.css' rel='stylesheet'>
+		<link href='assets/css/linea-arrows.css' rel='stylesheet'>
+		<link href='assets/css/linea-icons.css' rel='stylesheet'>
+		<!-- Plugins-->
+		<link href='assets/css/magnific-popup.css' rel='stylesheet'>
+		<link href='assets/css/vertical.min.css' rel='stylesheet'>
+		<link href='assets/css/pace-theme-minimal.css' rel='stylesheet'>
+		<link href='assets/css/animate.css' rel='stylesheet'>
+		<!-- Template core CSS-->
+		<link href='assets/css/template.min.css' rel='stylesheet'>
+		<!-- Datatable -->
+		<link rel='stylesheet' href='https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css'>
+
+	</head>
+
+	<body>
+
+		<div class="wrapper">
+
+			<!-- Header-->
+			<?php if($mode == 1) include "boardHeader.php"; else if($mode == 2) include "residentHeader.php"; ?>
+
+			<!-- Page Header -->
+			<section class="module-page-title">
+				
+				<div class="container">
+						
+					<div class="row-page-title">
+						
+						<div class="page-title-captions">
+							
+							<h1 class="h5">Process Payments</h1>
+						
+						</div>
+					
+					</div>
+					
+				</div>
+			
+			</section>
+
+			<!-- Content -->
+			<section class="module">
+					
+				<div class="container">
+						
+					<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+					
+						
+
+					</div>
+
+				</div>
+
+			</section>
+
+			<section class="module-page-title">
+					
+				<div class="container">
+						
+					<div class="row-page-title">
+							
+						<div class="page-title-captions"></div>
+
+					</div>
+
+				</div>
+
+			</section>
+
+			<!-- Footer-->
+			<?php include 'footer.php'; ?>
+
+			<a class='scroll-top' href='#top'><i class='fa fa-angle-up'></i></a>
+
+		</div>
+
+		<!-- Scripts-->
+		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.1.1/js/tether.min.js'></script>
+		<script src='assets/bootstrap/js/bootstrap.min.js'></script>
+		<script src='http://maps.googleapis.com/maps/api/js?key=AIzaSyA0rANX07hh6ASNKdBr4mZH0KZSqbHYc3Q'></script>
+		<script src='assets/js/plugins.min.js'></script>
+		<script src='assets/js/custom.min.js'></script>
+		<!-- Datatable -->
+		<script src='//code.jquery.com/jquery-1.12.4.js'></script>
+		<script src='https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js'></script>
+
+		<script>
+      	
+	      	$(function () {
+	        	
+	        	$("#example1").DataTable({ "pageLength": 50 });
+
+	      	});
+
+    	</script>
+
+		<!-- Color Switcher (Remove these lines)-->
+		<!--script src='assets/js/style-switcher.min.js'></script-->
+	</body>
+
+</html>
