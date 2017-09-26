b1be853850504dae33fbaf17c8b08469cd14a483
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-27 00:43:59 +0530

    Agreement

--- var/www/html/agreement.php
+++ var/www/html/agreement.php
@@ -5,13 +5,9 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
-    <script src='https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js'></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src='https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js'></script>
-    <script src="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src='https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js'></script>
     <style>
 .switch {
   position: relative;
