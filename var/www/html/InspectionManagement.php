831485ec7361977b098d7afc8bb53a745b3990a6
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-27 05:05:37 +0530

    update

--- var/www/html/InspectionManagement.php
+++ var/www/html/InspectionManagement.php
@@ -8,8 +8,8 @@
     <script src='https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js'></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src='https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js'></script>
-    <script src="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"></script>
+    <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
     <title>Inspeciton Management</title>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -246,7 +246,7 @@ function hidePleaseWait() {
     background-color: #b2ffb2;
     }*/
     tr.row_selected td{background-color:#b2ffb2 !important;}
-    
+
     /*.row_selected td {
     background-color: #b2ffb2 !important;  Add !important to make sure override datables base styles 
  }*/
