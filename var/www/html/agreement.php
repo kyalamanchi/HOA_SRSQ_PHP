cb193d0e807d0e4e42545db352f1e8248750827c
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-27 00:55:02 +0530

    Agreement Update

--- var/www/html/agreement.php
+++ var/www/html/agreement.php
@@ -1,7 +1,6 @@
 <html>
   <head>
     <title>Create Agreement</title>
-     <!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
