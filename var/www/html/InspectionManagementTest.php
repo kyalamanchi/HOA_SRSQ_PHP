da839e1181dcb9bd7fcb1fedb9b63f41738fba9a
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-27 05:58:51 +0530

    Changes

--- var/www/html/InspectionManagementTest.php
+++ var/www/html/InspectionManagementTest.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
+<head>
 <script type="text/javascript" src="http://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
-
 <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
+</head>
+<body>
 <table id="example" class="display" cellspacing="0" width="100%">
   <thead>
     <tr>
@@ -484,6 +486,6 @@
     </tr>
   </tbody>
 </table>
-
+</body>
 
 </html>
