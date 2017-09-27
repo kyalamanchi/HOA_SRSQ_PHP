2eda73a3fabf64d3dc5e11ebb831d09ec90874a5
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 01:18:21 +0530

    changes

--- var/www/html/InspectionManagementTest.php
+++ var/www/html/InspectionManagementTest.php
@@ -25,16 +25,13 @@
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
-          <!-- <h4 class="modal-title">Modal Header</h4> -->
-          <button type="button" class="btn btn-default " >Edit</button>
+          <button type="button" class="btn btn-default">Edit</button>
           <button type="button" class="btn btn-primary pull-right" style="padding-left: 5px;">Send Notice(s) - SouthData</button>
           <button type="button" class="btn btn-primary pull-right" style="padding-right: 5px;">Email Notice(s)</button>
-          
         </div>
         <div class="modal-body">
             <div>
-            <iframe src="258.pdf"
+            <iframe src="preview.pdf"
             style="width:880px; height:768px;" frameborder="0"></iframe>
         </div>
         </div>
