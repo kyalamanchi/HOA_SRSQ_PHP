b65d6fa9f6f2f50cdb4d67cd21d8ad6601bb2d55
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-27 22:40:45 +0530

    Changes

--- var/www/html/paymentPageSRSQ.php
+++ var/www/html/paymentPageSRSQ.php
@@ -1,5 +1,6 @@
 `<html>
   <head>
+  <title>Create Payment</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
