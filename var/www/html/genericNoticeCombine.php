5e301e3130e7ae3ac806619840af15831b92baf4
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 00:30:09 +0530

    Changes

--- var/www/html/genericNoticeCombine.php
+++ var/www/html/genericNoticeCombine.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("America/New_York");
 header("Content-Type: text/event-stream\n\n");
 $message  = "Generating Inspection Notice...Please Wait...";
-  echo 'data: '.$message."\n\n";
+  // echo 'data: '.$message."\n\n";
   ob_end_flush();
   flush();
  	$connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
