b7c828df7bd41a16c2493b5e75d2be5e505800ca
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 00:52:45 +0530

    test

--- var/www/html/InspectionManagementTest.php
+++ var/www/html/InspectionManagementTest.php
@@ -32,7 +32,7 @@
         <div class="modal-body">
             <div>
             <iframe src="258.pdf"
-            style="width:900px; height:1000px;" frameborder="0"></iframe>
+            style="width:880px; " frameborder="0"></iframe>
         </div>
         </div>
         <div class="modal-footer">
