2d2fc7ba1f9380af5b602d40eacc2edf51eec75c
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 04:56:07 +0530

    Changes

--- var/www/html/verifyUser.php
+++ var/www/html/verifyUser.php
@@ -5,19 +5,4 @@ $message  = "Please Wait...";
 echo 'data: '.$message."\n\n";  
 ob_end_flush();
 flush();
-
-if ( $_GET['id']; ){
-$message  = $_GET['id'];
-echo 'data: '.$message."\n\n";  
-ob_end_flush();
-flush();
-}
-
-else {
-  $message  = "Failed...HOA ID not found.";
-echo 'data: '.$message."\n\n";  
-ob_end_flush();
-flush();
-}
-
 ?>
