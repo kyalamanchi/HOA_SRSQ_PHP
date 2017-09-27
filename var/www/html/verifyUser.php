d6732bfe9b6f0e140ad4a256387b1636a38bf064
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 05:07:53 +0530

    test

--- var/www/html/verifyUser.php
+++ var/www/html/verifyUser.php
@@ -10,6 +10,28 @@ $message  = $_GET['id'];
 echo 'data: '.$message."\n\n";  
 ob_end_flush();
 flush();
+$query = "SELECT * FROM HOAID WHERE HOA_ID=".$_GET['id'];
+$queryResult = pg_query($query);
+$row = pg_fetch_assoc($queryResult);
+
+if ( $row['cell_no'] ){
+  $message = substr($row['cell_no'], -4);
+  echo 'data: '.$message."\n\n";  
+  ob_end_flush();
+  flush();
+}
+else if ( $row['email'] ){
+  $string = explode('@', $row['email']);
+  $message = '@'.$string[1];
+  echo 'data: '.$message."\n\n";  
+  ob_end_flush();
+  flush();
+}
+else {
+  $message  = "Verification failed";
+}
+
+
 }
 else {
   $message  = "Failed...HOA ID not found.";
