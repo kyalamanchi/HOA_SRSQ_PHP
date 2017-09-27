d7df2741594bd85872592e55040302cfc79e1c7d
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-27 06:57:23 +0530

    Changes

--- var/www/html/updateInspectionData.php
+++ var/www/html/updateInspectionData.php
@@ -1,5 +1,15 @@
 <?php
 $parsedJSON = json_decode(file_get_contents('php://input'));
-echo $parsedJSON[0]->inspection_id;
+
+$connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
+
+if( $connection ){
+
+    $query = "UPDATE INSPECTION_NOTICES SET DESCRIPTION='".$parsedJSON[0]->inspection_description."',INSPECTION_CATEGORY_ID=".$parsedJSON[0]->inspection_category_id.",INSPECTION_SUB_CATEGORY_ID=".$parsedJSON[0]->inspection_sub_category_id.",HOME_ID=".$parsedJSON[0]->home_id.",HOA_ID=".$parsedJSON[0]->hoa_id.",location_id=".$parsedJSON[0]->location_id.",inspection_status_id=".$parsedJSON[0]->inspection_status_id.",item='".$parsedJSON[0]->item."' WHERE ID=".$parsedJSON[0]->inspection_id;
+    echo $query;
+
+}
+
+
 
 ?>
