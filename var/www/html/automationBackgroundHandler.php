1e7c46e8d6d0ac0e949eba86a3b28e76712ad77c
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-29 00:46:57 +0530

    updates

--- var/www/html/automationBackgroundHandler.php
+++ var/www/html/automationBackgroundHandler.php
@@ -6,22 +6,45 @@ echo 'data: '.$message."\n\n";
 ob_end_flush();
 flush();
 if ( $_GET['id'] == 1){
+
+
 $message  = "Updating SRP Transactions";
 echo 'data: '.$message."\n\n";  
 ob_end_flush();
 flush();
+
+$req = curl_init();
+curl_setopt($req, CURLOPT_URL,"https://hoaboardtime.com/updateCurrentPaymentsSRP.php");
+curl_exec($req);
+
 $message  = "Updating SRP paymethods";
 echo 'data: '.$message."\n\n";  
 ob_end_flush();
 flush();
+
+$req = curl_init();
+curl_setopt($req, CURLOPT_URL,"https://hoaboardtime.com/updateHomePayMethodSRP.php");
+curl_exec($req);
+
 $message  = "Updating SRP deposits";
 echo 'data: '.$message."\n\n";  
 ob_end_flush();
 flush();
+
+$req = curl_init();
+curl_setopt($req, CURLOPT_URL,"https://hoaboardtime.com/srpcommunityDeposits.php");
+curl_exec($req);
+
 $message  = "Updating SRP deposit transactions";
 echo 'data: '.$message."\n\n";  
 ob_end_flush();
 flush();
+
+$req = curl_init();
+curl_setopt($req, CURLOPT_URL,"https://hoaboardtime.com/srpcommunityDepositsTransactions.php");
+curl_exec($req);
+
+
 $id = date('Y-m-d H:i:s');
 $message  = "Done!!!";
 echo "id: $id\n";
