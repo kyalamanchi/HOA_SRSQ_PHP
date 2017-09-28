ce65ba7b067b38d2020ef69e44fd2a636f69277c
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 22:47:29 +0530

    changes

--- var/www/html/AutomationTest.php
+++ var/www/html/AutomationTest.php
@@ -29,7 +29,7 @@
     <div class="panel panel-default">
       <div class="panel-heading">
         <h3 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Collapsible Group 2</a>
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2"><h3>Agreements<h5 style="float: right;">Last ran : </h5></h3></a>
         </h3>
       </div>
       <div id="collapse2" class="panel-collapse collapse">
@@ -41,7 +41,7 @@
     <div class="panel panel-default">
       <div class="panel-heading">
         <h3 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Collapsible Group 3</a>
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3"><h3>Billing Statements<h5 style="float: right;">Last ran : </h5></h3></a>
         </h3>
       </div>
       <div id="collapse3" class="panel-collapse collapse">
