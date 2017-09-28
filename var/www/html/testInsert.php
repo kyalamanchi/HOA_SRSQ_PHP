2d7ec927d082c861cb896910c05fd4feeba79d44
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-29 01:10:50 +0530

    test

---
+++ var/www/html/testInsert.php
@@ -0,0 +1,5 @@
+<?php
+$connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
+$query = "INSERT INTO BACKGROUND_JOBS(\"COMMUNITY_ID\",\"JOB_CATEGORY_ID\",\"JOB_SUB_CATEGORY_ID\",\"START_TIME\") VALUES(1,1,1,'".date('Y-m-d H:i:s')."')";
+pg_query($query);
+?>
