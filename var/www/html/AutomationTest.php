ba7a2444dad7176f18ff2df29b554f7eaea7cb90
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 22:33:22 +0530

    update

--- var/www/html/AutomationTest.php
+++ var/www/html/AutomationTest.php
@@ -17,10 +17,10 @@
     <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Collapsible Group 1</a>
+          <a data-toggle="collapse" data-parent="#accordion" href="#payments"><h4>Payments</h4></a>
         </h4>
       </div>
-      <div id="collapse1" class="panel-collapse collapse in">
+      <div id="payments" class="panel-collapse collapse in">
         <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
