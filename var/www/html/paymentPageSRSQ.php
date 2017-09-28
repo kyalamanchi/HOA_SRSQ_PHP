2e36180b7c3591b9a5c839b8de06621e2675d674
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 07:34:38 +0530

    changes

--- var/www/html/paymentPageSRSQ.php
+++ var/www/html/paymentPageSRSQ.php
@@ -184,7 +184,27 @@ color: red;
     <hr>
     <br><br>
   <div class="container" style=" margin: 0 auto;" >
-      <!-- <center><h3>Balance as of 28 SEPTEMBER : $0</h3></center> -->
+                                  <?php 
+                                   date_default_timezone_set('America/Los_Angeles');
+                                    $query  = "SELECT * FROM CURRENT_CHARGES WHERE HOA_ID=".$_GET['id']."AND ASSESSMENT_YEAR=".date("Y");
+                                    $queryResult = pg_query($query);
+                                    $currentChargesTotal  = 0;
+                                    while ($row = pg_fetch_assoc($queryResult)) {
+                                      $currentChargesTotal = $currentChargesTotal+$row['amount'];
+                                    }
+                                    $query = "SELECT * FROM CURRENT_PAYMENTS WHERE HOA_ID=".$_GET['id']." AND EXTRACT(YEAR FROM PROCESS_DATE)=".date("Y");
+                                    $queryResult = pg_query($query);
+                                    $currentPaymentsTotal = 0;
+                                    while ($row = pg_fetch_assoc($queryResult)) {
+                                      $currentPaymentsTotal = $currentPaymentsTotal + $row['amount'];
+                                    }
+                                    if ( $currentChargesTotal-$currentPaymentsTotal > 0 ){
+
+                                    }
+                                    else {
+                                      echo '<center><h4><span class="notbold">Current Balance as of '.date('Y-m-d').' is</span> $ 0 </h4></center>';
+                                    }
+                                    ?>
       <br>
       <br>
     <div class="row">
