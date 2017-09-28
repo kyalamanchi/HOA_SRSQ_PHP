8c7d8a70fdf36a68ad87ebe2cf69cce0aee97de2
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 23:27:11 +0530

    changes

--- var/www/html/AutomationTest.php
+++ var/www/html/AutomationTest.php
@@ -67,7 +67,12 @@
     </div>
     <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
       <div class="card-block">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <font size="4" style="float: right;">Last ran on : </font>
+         Generates billing statements and uploads to Dropbox. SRP path : /Billing_Statements/SRP/2017/ . SRSQ Path /Billing_Statements/SRSQ/2017/ . 
+        <br>
+        <br>
+        <button type="button" class="btn btn-outline-primary">Update Now</button>
+
       </div>
     </div>
   </div>
