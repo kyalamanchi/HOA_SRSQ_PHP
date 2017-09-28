e88579d22fc998c4f1bd47a16304e5a1e67ab527
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 06:34:50 +0530

    update

--- var/www/html/verifyUserData.php
+++ var/www/html/verifyUserData.php
@@ -12,19 +12,28 @@ if ( $_GET['id'] && $_GET['data']){
   $row = pg_fetch_assoc($queryResult);
   if ( $row['cell_no'] ){
       if ( !(strcmp($row['cell_no'], $_GET['data'])) ){
-        echo "success";
+  
+        $message  = "success";
+      echo 'data: '.$message."\n\n";  
+      ob_end_flush();
+      flush();
         exit(0);
       }
   }
    if ( $row['email'] ){
       if ( !(strcmp($row['email'], $_GET['data'])) ){
-        echo "success";
+        $message  = "success";
+      echo 'data: '.$message."\n\n";  
+      ob_end_flush();
+      flush();
         exit(0);
       }
   }
 
-  echo "failed";
-  exit(0);
+  $message  = "failed";
+      echo 'data: '.$message."\n\n";  
+      ob_end_flush();
+      flush();
   
 }
 else {
