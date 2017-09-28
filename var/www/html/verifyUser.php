3f5b348686273c6dfa43dbb33ffd3e35f01e0362
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-28 07:18:48 +0530

    changes

--- var/www/html/verifyUser.php
+++ var/www/html/verifyUser.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header("Content-Type: text/event-stream\n\n");
 date_default_timezone_set('America/Los_Angeles');
 $message  = "Please Wait...";
