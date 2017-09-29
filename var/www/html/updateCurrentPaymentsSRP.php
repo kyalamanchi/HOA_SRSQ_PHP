311dc818daa0a05865bbea4fad2b0ebf2db38a28
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-29 20:57:02 +0530

    changes

--- var/www/html/updateCurrentPaymentsSRP.php
+++ var/www/html/updateCurrentPaymentsSRP.php
@@ -148,6 +148,6 @@ else {
 	// print_r("Greater than 1000");
 }
 
-print_r("Records inserted "+$insertCount+". Records inserted "+$insertCount);
+print_r("Records inserted ".$insertCount.". Records updated "+$updateCount);
 
 ?>
