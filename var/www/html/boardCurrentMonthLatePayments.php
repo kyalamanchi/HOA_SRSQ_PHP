920c0603f48b0ed931a1975d57fc8aa871622667
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-29 18:42:52 +0530

    test

--- var/www/html/boardCurrentMonthLatePayments.php
+++ var/www/html/boardCurrentMonthLatePayments.php
@@ -36,12 +36,12 @@
     <script type="text/javascript">
     function generateForSouthData(button){
       var id = button.id;
-      // var pleaseWaitData = '<div class="progress">\
-      //                 <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
-      //                 aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
-      //                 </div>\
-      //               </div>';
-      // $("#pleaseWaitDialog2").find('.modal-body').html(pleaseWaitData);
+      var pleaseWaitData = '<div class="progress">\
+                      <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
+                      aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
+                      </div>\
+                    </div>';
+      $("#pleaseWaitDialog2").find('.modal-body').html(pleaseWaitData);
       $("#pleaseWaitDialog2").modal("show");
       var url = "https://hoaboardtime.com/billingStatementGeneration.php?id="+id;
       var source = new EventSource(url);
