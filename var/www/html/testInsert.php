c2102fb62d201da53c44dbc5df6e25d55ef19fd7
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-29 21:30:51 +0530

    changes

--- var/www/html/testInsert.php
+++ var/www/html/testInsert.php
@@ -1,8 +1,9 @@
 <?php
 $req = curl_init();
 curl_setopt($req, CURLOPT_URL,"https://hoaboardtime.com/updateCurrentPaymentsSRP.php");
-$message  = curl_exec($req);
-echo 'data: '.$message."\n\n";  
-ob_end_flush();
-flush();
+$message  = $message.curl_exec($req);
+echo $message;
+// echo 'data: '.$message."\n\n";  
+// ob_end_flush();
+// flush();
 ?>
