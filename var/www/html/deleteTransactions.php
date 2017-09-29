ff85e00cb9c7d0e12fc252fd942da8bb92eb0467
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-30 02:25:24 +0530

    changes

---
+++ var/www/html/deleteTransactions.php
@@ -0,0 +1,14 @@
+<?php
+$transactionList = array();
+array_push($transactionList, 'trn_1936243f-caa9-43c8-9c03-544aaefaf8ed');
+array_push($transactionList, 'trn_393a5258-3e4a-4cf3-b4b9-78effd507860');
+array_push($transactionList, 'trn_57c26af6-0239-41eb-af9a-b5a69711c58f');
+array_push($transactionList, 'trn_582c64fd-d294-47f0-92e8-3040ed660d3e');
+array_push($transactionList, 'trn_d577dfc6-4e88-4ab1-b485-76723e157d6f');
+foreach ($transactionList as $key) {
+	print_r($key);
+
+	print_r(nl2br("\n"));
+}
+
+?>
