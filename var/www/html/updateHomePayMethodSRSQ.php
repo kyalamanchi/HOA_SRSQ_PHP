6c93395f80fbdd963b62e53b8fb7d1f030b0fa2b
Dhivysh <Dhivysh@users.noreply.github.com>
2017-09-29 20:39:43 +0530

    changes

--- var/www/html/updateHomePayMethodSRSQ.php
+++ var/www/html/updateHomePayMethodSRSQ.php
@@ -1,5 +1,7 @@
 <?php
 date_default_timezone_set('America/Los_Angeles');
+$insertCount = 0;
+$updateCount = 0;
 $connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
 $homeidquery = "SELECT * FROM hoaid WHERE community_id = 2";
 $homeresult = pg_query($homeidquery);
@@ -9,7 +11,7 @@ while ($row = pg_fetch_assoc($homeresult)) {
 	$homeIDSARRAY[$row['hoa_id']] = $row['home_id'];
 }
 
-$url = "https://api.forte.net/v3/schedules?page_size=1000";
+$url = "https://api.forte.net/v3/schedules?page_size=10000";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('content-type: application/json','x-forte-auth-organization-id: org_332536','authorization: Basic ZjNkOGJhZmY1NWM2OTY4MTExNTQ2OTM3ZDU0YTU1ZGU6Zjc0NzdkNTExM2EwNzg4NTUwNmFmYzIzY2U2MmNhYWU='));
@@ -30,7 +32,9 @@ foreach ($result->results as $schedule) {
 		$day = date('d',strtotime($schedule->schedule_start_date));
 		$scheduleExpiry = date('Y-m-d', strtotime($rm,strtotime(date('Y-m-'.$day))));
 		$updateQuery = "UPDATE home_pay_method SET payment_type_id=1,community_id=2,hoa_id=".$schedule->customer_id.",clientid='".$schedule->customer_token."',sch_start='".date('Y-m-d',strtotime($schedule->schedule_start_date))."',sch_end='".$scheduleExpiry."',sch_expires='".$scheduleExpiry."',next_sch='".date('Y-m-d',strtotime($schedule->schedule_summary->schedule_next_date))."',sch_create_date='".date('Y-m-d',strtotime($schedule->schedule_created_date))."',schedule_qty='".$schedule->schedule_quantity."',no_sch_succ_compltd='".$schedule->schedule_summary->schedule_successful_quantity."',sch_amt=".$schedule->schedule_amount.",sch_status='".$schedule->schedule_status."',sch_frequency='".$schedule->schedule_frequency."',updated_on='".date('Y-m-d H:i:s')."',updated_by=401 WHERE home_id=".$homeIDSARRAY[$schedule->customer_id];
-		print_r($updateQuery.nl2br("\n\n"));
+		// print_r($updateQuery.nl2br("\n\n"));
+		$updateCount  = $updateCount + 1;
+
 	}
 	else if ( $schedule->schedule_summary->schedule_remaining_quantity == 1) {
 		$failedScheduleIDS[$schedule->schedule_id] = 1;
@@ -38,9 +42,9 @@ foreach ($result->results as $schedule) {
 }
 }
 else {
-	print_r("Number of schedules greater than 1000".nl2br("\n").$failedScheduleIDS);
+	// print_r("Number of schedules greater than 1000".nl2br("\n").$failedScheduleIDS);
 }
-	print_r(nl2br("\n"));
+	// print_r(nl2br("\n"));
 foreach ($failedScheduleIDS as $key => $value) {
 	$url = "https://api.forte.net/v3/schedules/";
 	$url = $url.$key."/";
@@ -66,19 +70,19 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		}
 
 		$updateQuery = "UPDATE home_pay_method SET payment_type_id=1,community_id=2,hoa_id=".$finalHoaID.",clientid='".$schedule->customer_token."',sch_start='".date('Y-m-d',strtotime($schedule->schedule_start_date))."',sch_end='".$scheduleExpiry."',sch_expires='".$scheduleExpiry."',next_sch='".date('Y-m-d',strtotime($schedule->schedule_summary->schedule_next_date))."',sch_create_date='".date('Y-m-d',strtotime($schedule->schedule_created_date))."',schedule_qty='".$schedule->schedule_quantity."',no_sch_succ_compltd='".$schedule->schedule_summary->schedule_successful_quantity."',sch_amt=".$schedule->schedule_amount.",sch_status='".$schedule->schedule_status."',sch_frequency='".$schedule->schedule_frequency."',updated_on='".date('Y-m-d H:i:s')."',updated_by=401 WHERE home_id=".$homeIDSARRAY[$finalHoaID];
-		print_r($updateQuery.nl2br("\n\n"));
+		// print_r($updateQuery.nl2br("\n\n"));
 		if (pg_query($updateQuery)){
-
+			$updateCount  = $updateCount + 1;
 		}
 		else {
-			print_r($updateQuery.nl2br("\n"));
+			// print_r($updateQuery.nl2br("\n"));
 		}
 
 		// print_r($updateQuery.nl2br("\n"));
 	}
 	else{
-		print_r($url.nl2br("\n"));
+		// print_r($url.nl2br("\n"));
 	}
 }
-
+print_r("Records update : ".$updateCount);
 ?>
