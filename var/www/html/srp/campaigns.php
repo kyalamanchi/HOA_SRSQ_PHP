74c2a30554bf7b4362307360e7cbfeb35d64530b
GeethChadalawada <geethchadalawada@gmail.com>
2017-09-29 19:39:39 +0530

    campigns

--- var/www/html/srp/campaigns.php
+++ var/www/html/srp/campaigns.php
@@ -184,10 +184,23 @@
 
 						<?php
 
-							$ch = curl_init('https://us12.api.mailchimp.com/3.0/reports/');
-          
+							
+							if($community_id == 1)
+							{
+
+								$ch = curl_init('https://us14.api.mailchimp.com/3.0/reports/');
+								curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey eecf4b5c299f0cc2124463fb10a6da2d-us14'));
+
+							}
+							else if($community_id == 2)
+							{
+
+								$ch = curl_init('https://us12.api.mailchimp.com/3.0/reports/');
+								curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey af5b50b9f714f9c2cb81b91281b84218-us12'));
+
+							}
+            				
             				curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
-            				curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey af5b50b9f714f9c2cb81b91281b84218-us12'));
             				curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
             
             				$result = curl_exec($ch);
