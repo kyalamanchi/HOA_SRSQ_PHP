534cf4fed3fd9879ec3138761cedafa98967bfa1
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-01 11:45:53 +0530

    paynow

--- var/www/html/srp/paymentPage1.php
+++ var/www/html/srp/paymentPage1.php
@@ -1,5 +1,24 @@
 <?php
 
+	if(isset($_POST['submit']))
+	{
+		echo "Yes";
+	}
+	else
+	{
+		echo "No";
+
+		echo "
+
+		<form method='POST'>
+
+			<button type='submit' name='submit' id='submit'>Submit</button>
+
+		</form>
+
+		";
+	}
+
 	echo $_POST['payment_hoa_id'];
 
 ?>
