7653a281ddaf624fdb777956ab0f982336bf995d
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-01 11:50:47 +0530

    pay now

--- var/www/html/srp/paymentPage1.php
+++ var/www/html/srp/paymentPage1.php
@@ -2,7 +2,7 @@
 
 	if(isset($_POST['submit']))
 	{
-		echo "Yes";
+		echo $_POST['hoa_id_select'];
 	}
 	else
 	{
@@ -11,6 +11,28 @@
 		echo "
 
 		<form method='POST'>
+
+			<select id='hoa_id_select' name='hoa_id_select' required>
+
+				<option value='' selected disabled>Select User</option>";
+
+				$result = pg_query("SELECT * FROM hoaid WHERE community_id=$community_id AND valid_until>='$today'");
+
+											while($row = pg_fetch_assoc($result))
+											{
+
+												$hoa_id = $row['hoa_id'];
+												$name = $row['firstname'];
+												$name .= " ";
+												$name .= $row['lastname'];
+
+												echo "<option value='$hoa_id'>$name</option>";
+
+											}
+
+			echo "
+
+			</select>
 
 			<button type='submit' name='submit' id='submit'>Submit</button>
 
