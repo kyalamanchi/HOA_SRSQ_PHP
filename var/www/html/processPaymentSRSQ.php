88c7ff47e16b670625bf356acf2e213a1790642f
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-03 02:13:06 +0530

    changes

--- var/www/html/processPaymentSRSQ.php
+++ var/www/html/processPaymentSRSQ.php
@@ -1,31 +1,33 @@
 <?php
-// $someJSON = file_get_contents('php://input');
-// $parsedJSON = json_decode($someJSON);
-// $customerID = $parsedJSON[0]->customer_id;
-// $authAmount = $parsedJSON[0]->auth_amount;
-// $name = $parsedJSON->account_holder;
-// $names = explode(' ', $name);
-// $fname = $names[0];
-// $lname  = $names[1];
-// $rnumber = $parsedJSON[0]->routing_number;
-// $anumber =  $parsedJSON[0]->account_number;
-// $url = "https://api.forte.net/v3/organizations/org_332536/locations/loc_190785/transactions";
-// $ch = curl_init($url);
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json','X-Forte-Auth-Organization-Id:org_332536','Authorization:Basic ZjNkOGJhZmY1NWM2OTY4MTExNTQ2OTM3ZDU0YTU1ZGU6Zjc0NzdkNTExM2EwNzg4NTUwNmFmYzIzY2U2MmNhYWU='));
-// $sendData = array("action" => "sale","customer_id" => $customerID ,"authorization_amount" => $authAmount,"billing_address" => array("first_name"=>$fname,"last_name" =>$lname),"echeck" => array( "sec_code" => "WEB","routing_number"=> $rnumber,"account_number" => $anumber,"account_holder" => $name ) );
-// curl_setopt($curl, CURLOPT_POSTFIELDS, $sendData);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-// $result = curl_exec($ch);
-// curl_close($ch);
-// echo $result;
-// $result = json_decode($result);
-// echo  $result;
-// if ( $result->response->response_desc){
-// 	echo $result->response->response_desc;
-// }
-// else {
-// echo $result;
-// }
-echo "Error occured...Please try again";
+$someJSON = file_get_contents('php://input');
+$parsedJSON = json_decode($someJSON);
+$customerID = $parsedJSON[0]->customer_id;
+$authAmount = $parsedJSON[0]->auth_amount;
+$name = $parsedJSON->account_holder;
+$names = explode(' ', $name);
+$fname = $names[0];
+$lname  = $names[1];
+$rnumber = $parsedJSON[0]->routing_number;
+$anumber =  $parsedJSON[0]->account_number;
+$url = "https://api.forte.net/v3/organizations/org_332536/locations/loc_190785/transactions";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json','X-Forte-Auth-Organization-Id:org_332536','Authorization:Basic ZjNkOGJhZmY1NWM2OTY4MTExNTQ2OTM3ZDU0YTU1ZGU6Zjc0NzdkNTExM2EwNzg4NTUwNmFmYzIzY2U2MmNhYWU='));
+$sendData = array("action" => "sale","customer_id" => $customerID ,"authorization_amount" => $authAmount,"billing_address" => array("first_name"=>$fname,"last_name" =>$lname),"echeck" => array( "sec_code" => "WEB","routing_number"=> $rnumber,"account_number" => $anumber,"account_holder" => $name ) );
+curl_setopt($curl, CURLOPT_POSTFIELDS, $sendData);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
+echo "<br><br><br>";
+$result = json_decode($result);
+echo  $result;
+if ( $result->response->response_desc){
+	echo $result->response->response_desc;
+}
+else {
+echo "<br><br><br>";
+echo $result;
+}
+
 ?>
