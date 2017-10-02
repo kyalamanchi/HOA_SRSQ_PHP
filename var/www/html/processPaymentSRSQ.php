489247dbadab3c528ef7e296ed7b5bd892e9cd8f
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-03 02:30:32 +0530

    update

--- var/www/html/processPaymentSRSQ.php
+++ var/www/html/processPaymentSRSQ.php
@@ -9,16 +9,15 @@ $fname = $names[0];
 $lname  = $names[1];
 $rnumber = $parsedJSON[0]->routing_number;
 $anumber =  $parsedJSON[0]->account_number;
+$data = array("action" => "sale","customer_id" => $customerID ,"authorization_amount" => $authAmount,"billing_address" => array("first_name"=>$fname,"last_name" =>$lname),"echeck" => array( "sec_code" => "WEB","routing_number"=> $rnumber,"account_number" => $anumber,"account_holder" => $name));
+$data_string = json_encode($data); 
 $url = "https://api.forte.net/v3/organizations/org_332536/locations/loc_190785/transactions";
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json','X-Forte-Auth-Organization-Id:org_332536','Authorization:Basic ZjNkOGJhZmY1NWM2OTY4MTExNTQ2OTM3ZDU0YTU1ZGU6Zjc0NzdkNTExM2EwNzg4NTUwNmFmYzIzY2U2MmNhYWU='));
-$sendData = array("action" => "sale","customer_id" => $customerID ,"authorization_amount" => $authAmount,"billing_address" => array("first_name"=>$fname,"last_name" =>$lname),"echeck" => array( "sec_code" => "WEB","routing_number"=> $rnumber,"account_number" => $anumber,"account_holder" => $name));
-
-curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($sendData));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Content-Length: ' . strlen($data_string),'X-Forte-Auth-Organization-Id:org_332536','Authorization:Basic ZjNkOGJhZmY1NWM2OTY4MTExNTQ2OTM3ZDU0YTU1ZGU6Zjc0NzdkNTExM2EwNzg4NTUwNmFmYzIzY2U2MmNhYWU='));
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $result = curl_exec($ch);
-
 curl_close($ch);
 $result = json_decode($result);
 if ( $result->response->response_desc){
