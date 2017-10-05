cb0a3217ec278920c73715286b4068308e748b2b
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-05 20:13:28 +0530

    changes

--- var/www/html/forteFullLoadDetailsSRSQ.php
+++ var/www/html/forteFullLoadDetailsSRSQ.php
@@ -1,8 +1,6 @@
 <?php
 $connection = pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
-
 if ( $connection){
-    
     if ( $_GET['id'] == 1 ){
     echo "Message";
     $url = 'https://api.forte.net/v3/organizations/org_332536/locations/loc_190785/transactions?filter=customer_id+eq+'.$_GET['data'].'+and+start_received_date+eq+\'2016-01-01\'+and+end_received_date+eq+\''.date('Y-m-d').'\'&page_size=10';
@@ -14,8 +12,19 @@ if ( $connection){
      $result = json_decode($result);
      $sendData = array();
      foreach ($result->results as $transaction) {
-         print_r($transaction);
-         print_r(nl2br("\n"));
+         $data  = array();
+         $data['transction_id']  = $transaction->transaction_id;
+         $data['customer_token'] = $transaction->customer_token;
+         $data['customer_id'] = $transaction->customer_id;
+         $data['status'] = $transaction->status;
+         $data['action'] = $transaction->action;
+         $data['authorization_amount'] = $transaction->authorization_amount;
+         $data['authorization_code'] = $transaction->authorization_code;
+         $data['entered_by'] = $data->entered_by;
+         $data['received_date'] = $data->received_date;
+         $data['first_name'] = $data->billing_address->first_name;
+         $data['last_name'] = $data->billing_address->last_name;
+         $data['masked_account_number'] = $data->echeck->masked_account_number;
      }
     }
 
