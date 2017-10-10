c762a8e55a79bd273860ef2951379467497f55e1
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-11 05:08:25 +0530

    access token

--- var/www/html/getDocumentPreview.php
+++ var/www/html/getDocumentPreview.php
@@ -7,7 +7,7 @@
 	if($_SESSION['hoa_community_id'] == 1)
 		$accessToken = '0gTJRfMcSHAAAAAAAAAADNfolm5IYvkINbXQpejgF8X2Hoy_6kXOlJemzq1a-588';
 	else if($_SESSION['hoa_community_id'] == 2)
-		$accessToken = 'QwUjEm5GAkAAAAAAAAAAVDjZsvVGfEhuiibnS0Zd-m96gHxqjZmpb2NTrT0g2Rd_';
+		$accessToken = 'QwUjEm5GAkAAAAAAAAAADocHK4CgCJoBl2A8-fe9Fs42E06qkDqJA2S9YPwGbZyF';
 
 	$client = new Dropbox\Client($accessToken, $appName, 'UTF-8');
 
