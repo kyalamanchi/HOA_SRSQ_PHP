c6117cc679afd201cd6ec239d03f2f1c20a71fd6
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-11 10:36:43 +0530

    dashboard

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -149,7 +149,7 @@
 
 						<div class="row">
 
-							<div class="col-md-12">
+							<div class="table-responsive col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
 								
 								<!-- Tabs-->
 								<ul class="nav nav-tabs">
