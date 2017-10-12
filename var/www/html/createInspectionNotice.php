dc62054e842d3c673bd712b8b105c88f60f45557
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-12 18:25:51 +0530

    changes

--- var/www/html/createInspectionNotice.php
+++ var/www/html/createInspectionNotice.php
@@ -222,9 +222,8 @@ function getSubCategory(){
   request.onreadystatechange = function(){
     if (request.readyState == XMLHttpRequest.DONE){
       hidePleaseWait();
-      alert(request.responseText);
-      $("inspectionSubCategory").append('<option>'+request.responseText+'</option>');
-      $("inspectionSubCategory").selectpicker("refresh");
+      $("#documentType").append('<option >'+'Some message'+'</option>');
+      $("#documentType").selectpicker('refresh');
     }
   }
 
