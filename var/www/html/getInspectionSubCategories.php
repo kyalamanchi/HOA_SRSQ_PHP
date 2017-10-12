8a2c0b829f8a17779bfa92307254eee3acfdfd79
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-12 23:40:50 +0530

    changes

--- var/www/html/getInspectionSubCategories.php
+++ var/www/html/getInspectionSubCategories.php
@@ -1,9 +1,9 @@
 <?php
-// $data = file_get_contents('php://input');
+$data = file_get_contents('php://input');
 error_reporting(E_ERROR | E_PARSE);
 try{
 if ($connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database")){
-		$query  = "SELECT * FROM INSPECTION_SUB_CATEGORY WHERE INSPECTION_CATEGORY_ID = (SELECT ID FROM INSPECTION_CATEGORY WHERE NAME='Animals  Pets')";
+		$query  = "SELECT * FROM INSPECTION_SUB_CATEGORY WHERE INSPECTION_CATEGORY_ID = (SELECT ID FROM INSPECTION_CATEGORY WHERE NAME='".$data."')";
 		$queryResult = pg_query($query);
 		$returnData = array();
 		while ($row = pg_fetch_assoc($queryResult)) {
@@ -14,7 +14,6 @@ if ($connection = pg_pconnect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazona
 		}
 		echo json_encode($returnData);
 	}
-}
 else {
 	throw new Exception("Error Processing Request", 1);
 }
