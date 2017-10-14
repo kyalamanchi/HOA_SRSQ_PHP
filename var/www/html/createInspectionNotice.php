ccd659e9d22beb553f88cb96fde6e76ff23f7989
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-15 00:18:30 +0530

    update

--- var/www/html/createInspectionNotice.php
+++ var/www/html/createInspectionNotice.php
@@ -276,7 +276,18 @@ function getSubCategory(){
   <hr>
   <div class="container">
     <h2>Quick Send</h2>
-
+      <div class="row-fluid" style="float: left;">
+      <h4>HOA ID</h4>
+      <select class="selectpicker" data-show-subtext="true" data-live-search="true" id="hoaID" onchange="changeDetails();">
+      <?php
+        echo '<option></option>';
+        foreach ($hoaIDArray as $key => $value) {
+          echo '<option data-subtext="'.$value.'">'.$key.'</option>';
+        }
+      ?>
+      </select>
+    </div>
+    
     <hr>
     <form>
     <div class="row-fluid" style="float: left;">
