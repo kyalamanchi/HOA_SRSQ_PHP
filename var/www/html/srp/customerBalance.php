2c568ee4b12e92576fe06f68f811bcbbbe490884
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-14 19:14:19 +0530

    changes

--- var/www/html/srp/customerBalance.php
+++ var/www/html/srp/customerBalance.php
@@ -206,7 +206,7 @@
 
 											if(!isset($_POST['mode']))
 											{
-												if($balance > 0.0)
+												if($total_balance > 0.0)
 													echo "<tr><td>$name<br>($hoa_id)</td><td>$living_in<br>($home_id)</td><td><i class='fa fa-envelope'></i> $email<br><i class='fa fa-phone'></i> $cell_no</td><td>$ $total_charges - $ $total_payments = <strong>$ $total_balance</strong></td><td><i class='fa fa-print'></i> Invoice</td></tr>";
 											}
 											else
