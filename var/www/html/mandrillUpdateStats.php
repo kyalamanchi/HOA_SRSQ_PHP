df07d4cfa5630d9bcc3d3221dd2a63b4ed28c786
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-18 01:19:28 +0530

    changes

--- var/www/html/mandrillUpdateStats.php
+++ var/www/html/mandrillUpdateStats.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 date_default_timezone_set('America/Los_Angeles');
 $connection  = pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
