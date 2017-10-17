0506c449cbda72f087435a3715921696c8af7c1e
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-17 22:06:15 +0530

    update

--- var/www/html/Automation.php
+++ var/www/html/Automation.php
@@ -74,7 +74,7 @@
       </h5>
     </div>
 
-    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+    <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
       <div class="card-block">
         <?php
         $connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
