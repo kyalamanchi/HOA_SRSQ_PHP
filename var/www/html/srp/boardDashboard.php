c87660f5c33ffb0cb456d01a465693408fda8dfb
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-17 22:59:21 +0530

    changes

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -599,13 +599,13 @@
 
 											<div class='row'>
 
-												<div class='col-md-4'>
+												<div class='col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12' style='background-color: sky-blue;'>
 
 													January
 
 												</div>
 
-												<div class='col-md-8'>
+												<div class='col-xl-9 col-lg-9 col-md-8 col-sm-6 col-xs-12'>
 
 													<div class='row'>
 
