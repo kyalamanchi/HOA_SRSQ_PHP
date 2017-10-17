5af1e775e2c3d966ad8c3ef50304e7ca8eaa9c53
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-17 23:07:38 +0530

    changes

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -599,13 +599,13 @@
 
 											<div class='row'>
 
-												<div class='col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12'>
+												<div class='col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12'>
 
-													January
+													<h3 class='h3'>January</h3>
 
 												</div>
 
-												<div class='col-xl-9 col-lg-9 col-md-8 col-sm-6 col-xs-12'>
+												<div class='col-xl-10 col-lg-10 col-md-9 col-sm-8 col-xs-12'>
 
 													<div class='row'>
 
