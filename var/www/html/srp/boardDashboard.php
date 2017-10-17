5ec1fb9b401cca77884161e0b4fb4bb18a0ece27
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-17 23:02:36 +0530

    testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -599,13 +599,13 @@
 
 											<div class='row'>
 
-												<div class='col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12' style='background-color: blue;'>
+												<div class='col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12'>
 
 													January
 
 												</div>
 
-												<div class='col-xl-9 col-lg-9 col-md-8 col-sm-6 col-xs-12'>
+												<div class='col-xl-9 col-lg-9 col-md-8 col-sm-6 col-xs-12' style='background-color: blue;'>
 
 													<div class='row'>
 
