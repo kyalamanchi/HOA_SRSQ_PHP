d6c12d54b43395cf05999b72a3370643e6954cd4
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-17 22:41:47 +0530

    update

--- var/www/html/Automation.php
+++ var/www/html/Automation.php
@@ -6,6 +6,7 @@
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script type="text/javascript">
     function updatePayments(){
         document.getElementById("payResult").innerHTML = "";
@@ -45,8 +46,16 @@
         }
     }
     function updateAll(){
-      
+      swal("Payments,Agreements,Billing Statements will be updated.","","success");
+      var request = new XMLHttpRequest();
+      request.open("POST","https://hoaboardtime.com/getHoaIDDetails.php",true);
+      request.send( $("#hoaID").find("option:selected").text() );
+      request.onreadystatechange  = function(){
+      if ( request.readyState == XMLHttpRequest.DONE ){
+          swal("Updation Complete","","success");
+      }
     }
+  }
 </script>
 <style type="text/css">
   .pull-right{
