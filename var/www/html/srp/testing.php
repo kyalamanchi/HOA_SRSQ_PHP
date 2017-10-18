fd802dfe65d4073ca31321b8887bc9992170caa2
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-18 08:46:54 +0530

    testing

--- var/www/html/srp/testing.php
+++ var/www/html/srp/testing.php
@@ -2,7 +2,7 @@
 $url = 'https://content.dropboxapi.com/2/files/download';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer QwUjEm5GAkAAAAAAAAAAN-KemUHI72QOlDsQxtH6H9JlRixSoi1fqq7D7BCHrNFm','Dropbox-API-Arg: {"path": "id:FM3YTRX_UaAAAAAAAAAATQ"}'));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer QwUjEm5GAkAAAAAAAAAAN-KemUHI72QOlDsQxtH6H9JlRixSoi1fqq7D7BCHrNFm','Dropbox-API-Arg: {"path": "id:xyPla0oGjJAAAAAAAAAHRQ"}'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $response = curl_exec($ch);
 $file = fopen('data.pdf', 'w');
