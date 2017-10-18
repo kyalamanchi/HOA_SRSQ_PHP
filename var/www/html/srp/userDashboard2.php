6745f0f12b9a33d851449063818bcf7a7ccff3dd
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-18 20:41:46 +0530

    grid

--- var/www/html/srp/userDashboard2.php
+++ var/www/html/srp/userDashboard2.php
@@ -421,13 +421,13 @@
 																				
 																					<div class='row'>
 
-																						<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12'>
+																						<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center'>
 
 																							<label><strong>Living Status</strong></label><br>
 
 																						</div>
 
-																						<div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6 text-center'>
+																						<div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
 
 																							<input type='radio'";
 
