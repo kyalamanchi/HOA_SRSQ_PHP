a88a4bd3f1aed3e9b553e63c43c2e1badfec9ebf
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-18 22:48:07 +0530

    changes

--- var/www/html/boardUserDashboard2.php
+++ var/www/html/boardUserDashboard2.php
@@ -1894,9 +1894,9 @@
                                   foreach ($obj->results as $key) 
                                   {  
 
-                                    if(true)
+                                    if($key->customer_id == $hoa_id)
                                       echo "<tr><td>".date('m-d-Y', strtotime($key->received_date))."</td><td>".$key->customer_id."</td><td>".$key->authorization_code."</td><td>".$key->status."</td><td>$ ".$key->authorization_amount."</td></tr>";
-
+                                    
                                   }
 
                                                                               
