39b1795deaf43bf53d00ee8abc527cae94136807
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-19 03:10:59 +0530

    changes

--- var/www/html/createInspectionNotice.php
+++ var/www/html/createInspectionNotice.php
@@ -380,7 +380,7 @@ request.onreadystatechange = function(){
     </div>
       <div style="width: 25%;float: left;padding-left: 10px;">
       <h4>HOME ADDRESS</h4>
-      <input type="email" class="form-control" id="home_id" aria-describedby="homeID" placeholder="HOME ID" disabled="disabled" >
+      <input type="email" class="form-control" id="home_id" aria-describedby="homeID" placeholder="HOME ADDRESS" disabled="disabled" >
       <small id="emailHelp" class="form-text text-muted"></small>
       </div>
       <div style="clear: both;"></div>
