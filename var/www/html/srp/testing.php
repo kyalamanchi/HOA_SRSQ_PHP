5eae24c3d28937c0c321667e4866ee288bf0ddfd
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-19 23:53:55 +0530

    testing

--- var/www/html/srp/testing.php
+++ var/www/html/srp/testing.php
@@ -7,6 +7,6 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $response = curl_exec($ch);
 $file = fopen('data.pdf', 'w');
 fwrite($file, $response);
-header('Content-type: application/pdf'); header('Content-Disposition: inline; filename="data.pdf"'); 
-readfile('data.pdf');
+fclose($file);
+header('Locaton: data.pdf');
 ?>
