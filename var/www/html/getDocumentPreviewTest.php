255c4cb1f93b0b4dbb7b1c0243ed6d9b6cf33151
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-22 02:46:35 +0530

    changes

--- var/www/html/getDocumentPreviewTest.php
+++ var/www/html/getDocumentPreviewTest.php
@@ -1,7 +1,7 @@
 <?php
 	
 	session_start();
-	
+
 	if($_SESSION['hoa_community_id'] == 1)
 	{
 		$accessToken = '0gTJRfMcSHAAAAAAAAAADNfolm5IYvkINbXQpejgF8X2Hoy_6kXOlJemzq1a-588';
@@ -17,7 +17,7 @@
 	$url = 'https://content.dropboxapi.com/2/files/download';
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer QwUjEm5GAkAAAAAAAAAADocHK4CgCJoBl2A8-fe9Fs42E06qkDqJA2S9YPwGbZyF','Dropbox-API-Arg: {"path": "'.$path.'"}'));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$accessToken,'Dropbox-API-Arg: {"path": "'.$path.'"}'));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	$response = curl_exec($ch);
 	header('Content-type: application/pdf'); header('Content-Disposition: inline; filename="'.$description.'.pdf"'); 
