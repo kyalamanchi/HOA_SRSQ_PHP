708f7c9a2b8a5152061f1dfe1a4eac75c5d1a396
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-22 02:09:20 +0530

    test

--- var/www/html/testdb.php
+++ var/www/html/testdb.php
@@ -2,7 +2,7 @@
 $url = 'https://content.dropboxapi.com/2/files/download';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer n-Bgs_XVPEAAAAAAAAEQYgvfkzJWzxx59jqgvKQeXbtsYt-eXdZ6BNRYivEGKVGB','Dropbox-API-Arg: {"path": "Alchemy/2017Ad.pdf"}'));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer n-Bgs_XVPEAAAAAAAAEQYgvfkzJWzxx59jqgvKQeXbtsYt-eXdZ6BNRYivEGKVGB','Dropbox-API-Arg: {"path": "/Alchemy/2017Ad.pdf"}'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $response = curl_exec($ch);
 header('Content-type: application/pdf'); header('Content-Disposition: inline; filename="a.pdf"'); 
