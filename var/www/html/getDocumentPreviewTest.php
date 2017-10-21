ede88274eb3b3f0b2efdf5e3d8fc1e3895bbe866
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-22 03:00:01 +0530

    changes

--- var/www/html/getDocumentPreviewTest.php
+++ var/www/html/getDocumentPreviewTest.php
@@ -4,12 +4,12 @@
 
 
 
-	if($_SESSION['hoa_community_id'] == 1)
+	if($_GET['cid'] == 1)
 	{
 		$accessToken = '0gTJRfMcSHAAAAAAAAAADNfolm5IYvkINbXQpejgF8X2Hoy_6kXOlJemzq1a-588';
 
 	}
-	else if($_SESSION['hoa_community_id'] == 2){
+	else if($_GET['cid'] == 2){
 		$accessToken = 'QwUjEm5GAkAAAAAAAAAADocHK4CgCJoBl2A8-fe9Fs42E06qkDqJA2S9YPwGbZyF';
 	}
 
