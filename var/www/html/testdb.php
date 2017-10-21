470ec1d3b5d2905e66234e71cfef5eb4e63e715f
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-22 02:16:24 +0530

    changes

--- var/www/html/testdb.php
+++ var/www/html/testdb.php
@@ -2,7 +2,7 @@
 $url = 'https://content.dropboxapi.com/2/files/download';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer n-Bgs_XVPEAAAAAAAAEQYgvfkzJWzxx59jqgvKQeXbtsYt-eXdZ6BNRYivEGKVGB','Dropbox-API-Arg: {"path": "/Billing_Statements/2017-08-21.pdf"}'));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer QwUjEm5GAkAAAAAAAAAAXIxENhxZTsAt7wiYseon4tH28AjCi8TBwUxzjKrlwnT-','Dropbox-API-Arg: {"path": "/SRSQ_HOA/Documents/Minutes/SRSQ_Minutes_1999_October__Board_Signed.pdf"}'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $response = curl_exec($ch);
 header('Content-type: application/pdf'); header('Content-Disposition: inline; filename="a.pdf"'); 
