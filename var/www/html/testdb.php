41e207e3f010c0bce1019e8b6088ccdead2ef26b
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-22 01:32:04 +0530

    changes

---
+++ var/www/html/testdb.php
@@ -0,0 +1,11 @@
+<?php
+$url = 'https://content.dropboxapi.com/2/files/download';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer QwUjEm5GAkAAAAAAAAAAN-KemUHI72QOlDsQxtH6H9JlRixSoi1fqq7D7BCHrNFm','Dropbox-API-Arg: {"path": "id:FM3YTRX_UaAAAAAAAAAATQ"}'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$response = curl_exec($ch);
+
+header('Content-type: application/pdf'); header('Content-Disposition: inline; filename="data.pdf"'); 
+echo $response;
+?>
