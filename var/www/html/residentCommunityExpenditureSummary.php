4ad799e84fc51d94d583063c4bb72eef727e3cfb
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-22 00:07:23 +0530

    scripts

--- var/www/html/residentCommunityExpenditureSummary.php
+++ var/www/html/residentCommunityExpenditureSummary.php
@@ -346,20 +346,23 @@
     </script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="plugins/morris/morris.min.js"></script>
     <script src="plugins/sparkline/jquery.sparkline.min.js"></script>
     <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
     <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
     <script src="plugins/knob/jquery.knob.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-    <script src="plugins/daterangepicker/daterangepicker.js"></script>
-    <script src="plugins/datepicker/bootstrap-datepicker.js"></script>
     <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
     <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
     <script src="plugins/fastclick/fastclick.js"></script>
     <script src="dist/js/app.min.js"></script>
     <script src="dist/js/pages/dashboard.js"></script>
     <script src="dist/js/demo.js"></script>
+
+    <script>
+      $(function () {
+        $("#example1").DataTable({ "pageLength": 50, "order": [[ 0, "desc" ]] });
+      });
+    </script>
 
   </body>
 
