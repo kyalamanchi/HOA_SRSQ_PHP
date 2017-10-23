6babf0fd9eb79663e98b57e271ad87288927e6f9
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-23 16:38:11 +0530

    board

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -256,7 +256,7 @@
 
 												<div class='col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6'>
 
-													<div class='counter h6'>
+													<div class='counter h3'>
 
 														<div class='counter-number'>
 															
@@ -276,7 +276,7 @@
 
 												<div class='col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6'>
 
-													<div class='counter h6'>
+													<div class='counter h3'>
 
 														<div class='counter-number'>
 
