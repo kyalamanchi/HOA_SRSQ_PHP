4fc0f47d1af3f3cd1dfcf649716d9a2837a3b0d6
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-24 02:58:29 +0530

    update

--- var/www/html/agreementTest.php
+++ var/www/html/agreementTest.php
@@ -7,22 +7,22 @@ print_r($files1);
 print_r($files2);
 print_r("\n\n\n");
 
-$fileNames = array("AutomationTest.php","InspectionManagementTest.php","boardCommunityDisclosuresTest.php","boardCurrentMonthLatePaymentsTest.php","boardCustomerBalanceTest.php","boardDashboardTest.php","createInspectionNoticeTest.php","forteFullLoadTest.php","indexTest.php","loginTest.php","paymentPageSRSQTest.php","residentDocumentManagementTest.php","residentDashboardTest.php","residentDocumentManagementTest.php","residentDashboardTest.php","paymentPageSRSQTest.php");
-foreach ($fileNames as $key) {
-	print_r($key);
-	print_r(nl2br("\n"));
-	if ( file_exists($key) ){
-	print_r($key."File Found");
-	unlink($key);
-	}
-	print_r(nl2br("\n"));
-	if ( file_exists($key) ){
-		print_r("File found".$key);
-		print_r(nl2br("\n"));
-	}
-	else {
-		print_r($key."File removed");
-	}
-	print_r(nl2br("\n"));
-}
+// $fileNames = array("AutomationTest.php","InspectionManagementTest.php","boardCommunityDisclosuresTest.php","boardCurrentMonthLatePaymentsTest.php","boardCustomerBalanceTest.php","boardDashboardTest.php","createInspectionNoticeTest.php","forteFullLoadTest.php","indexTest.php","loginTest.php","paymentPageSRSQTest.php","residentDocumentManagementTest.php","residentDashboardTest.php","residentDocumentManagementTest.php","residentDashboardTest.php","paymentPageSRSQTest.php");
+// foreach ($fileNames as $key) {
+// 	print_r($key);
+// 	print_r(nl2br("\n"));
+// 	if ( file_exists($key) ){
+// 	print_r($key."File Found");
+// 	unlink($key);
+// 	}
+// 	print_r(nl2br("\n"));
+// 	if ( file_exists($key) ){
+// 		print_r("File found".$key);
+// 		print_r(nl2br("\n"));
+// 	}
+// 	else {
+// 		print_r($key."File removed");
+// 	}
+// 	print_r(nl2br("\n"));
+// }
 ?>
