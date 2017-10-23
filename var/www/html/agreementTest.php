e4a3b355390f693195f5facf0e321bb92b2e60ee
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-24 02:24:45 +0530

    update

--- var/www/html/agreementTest.php
+++ var/www/html/agreementTest.php
@@ -5,4 +5,11 @@ $files2 = scandir($dir, 1);
 
 print_r($files1);
 print_r($files2);
+print_r("\n\n\n");
+
+$fileNames = array("AutomationTest.php","InspectionManagementTest.php","boardCommunityDisclosuresTest.php","boardCurrentMonthLatePaymentsTest.php","boardCustomerBalanceTest.php","boardDashboardTest.php","createInspectionNoticeTest.php","forteFullLoadTest.php","indexTest.php","loginTest.php","paymentPageSRSQTest.php","residentDocumentManagementTest.php","residentDashboardTest.php","residentDocumentManagementTest.php","residentDashboardTest.php","paymentPageSRSQTest.php");
+foreach ($fileNames as $key) {
+	print_r($key);
+	print_r(nl2br("\n"));
+}
 ?>
