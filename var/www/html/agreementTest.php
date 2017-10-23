0793d6bdfd782df8f790784444ee4d6d69efe635
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-24 03:11:46 +0530

    fix

--- var/www/html/agreementTest.php
+++ var/www/html/agreementTest.php
@@ -5,21 +5,21 @@ print_r($files1);
 print_r("\n\n\n");
 
 $fileNames = array("createInspectionNoticeTest.php","loginTest.php");
-foreach ($fileNames as $key) {
-	print_r($key);
-	print_r(nl2br("\n"));
-	if ( file_exists($key) ){
-	print_r($key."File Found");
-	unlink($key);
-	}
-	print_r(nl2br("\n"));
-	if ( file_exists($key) ){
-		print_r("File found".$key);
-		print_r(nl2br("\n"));
-	}
-	else {
-		print_r($key."File removed");
-	}
-	print_r(nl2br("\n"));
-}
+// foreach ($fileNames as $key) {
+// 	print_r($key);
+// 	print_r(nl2br("\n"));
+// 	if ( file_exists($key) ){
+// 	print_r($key."File Found");
+// 	unlink($key);
+// 	}
+// 	print_r(nl2br("\n"));
+// 	if ( file_exists($key) ){
+// 		print_r("File found".$key);
+// 		print_r(nl2br("\n"));
+// 	}
+// 	else {
+// 		print_r($key."File removed");
+// 	}
+// 	print_r(nl2br("\n"));
+// }
 ?>
