5d7fd31440913457f28b761e1c25f7ba015053d8
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-24 03:19:34 +0530

    changes

--- var/www/html/fileManager.php
+++ var/www/html/fileManager.php
@@ -4,22 +4,22 @@ $files1 = scandir($dir);
 print_r($files1);
 print_r("\n\n\n");
 
-$fileNames = array("createInspectionNoticeTest.php","loginTest.php","agreementTest.php");
-foreach ($fileNames as $key) {
-	print_r($key);
-	print_r(nl2br("\n"));
-	if ( file_exists($key) ){
-	print_r($key."File Found");
-	unlink($key);
-	}
-	print_r(nl2br("\n"));
-	if ( file_exists($key) ){
-		print_r("File found".$key);
-		print_r(nl2br("\n"));
-	}
-	else {
-		print_r($key."File removed");
-	}
-	print_r(nl2br("\n"));
-}
+// $fileNames = array("createInspectionNoticeTest.php","loginTest.php","agreementTest.php");
+// foreach ($fileNames as $key) {
+// 	print_r($key);
+// 	print_r(nl2br("\n"));
+// 	if ( file_exists($key) ){
+// 	print_r($key."File Found");
+// 	unlink($key);
+// 	}
+// 	print_r(nl2br("\n"));
+// 	if ( file_exists($key) ){
+// 		print_r("File found".$key);
+// 		print_r(nl2br("\n"));
+// 	}
+// 	else {
+// 		print_r($key."File removed");
+// 	}
+// 	print_r(nl2br("\n"));
+// }
 ?>
