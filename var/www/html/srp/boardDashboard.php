84957c9df0e7b56a8e0e47e9defb6a76182fc779
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-23 17:38:57 +0530

    quickbooks balance

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -163,6 +163,79 @@
 
 		?>
 
+		<?php
+
+			if($community_id == 1)
+            { 
+                      
+                $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145854171542/account/77?minorversion=8');      
+                      
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Intuit-qbov3-postman-collection1','Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprd0JzDPeMNuATqXcic8hnusenW2",oauth_token="qyprdxuMeT1noFaS5g6aywjSOkFQo16WnvwigzPbxQ01LPYF",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1492203509",oauth_nonce="Q2Ck7t",oauth_version="1.0",oauth_signature="jzXGHD9VKI6fxwrXaWg90HQgFuI%3D"'));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                            
+                $result = curl_exec($ch);
+                $json_decode = json_decode($result,TRUE);
+                $srp_primarySavings = $json_decode['Account'];
+                $srp_current_balance = $srp_primarySavings['CurrentBalance'];
+                            
+                curl_close($ch);
+
+                $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145854171542/account/74?minorversion=8');      
+                            
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Intuit-qbov3-postman-collection1','Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprd0JzDPeMNuATqXcic8hnusenW2",oauth_token="qyprdxuMeT1noFaS5g6aywjSOkFQo16WnvwigzPbxQ01LPYF",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1508539532",oauth_nonce="2nX9kd69aNw",oauth_version="1.0",oauth_signature="5ZScoTRHF28D3YT0kHO27%2Br8Hvo%3D"'));
+                      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                            
+                $result2 = curl_exec($ch);
+                $json_decode2 = json_decode($result2,TRUE);
+                $srp = $json_decode2['Account'];
+                $srp_savings_balance = $srp['CurrentBalance'];
+
+            }
+            else if($community_id == 2)
+            {
+                      
+                $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/account/33?minorversion=8');      
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Intuit-qbov3-postman-collection1','Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1506682054",oauth_nonce="skPZikoZJCt",oauth_version="1.0",oauth_signature="aEBIdXcJdXSWiLp5k9gxlVuvsbs%3D"'));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+                $result = curl_exec($ch);
+                $json_decode = json_decode($result,TRUE);
+                $srp_primarySavings = $json_decode['Account'];
+                $srp_primary_Savings_CurrentBalance = $srp_primarySavings['CurrentBalance'];
+
+                curl_close($ch);
+
+                 $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/account/32?minorversion=8');      
+                      
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Intuit-qbov3-postman-collection1','Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1492203509",oauth_nonce="Q2Ck7t",oauth_version="1.0",oauth_signature="5IDpz%2F%2FItyjMYbh4Ke3JoBx3YGY%3D"'));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+                $result2 = curl_exec($ch);
+                $json_decode2 = json_decode($result2,TRUE);
+                $srp = $json_decode2['Account'];
+                $srp_savings = $srp['CurrentBalance'];
+
+                curl_close($ch);
+
+                $ch  = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/account/31?minorversion=8');
+                      
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Intuit-qbov3-postman-collection1','Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1506681985",oauth_nonce="H7DXVHb2Qdp",oauth_version="1.0",oauth_signature="HDWt%2BfIz3NrAhJE9fO9G%2FI8Q%2Fcg%3D"'));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+                $result3 = curl_exec($ch);
+                $json_decode3 = json_decode($result3,TRUE);
+                $srsq_third_Account = $json_decode3['Account'];
+                $srsq_third_Account_Balance = $srsq_third_Account['CurrentBalance'];
+            
+            }
+
+		?>
+
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
@@ -528,9 +601,9 @@
 
 																	<div class='counter-number'>
 																		
-																		<a href='currentMonthPayments.php'>
+																		<a href='communityIncome.php'>
 
-																			".round($amount_received, 1)."
+																			$ ".$srp_savings_balance."
 
 																		</a>
 																			
@@ -548,9 +621,9 @@
 
 																	<div class='counter-number'>
 
-																		<a href='currentMonthPayments.php'>
+																		<a href='communityIncome.php'>
 
-																			".round($members_paid, 1)."
+																			$ ".$srp_current_balance."
 
 																		</a>
 
@@ -582,9 +655,9 @@
 
 																	<div class='counter-number'>
 																		
-																		<a href='currentMonthPayments.php'>
+																		<a href='communityIncome.php'>
 
-																			".round($amount_received, 1)."
+																			$ ".$srp_primary_Savings_CurrentBalance."
 
 																		</a>
 																			
@@ -602,9 +675,9 @@
 
 																	<div class='counter-number'>
 
-																		<a href='currentMonthPayments.php'>
+																		<a href='communityIncome.php'>
 
-																			".round($members_paid, 1)."
+																			$ ".$srp_savings."
 
 																		</a>
 
@@ -622,9 +695,9 @@
 
 																	<div class='counter-number'>
 
-																		<a href='currentMonthPayments.php'>
+																		<a href='communityIncome.php'>
 
-																			".round($members_paid, 1)."
+																			$ ".$srsq_third_Account_Balance."
 
 																		</a>
 
