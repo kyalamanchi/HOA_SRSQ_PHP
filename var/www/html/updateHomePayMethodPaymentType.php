e721dd203fe915970e6123c992d56198315414ae
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-24 16:57:21 +0530

    changes

--- var/www/html/updateHomePayMethodPaymentType.php
+++ var/www/html/updateHomePayMethodPaymentType.php
@@ -2,10 +2,11 @@
 date_default_timezone_set('America/Los_Angeles');
 $connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
 if ( $connection ){
-	$query = "SELECT A.HOA_ID FROM  (SELECT PROCESS_DATE,HOA_ID FROM CURRENT_PAYMENTS CP1 WHERE PROCESS_DATE = ( SELECT MAX(PROCESS_DATE) FROM CURRENT_PAYMENTS WHERE CP1.HOA_ID = CURRENT_PAYMENTS.HOA_ID ) ORDER BY PROCESS_DATE DESC) AS A ORDER BY HOA_ID";
+	$query = "SELECT A.HOA_ID,A.PAYMENT_TYPE_ID FROM  (SELECT PROCESS_DATE,HOA_ID,PAYMENT_TYPE_ID FROM CURRENT_PAYMENTS CP1 WHERE PROCESS_DATE = ( SELECT MAX(PROCESS_DATE) FROM CURRENT_PAYMENTS WHERE CP1.HOA_ID = CURRENT_PAYMENTS.HOA_ID ) ORDER BY PROCESS_DATE DESC) AS A ORDER BY HOA_ID";
 	$qr = pg_query($query);
 	while ($row = pg_fetch_assoc($qr)) {
-		print_r($row['hoa_id']);
+		$qu = "UPDATE HOME_PAY_METHOD SET PAYMENT_TYPE_ID=".$row['payment_type_id']." WHERE HOA_ID=".$row['hoa_id'];
+		print_r($qu);
 		print_r(nl2br("\n"));
 	}
 }
