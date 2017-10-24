b94c9a168fdaf54994252275f22e56f8f61e6419
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-24 17:01:05 +0530

    changes

--- var/www/html/updateHomePayMethodPaymentType.php
+++ var/www/html/updateHomePayMethodPaymentType.php
@@ -5,9 +5,8 @@ if ( $connection ){
 	$query = "SELECT A.HOA_ID,A.PAYMENT_TYPE_ID FROM  (SELECT PROCESS_DATE,HOA_ID,PAYMENT_TYPE_ID FROM CURRENT_PAYMENTS CP1 WHERE PROCESS_DATE = ( SELECT MAX(PROCESS_DATE) FROM CURRENT_PAYMENTS WHERE CP1.HOA_ID = CURRENT_PAYMENTS.HOA_ID ) ORDER BY PROCESS_DATE DESC) AS A ORDER BY HOA_ID";
 	$qr = pg_query($query);
 	while ($row = pg_fetch_assoc($qr)) {
-		$qu = "UPDATE HOME_PAY_METHOD SET PAYMENT_TYPE_ID=".$row['payment_type_id']." WHERE HOA_ID=".$row['hoa_id'];
-		print_r($qu);
-		print_r(nl2br("\n"));
+		$qu = "UPDATE HOME_PAY_METHOD SET PAYMENT_TYPE_ID=".$row['payment_type_id'].",updated_on='".date('Y-m-d H:i:s')."',updated_by=401 WHERE HOA_ID=".$row['hoa_id'];
+		pg_query($qu);
 	}
 }
 ?>
