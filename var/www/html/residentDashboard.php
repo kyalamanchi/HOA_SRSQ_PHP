0b36493d46240fffe2e4a0a7710f11b4fe41602b
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-26 11:02:52 +0530

    account statement

--- var/www/html/residentDashboard.php
+++ var/www/html/residentDashboard.php
@@ -458,7 +458,7 @@
 
             <div class="col-xl-3 col-lg-3 col-md-4 col-sm-12 col-xs-12">
 
-              <a href='https://hoaboardtime.com/residentInvoice.php'>
+              <a href='https://hoaboardtime.com/boardPrintCustomerInvoice.php?home_id=$home_id&hoa_id=$hoa_id'>
 
                 <div class="row container-fluid text-left">
 
--- var/www/html/srp/residentDashboard.php
+++ var/www/html/srp/residentDashboard.php
@@ -193,7 +193,7 @@
 													
 										<?php 
 
-											echo "<a target='_blank' href='accountStatement.php?home_id=$home_id&hoa_id=$hoa_id&community_id=$community_id'><img src='account_statement.png' alt='Account Statement Icon'></a>";
+											echo "<a target='_blank' href='accountStatement.php?home_id=$home_id&hoa_id=$hoa_id'><img src='account_statement.png' alt='Account Statement Icon'></a>";
 
 										?>
 														
