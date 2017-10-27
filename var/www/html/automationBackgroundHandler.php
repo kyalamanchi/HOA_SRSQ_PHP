d79a50b9fc97c214f09509b29c9e09db2a9dbe39
Dhivysh <Dhivysh@users.noreply.github.com>
2017-10-27 17:02:46 +0530

    changes

--- var/www/html/automationBackgroundHandler.php
+++ var/www/html/automationBackgroundHandler.php
@@ -312,6 +312,32 @@ pg_query($query);
 $query = "INSERT INTO BACKGROUND_JOBS(\"COMMUNITY_ID\",\"JOB_CATEGORY_ID\",\"START_TIME\") VALUES(2,4,'".date('Y-m-d H:i:s')."')";
 pg_query($query);
 }
+
+else if ( $_GET['id'] == 5 ){
+	$message  = "Please wait.....";
+	echo 'data: '.$message."\n\n";  
+	ob_end_flush();
+	flush();
+	$message  = "Updating SMS data...Please wait...";
+	echo 'data: '.$message."\n\n";  
+	ob_end_flush();
+	flush();
+	$req = curl_init();
+	curl_setopt($req, CURLOPT_URL,"https://hoaboardtime.com/mandrillUpdateStats.php");
+	curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($req);
+	$id = date('Y-m-d H:i:s');
+	$message  = "Done!!!";
+	echo "id: $id\n";
+	echo 'data: '.$message."\n\n";  
+	ob_end_flush();
+	flush();
+	$query = "INSERT INTO BACKGROUND_JOBS(\"COMMUNITY_ID\",\"JOB_CATEGORY_ID\",\"START_TIME\") VALUES(1,6,'".date('Y-m-d H:i:s')."')";
+	pg_query($query);
+	$query = "INSERT INTO BACKGROUND_JOBS(\"COMMUNITY_ID\",\"JOB_CATEGORY_ID\",\"START_TIME\") VALUES(2,6,'".date('Y-m-d H:i:s')."')";
+	pg_query($query);
+}
+
 }
 else {
 
@@ -599,6 +625,30 @@ pg_query($query);
 $query = "INSERT INTO BACKGROUND_JOBS(\"COMMUNITY_ID\",\"JOB_CATEGORY_ID\",\"START_TIME\") VALUES(2,4,'".date('Y-m-d H:i:s')."')";
 pg_query($query);
 
+
+
+	$message  = "Please wait.....";
+	echo 'data: '.$message."\n\n";  
+	ob_end_flush();
+	flush();
+	$message  = "Updating SMS data...Please wait...";
+	echo 'data: '.$message."\n\n";  
+	ob_end_flush();
+	flush();
+	$req = curl_init();
+	curl_setopt($req, CURLOPT_URL,"https://hoaboardtime.com/mandrillUpdateStats.php");
+	curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($req);
+	$id = date('Y-m-d H:i:s');
+	$message  = "Done!!!";
+	echo "id: $id\n";
+	echo 'data: '.$message."\n\n";  
+	ob_end_flush();
+	flush();
+	$query = "INSERT INTO BACKGROUND_JOBS(\"COMMUNITY_ID\",\"JOB_CATEGORY_ID\",\"START_TIME\") VALUES(1,6,'".date('Y-m-d H:i:s')."')";
+	pg_query($query);
+	$query = "INSERT INTO BACKGROUND_JOBS(\"COMMUNITY_ID\",\"JOB_CATEGORY_ID\",\"START_TIME\") VALUES(2,6,'".date('Y-m-d H:i:s')."')";
+	pg_query($query);
 
 }
 
