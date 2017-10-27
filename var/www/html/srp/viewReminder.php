47c4b93b7c43b334b4013c15ac8266a8ab1da194
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-27 21:04:01 +0530

    changes

--- var/www/html/srp/viewReminder.php
+++ var/www/html/srp/viewReminder.php
@@ -230,7 +230,7 @@
 			                          			if($update_date != '')
 			                          				$update_date = date('m-d-Y', strtotime($update_date));
 
-						                        echo "<tr><td>$open_date</td><td>$due_date</td><td>$update_date</td><td>$reminder_type</td><td>$comments</td><td>$vendor_assigned</td></tr>";
+						                        echo "<tr class='text-muted'><td>$open_date</td><td>$due_date</td><td>$update_date</td><td>$reminder_type</td><td>$comments</td><td>$vendor_assigned</td></tr>";
 
 			                        		}
 
