f6ca2ff99ef455511dbcd526702ed3c607043ee0
GeethChadalawada <geethchadalawada@gmail.com>
2017-10-27 20:30:21 +0530

    changes

--- var/www/html/srp/accountsPayable.php
+++ var/www/html/srp/accountsPayable.php
@@ -95,7 +95,7 @@
 				<!-- Content -->
 				<section class="module">
 						
-					<div class="container">
+					<div class="row">
 							
 						<div class='table-responsive col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
 						
