ff51e9876ab8a2dc48294824770291be77f55903
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-01 18:08:30 +0530

    testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -2848,7 +2848,7 @@
 
           }],
 
-          labels: ['January', 'February', 'March', 'April']
+          labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
         },
 
