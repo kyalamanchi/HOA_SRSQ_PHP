0afee2648ac551325f83b7f8440e310a585d2e0d
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-01 18:34:32 +0530

    testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -1430,13 +1430,13 @@
 
                       <div class='row'>
 
-                        <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                        <div class='col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12'>
                           
                           <canvas id="myChart1"></canvas>
 
                         </div>
 
-                        <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                        <div class='col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12'>
                           
                           <canvas id="myChart2"></canvas>
 
