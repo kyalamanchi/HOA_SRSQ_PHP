2673aa957a79da81330a07b0687c6faa17689330
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-01 18:21:21 +0530

    testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -2840,6 +2840,7 @@
             label: 'Line Dataset',
             steppedLine: true,
             borderColor: "#c080f9",
+            backgroundColor: "#cca6ed",
             pointBackgroundColor: "rgba(255,99,132,1)",
             pointBorderColor: "#fff",
             data: [ <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?>, <?php echo $total_homes; ?> ],
