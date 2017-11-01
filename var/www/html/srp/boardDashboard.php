e5ee8ce12db18b00faee4105b40481773204582f
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-01 15:05:36 +0530

    chanegs

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -2697,28 +2697,31 @@
     <script type="text/javascript">
       
       var ctx = document.getElementById('myChart').getContext('2d');
-      var chart = new Chart(ctx, {
-        
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-
-          labels: ["January", "February", "March", "April", "May", "June", "July"],
-          datasets: [{
-            label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-          }]
-
-        },
-
-        // Configuration options go here
-        options: {}
-
+      var myDoughnutChart = new Chart(ctx, {
+    
+        type: 'doughnut',
+        data: data
+        //options: options
+      
       });
+
+      data = {
+    
+        datasets: [{
+          
+          data: [10, 20, 30]
+
+        }],
+
+        labels: [
+
+          'Red',
+          'Yellow',
+          'Blue'
+        
+        ]
+
+      };
 
     </script>
 		
