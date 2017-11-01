4b78e94def1e15b4325466c97e93b6a94f5dd576
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-01 15:27:11 +0530

    testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -2711,13 +2711,7 @@
           datasets: [{
             
             data: [10, 20, 30],
-            backgroundColor: [
-
-              pattern.draw('circle', '#36a2eb'),
-              pattern.draw('diamond', '#cc65fe'),
-              pattern.draw('triangle', '#ffce56')
-
-            ]
+            backgroundColor: ['Red', 'Yellow', 'Blue']
 
           }],
 
