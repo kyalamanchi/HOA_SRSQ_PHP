865e488aaf10b57f2d4dd1e332598551ea25958e
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-02 05:14:47 +0530

    test

--- var/www/html/testdb.php
+++ var/www/html/testdb.php
@@ -8,4 +8,12 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $response = curl_exec($ch);
 // header('Content-type: application/pdf'); header('Content-Disposition: inline; filename="a.pdf"'); 
 echo $response;
+ $consumer_key     = 'qyprdRAm244oPXhP3miXslnVdpDfWF';
+            $consumer_secret  = 'i3EglyzIw5eIUBXRttNUd7xBc5zqyxZxguHlYtdo';
+            $access_token     = 'qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v';
+            $access_secret    = '1XKRXuoXJeSKp84LmA1SF6aPFrywn4tOsUpB0Sn5';
+
+            $oauth = new OAuth($consumer_key,$consumer_secret,OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_AUTHORIZATION);
+            print_r($oauth);
+            
 ?>
