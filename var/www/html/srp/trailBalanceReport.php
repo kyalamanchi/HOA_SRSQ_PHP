1bcbb237eef338d8763b3902b1d8595074c018ee
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-01 12:18:36 +0530

    changes

--- var/www/html/srp/trailBalanceReport.php
+++ var/www/html/srp/trailBalanceReport.php
@@ -116,7 +116,7 @@
 
 										if($community_id == 1)
 										{
-
+											
 											$ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145854171542/reports/TrialBalance?minorversion=8');
 								            curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
 								            curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent:Intuit-qbov3-postman-collection1','Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprd0JzDPeMNuATqXcic8hnusenW2",oauth_token="qyprdxuMeT1noFaS5g6aywjSOkFQo16WnvwigzPbxQ01LPYF",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1492203509",oauth_nonce="Q2Ck7t",oauth_version="1.0",oauth_signature="jzXGHD9VKI6fxwrXaWg90HQgFuI%3D"'));
@@ -124,10 +124,6 @@
 								            $result = curl_exec($ch);
 								            
 								            $result =  json_decode($result);
-
-								            print_r($result);
-
-								            die();
 
 								            foreach ($result->Rows->Row as $row) 
 								            {
