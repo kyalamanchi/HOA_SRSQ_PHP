5481f537817fbf6a5e2f5189fb970ea668889961
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-01 15:00:25 +0530

    chart testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -278,7 +278,9 @@
 		<link href="assets/css/animate.css" rel="stylesheet">
 		<!-- Template core CSS-->
 		<link href="assets/css/template.min.css" rel="stylesheet">
+
 	</head>
+
 	<body>
 
 		<!-- Layout-->
@@ -1407,6 +1409,12 @@
                           </div>
 
                         </div>
+
+                      </div>
+
+                      <div class='row'>
+
+                        <canvas id="myChart"></canvas>
 
                       </div>
 
@@ -2682,7 +2690,35 @@
 		<!-- Color Switcher (Remove these lines)-->
 		<!--script src="assets/js/style-switcher.min.js"></script-->
 
+    <!-- Chart JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+    <script type="text/javascript">
+      
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var chart = new Chart(ctx, {
+        
+        // The type of chart we want to create
+        type: 'line',
 
+        // The data for our dataset
+        data: {
+
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }]
+
+        },
+
+        // Configuration options go here
+        options: {}
+
+      });
+
+    </script>
 		
 	</body>
 
