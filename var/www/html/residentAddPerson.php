7683dd9a132dec8f45336496058d1fbf55797f8f
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-02 17:40:41 +0530

    testing

--- var/www/html/residentAddPerson.php
+++ var/www/html/residentAddPerson.php
@@ -20,7 +20,7 @@
 	$date = date("Y-m-d");
 	$year = date("Y");
 
-	print_r($result);
+	print_r($user_id);
 
 	$result = pg_query("INSERT INTO person(fname, lname, cell_no, email, hoa_id, home_id, community_id, is_active, role_type_id, relationship_id, valid_from, valid_until, updated_by, updated_on) VALUES ('$fname', '$lname', $cell_no, '$email', $hoa_id, $home_id, $community_id, 't', $role_type, $relationship, '$date', '12-31-$year', $user_id, '$date')");
 
