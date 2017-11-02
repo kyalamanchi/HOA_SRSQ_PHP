3eda5965480b7d79919077a40c7ff89ff93a2413
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-02 15:17:41 +0530

    testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -1439,23 +1439,13 @@
 
                       <div class='row'>
 
-                        <div class='col-xl-8 col-lg-10 col-md-12 col-sm-12 col-xs-12 offset-xl-2 offset-lg-1'>
+                        <div class='col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12'>
                           
                           <canvas id="myChart1"></canvas>
 
                         </div>
 
-                      </div>
-
-                    </div>
-
-                    <br>
-
-                    <div class='container'>
-
-                      <div class='row'>
-
-                        <div class='col-xl-8 col-lg-10 col-md-12 col-sm-12 col-xs-12 offset-xl-2 offset-lg-1'>
+                        <div class='col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12'>
                           
                           <canvas id="myChart2"></canvas>
 
@@ -1469,7 +1459,7 @@
 
                       <div class='row'>
 
-                        <div class='col-xl-8 col-lg-10 col-md-12 col-sm-12 col-xs-12 offset-xl-2 offset-lg-1'>
+                        <div class='col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12'>
                           
                           <canvas id="myChart3"></canvas>
 
