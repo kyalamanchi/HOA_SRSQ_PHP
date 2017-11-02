5ebfbbef0cde425ec4c54fb989560828f177b209
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-02 14:59:34 +0530

    testing

--- var/www/html/srp/boardDashboard.php
+++ var/www/html/srp/boardDashboard.php
@@ -1439,7 +1439,7 @@
 
                       <div class='row'>
 
-                        <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                        <div class='col-xl-8 col-lg-10 col-md-10 col-sm-12 col-xs-12 offset-xl-2 offset-lg-1 offset-md-1'>
                           
                           <canvas id="myChart1"></canvas>
 
@@ -1453,7 +1453,7 @@
 
                       <div class='row'>
 
-                        <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                        <div class='col-xl-8 col-lg-10 col-md-10 col-sm-12 col-xs-12 offset-xl-2 offset-lg-1 offset-md-1'>
                           
                           <canvas id="myChart2"></canvas>
 
