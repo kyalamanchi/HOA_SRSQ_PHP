9e4c9389baa939440a4fb719c73ff4cf5becebec
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-02 17:33:43 +0530

    changes

--- var/www/html/residentEditPerson.php
+++ var/www/html/residentEditPerson.php
@@ -6,6 +6,8 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
+	$user_id = $_SESSION['hoa_user_id'];
+
 	$fname = $_POST['person_firstname'];
 	$lname = $_POST['person_lastname'];
 	$cell_no = $_POST['person_cell_no'];
@@ -14,8 +16,9 @@
 	$relationship = $_POST['relationship'];
 	$person_id = $_POST['person_id'];
 	$hoa_id = $_POST['hoa_id'];
+	$today = date('Y-m-d');
 
-	$result = pg_query("UPDATE person SET fname='$fname', lname='$lname', email='$email', cell_no=$cell_no, role_type_id=$role_type, relationship_id=$relationship WHERE id=$person_id");
+	$result = pg_query("UPDATE person SET fname='$fname', lname='$lname', email='$email', cell_no=$cell_no, role_type_id=$role_type, relationship_id=$relationship, updated_by=$user_id, updated_on='$today' WHERE id=$person_id");
 
 	if($result)
 		echo "<br><br><br><br><center><h3>Person details updated successfully.</h3></center>";
