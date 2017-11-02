36b8dd05742d47714b51bff7f9d1f61fe1019b1c
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-02 17:06:52 +0530

    testing

--- var/www/html/boardUserDashboard2.php
+++ var/www/html/boardUserDashboard2.php
@@ -506,12 +506,12 @@
                                         
                                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                       <label>Resident Since</label>
-                                      <input type='date' class="form-control" name='edit_valid_from' id='edit_valid_from' value="<?php echo date('d/m/Y', strtotime($valid_from)); ?>" required>
+                                      <input type='date' class="" name='edit_valid_from' id='edit_valid_from' value="<?php echo date('d/m/Y', strtotime($valid_from)); ?>" required>
                                     </div>
                                         
                                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                       <label>Resident Until</label>
-                                      <input type='date' class="form-control" name='edit_valid_until' id='edit_valid_until' value="<?php echo $valid_until; ?>" >
+                                      <input type='date' class="" name='edit_valid_until' id='edit_valid_until' value="<?php echo $valid_until; ?>" >
 
                                       <input type='hidden' name='hoa_id' id='hoa_id' value="<?php echo $hoa_id; ?>">
                                     </div>
