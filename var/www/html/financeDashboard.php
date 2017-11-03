e01645599b8234d581ae2819fefdf60fe7976b08
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-03 20:22:17 +0530

    finance dashboard

--- var/www/html/financeDashboard.php
+++ var/www/html/financeDashboard.php
@@ -423,6 +423,96 @@
 
           </div>
 
+          <br>
+
+          <div class="row container-fluid">
+
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+
+              <div class="row container-fluid text-left">
+
+                <div class="row container-fluid">
+
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+
+                    <a href='https://hoaboardtime.com/trailBalanceReport.php'>
+
+                      $
+
+                    </a>
+
+                  </div>
+
+                </div>
+
+                <div class="row container-fluid text-center">
+
+                  <h5><strong>Trail Balance Report</strong></h5>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+
+              <div class="row container-fluid text-left">
+
+                <div class="row container-fluid">
+
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+
+                    <a href='https://hoaboardtime.com/chartOfAccounts.php'>
+
+                      $
+
+                    </a>
+
+                  </div>
+
+                </div>
+
+                <div class="row container-fluid text-center">
+
+                  <h5><strong>Chart Of Accounts</strong></h5>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+
+              <div class="row container-fluid text-left">
+
+                <div class="row container-fluid">
+
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+
+                    <a href='https://hoaboardtime.com/vendorExpenditure.php'>
+
+                      $
+
+                    </a>
+
+                  </div>
+
+                </div>
+
+                <div class="row container-fluid text-center">
+
+                  <h5><strong>Expenditure By Vendor</strong></h5>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
         </section>
 
         <br>
