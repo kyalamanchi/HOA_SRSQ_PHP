fdebb521ff985f026bdc035ec9850fefe7538fe4
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-06 18:38:47 +0530

    changes

--- var/www/html/srp/slider/firstPage.php
+++ var/www/html/srp/slider/firstPage.php
@@ -107,9 +107,9 @@
 											$community_code[$i] = $row['community_code'];
 
 											if($i == 0)
-												echo "<li class='nav-item active'>";
+												echo "<li class='nav-link active'>";
 											else
-												echo "<li class='nav-item'>";
+												echo "<li class='nav-link'>";
 
 											echo "'<a class='nav-link' href='#tab-$i' data-toggle='tab'>$community_code[$i]</a></li>";
 
