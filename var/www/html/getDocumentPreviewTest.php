da3f3e23449afd3fbb244a03d9a74d9c9badb2d9
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-06 11:59:33 +0530

    changes

--- var/www/html/getDocumentPreviewTest.php
+++ var/www/html/getDocumentPreviewTest.php
@@ -6,7 +6,7 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	if(!$_SESSION['hoa_username'])
+	if(!$_SESSION["hoa_username"])
 		header("Location: logout.php");
 
 	$mode = $_SESSION['hoa_mode'];
