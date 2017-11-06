bbe1b1ca88aeeb6ea83323c99ffb35a1360a9da9
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-06 18:27:10 +0530

    slider testing

---
+++ var/www/html/srp/slider/firstPage.php
@@ -0,0 +1,210 @@
+<?php
+	
+	session_start();
+
+	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
+?>
+
+<!DOCTYPE html>
+
+<html lang='en'>
+
+	<head>
+
+		<meta charset='UTF-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<meta name='description' content='Stoneridge Place At Pleasanton HOA'>
+		<meta name='author' content='Geeth'>
+
+		<title>First Page</title>
+
+		<!-- Web Fonts-->
+		<link href='https://fonts.googleapis.com/css?family=Poppins:500,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Hind:400,600,700' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Lora:400i' rel='stylesheet'>
+		<!-- Bootstrap core CSS-->
+		<link href='assets/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+		<!-- Icon Fonts-->
+		<link href='assets/css/font-awesome.min.css' rel='stylesheet'>
+		<link href='assets/css/linea-arrows.css' rel='stylesheet'>
+		<link href='assets/css/linea-icons.css' rel='stylesheet'>
+		<!-- Plugins-->
+		<link href='assets/css/magnific-popup.css' rel='stylesheet'>
+		<link href='assets/css/vertical.min.css' rel='stylesheet'>
+		<link href='assets/css/pace-theme-minimal.css' rel='stylesheet'>
+		<link href='assets/css/animate.css' rel='stylesheet'>
+		<!-- Template core CSS-->
+		<link href='assets/css/template.min.css' rel='stylesheet'>
+		<!-- Datatable -->
+		<link rel='stylesheet' href='https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css'>
+
+	</head>
+
+	<body>
+
+		<div class='layout'>
+
+			<!-- Header-->
+			<header class='header header-right undefined'>
+	
+				<div class='container-fluid'>
+								
+					<!-- Logos-->
+					<div class='inner-header'>
+
+						<a class='inner-brand' href=''><h3 style='color: green;'>HOA</h3></a>
+
+					</div>
+					
+					<!-- Mobile menu-->
+					<div class='nav-toggle'><a href='#' data-toggle='collapse' data-target='.inner-navigation'><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></a></div>
+				
+				</div>
+
+			</header>
+
+			<div class='wrapper'>
+
+				<!-- Page Header -->
+				<section class='module-page-title'>
+					
+					<div class='container'>
+							
+						<div class='row-page-title'>
+							
+							<div class='page-title-captions'>
+								
+								<h1 class='h5'>Select Community &amp; User</h1>
+							
+							</div>
+						
+						</div>
+						
+					</div>
+				
+				</section>
+
+				<!-- Content -->
+				<section class='module'>
+						
+					<div class='container'>
+							
+						<div class='table-responsive col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+						
+							<ul class='nav nav-tabs'>
+
+									<?php
+
+										$result = pg_query("SELECT * FROM community_info ORDER BY community_id");
+										$i = 0;
+										$community_id = array();
+										$community_name = array();
+										$community_code = array();
+
+										while($row = pg_fetch_assoc($result))
+										{
+
+											$community_id[$i] = $row['community_id'];
+											$community_name[$i] = $row['legal_name'];
+											$community_code[$i] = $row['community_code'];
+
+											echo "<li class='nav-item";
+
+											if($i == 0)
+											{
+
+												$i++;
+
+												echo " active";
+
+											}
+
+											echo "'><a class='nav-link' href='#tab-$i' data-toggle='tab'>$community_code</a></li>";
+
+										}
+
+										$total_communities = $i;
+
+									?>
+
+								</ul>
+
+								<div class='tab-content'>
+
+									<?php
+
+										for($i = 0; $i < $total_communities; $i++) 
+										{
+
+											echo "
+
+											<div class='tab-pane";
+
+											if($i == 0)
+												echo " in active";
+
+											echo "' id='tab-1'>
+										
+												<div class='special-heading m-b-40'>
+											
+													<h4>$community_name[$i]</h4>
+										
+												</div>
+										
+												<div class='container'>
+
+
+												</div>
+
+											</div>
+
+											";
+
+										}
+
+									?>
+
+								</div>
+
+						</div>
+
+					</div>
+
+				</section>
+
+				<!-- Footer-->
+				<?php include 'footer.php'; ?>
+
+				<a class='scroll-top' href='#top'><i class='fa fa-angle-up'></i></a>
+
+			</div>
+
+		</div>
+
+		<!-- Scripts-->
+		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/tether/1.1.1/js/tether.min.js'></script>
+		<script src='assets/bootstrap/js/bootstrap.min.js'></script>
+		<script src='http://maps.googleapis.com/maps/api/js?key=AIzaSyA0rANX07hh6ASNKdBr4mZH0KZSqbHYc3Q'></script>
+		<script src='assets/js/plugins.min.js'></script>
+		<script src='assets/js/custom.min.js'></script>
+		<!-- Datatable -->
+		<script src='//code.jquery.com/jquery-1.12.4.js'></script>
+		<script src='https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js'></script>
+
+		<script>
+      	
+	      	$(function () {
+	        	
+	        	$('#example1').DataTable({ 'pageLength': 50 });
+
+	      	});
+
+    	</script>
+
+		<!-- Color Switcher (Remove these lines)-->
+		<!--script src='assets/js/style-switcher.min.js'></script-->
+	</body>
+
+</html>
