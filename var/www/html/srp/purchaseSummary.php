e84c5067b4a9c288c20144076b6c7d3874d5ff81
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-06 16:25:53 +0530

    chanegs

--- var/www/html/srp/purchaseSummary.php
+++ var/www/html/srp/purchaseSummary.php
@@ -97,7 +97,7 @@
 							
 						<div class='table-responsive col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
 						
-							<table class="table table-responsive table-hover" id="example" style="color: black;">
+							<table class="table table-responsive table-hover" id="example1" style="color: black;">
   
   								<thead>
         
