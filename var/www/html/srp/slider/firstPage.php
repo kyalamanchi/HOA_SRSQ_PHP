d32caa2214a60204ae1456ab1d747b42238216bb
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-06 18:59:16 +0530

    chanegs

--- var/www/html/srp/slider/firstPage.php
+++ var/www/html/srp/slider/firstPage.php
@@ -176,7 +176,7 @@
 
 														<br><br>
 
-														<button class='btn btn-info btn-sm' type='submit'>Submit</button>
+														<center><button class='btn btn-info btn-sm' type='submit'>Submit</button></center>
 
 													</form>
 
