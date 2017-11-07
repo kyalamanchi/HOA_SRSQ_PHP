01d1b0e21408bc528782ead7947cc5f494306ab2
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-08 04:34:10 +0530

    changes

--- var/www/html/srp/slider/userPage.php
+++ var/www/html/srp/slider/userPage.php
@@ -221,7 +221,7 @@
 
 									<hr class='small'>
 
-									<button class='btn btn-info btn-xs' data-toggle='modal' data-target='#modal_edit_user_details'><i class='fa fa-edit'></i> Edit</button> <button id='user_details_continue' name='user_details_continue' class='btn btn-success btn-xs'>Continue <i class='fa fa-arrow-right'></i></button>
+									<button id='user_details_continue' name='user_details_continue' class='btn btn-success btn-xs'>Continue <i class='fa fa-arrow-right'></i></button>
 
 								</div>
 
