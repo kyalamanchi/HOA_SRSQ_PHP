5cabe6473b3d981eeffa3973c438be3585baf432
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-07 15:14:36 +0530

    changes

--- var/www/html/srp/slider/assets/js/main.js
+++ var/www/html/srp/slider/assets/js/main.js
@@ -1,4 +1,16 @@
 $('form.ajax').on('submit', function(){
-	console.log("Triggered");
+	
+	var obj = $(this),
+	url = obj.attr('action'),
+	method = obj.attr('method'),
+
+	obj.find('[name]').each(function(index, value){
+
+		var input = $(this),
+		index = input.attr('name'),
+		value = input.val();
+
+	});
+
 	return false;
 });
