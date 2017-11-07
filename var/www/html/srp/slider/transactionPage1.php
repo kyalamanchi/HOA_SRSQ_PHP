071d4a973055eb6b37642ae7e2eef31230e118c2
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-07 18:41:19 +0530

    changes

--- var/www/html/srp/slider/transactionPage1.php
+++ var/www/html/srp/slider/transactionPage1.php
@@ -5,8 +5,11 @@
 	$hoa_id = $_SESSION['hoa_alchemy_hoa_id'];
 
 	$row = pg_fetch_assoc(pg_query("SELECT * FROM hoaid WHERE hoa_id=$hoa_id"));
+	$username = $row['firstname'];
+	$username .= " ";
+	$username .= $row['lastname'];
 
-	$_SESSION['hoa_alchemy_username'] = $row['firstname']." ".$row['lastname'];
+	$_SESSION['hoa_alchemy_username'] = $username;
 
 	echo $_SESSION['hoa_alchemy_hoa_id']." ".$_SESSION['hoa_alchemy_username'];
 
