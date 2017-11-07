763de01dd254c6d6a7b7d7f8a1bfbf0bb9ad5cdb
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-08 02:56:01 +0530

    changes

--- var/www/html/srp/slider/assets/js/userPage.js
+++ var/www/html/srp/slider/assets/js/userPage.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
 $('#edit_living_status').change(function(){
 
-	var living_status = %('#edit_living_status').val();
+	var living_status = $('#edit_living_status').val();
 
 	if(living_status == 'f')
 		$('#mailing_address_div').show();
