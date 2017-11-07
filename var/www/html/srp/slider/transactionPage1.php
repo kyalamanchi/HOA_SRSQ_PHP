8bdad336599139a3c73f2bb124f9d4003076ff3b
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-07 18:42:40 +0530

    connect pg db

--- var/www/html/srp/slider/transactionPage1.php
+++ var/www/html/srp/slider/transactionPage1.php
@@ -2,6 +2,8 @@
 
 	session_start();
 
+	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
 	$hoa_id = $_SESSION['hoa_alchemy_hoa_id'];
 
 	$row = pg_fetch_assoc(pg_query("SELECT * FROM hoaid WHERE hoa_id=$hoa_id"));
