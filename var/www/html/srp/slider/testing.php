7bb965366a3295f97eb3eca8dd6ef545582837c3
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-07 13:12:46 +0530

    testing.php

--- var/www/html/srp/slider/testing.php
+++ var/www/html/srp/slider/testing.php
@@ -1,7 +1,7 @@
 <?php
 
-	if(isset($_POST['cell_number']))
-		echo $_POST['cell_number'];
+	if(isset($_POST['confirm_cell_no']))
+		echo $_POST['confirm_cell_no'];
 	else
 		echo "Enter a number";
 
