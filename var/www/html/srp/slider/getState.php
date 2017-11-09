042b8fd060e35a63dcb69d88e71cc6f4bbe5f1a1
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-09 17:02:03 +0530

    changes

--- var/www/html/srp/slider/getState.php
+++ var/www/html/srp/slider/getState.php
@@ -2,14 +2,34 @@
 
 	session_start();
 
+	if(!$_SESSION['hoa_alchemy_hoa_id'])
+		header("Location: logout.php");
+
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
 	$country = $_POST['country_id'];
 
-	//$result = pg_query("SELECT * FROM state WHERE country_id=$country");
+	if($country == "")
+		echo "empty";
+	else
+	{
 
-		$states = "<option value='' selected disabled>Geeth $country</option>";
+		$result = pg_query("SELECT * FROM state WHERE country_id=$country");
 
-	echo $states;
+		$states = "";
+
+		while($row = pg_fetch_assoc($result))
+		{
+
+			$state_id = $row['state_id'];
+			$state_name = $row['state_name'];
+
+			$states .= "<option value='$state_id'>$state_name</option>";
+
+		}
+
+		echo $states;
+
+	}
 
 ?>
