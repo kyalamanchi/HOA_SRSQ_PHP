a76318680f5c0b21a5fcebc02c559380aa89f83f
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-09 20:08:31 +0530

    testing

--- var/www/html/srp/slider/assets/js/userPage.js
+++ var/www/html/srp/slider/assets/js/userPage.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	$('#email_div').hide();
 	$('#email_continue').hide();
 
-	$('#edit_mailing_country').ready(function(){
+	$('#edit_mailing_country').load(function(){
 
 		var country = $(this).val();
 
