a15076372e62cc8f976c2f324ec58b29a2624f65
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-09 23:30:17 +0530

    changes

--- var/www/html/srp/slider/userPage.php
+++ var/www/html/srp/slider/userPage.php
@@ -837,7 +837,7 @@
 
 											<center>
 
-												<button class='btn btn-warning btn-xs' name='home_edit_back' id='home_edit_back'><i class='fa fa-arrow-left'></i> Back</button> <button class='btn btn-success btn-xs' type='submit'><i class='fa fa-check'></i> Save</button>
+												<button class='btn btn-warning btn-xs' type='button' name='home_edit_back' id='home_edit_back'><i class='fa fa-arrow-left'></i> Back</button> <button class='btn btn-success btn-xs' type='submit'><i class='fa fa-check'></i> Save</button>
 
 											</center>
 
