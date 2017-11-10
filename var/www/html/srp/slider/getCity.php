2fbb47c86e408438a424e4079e50fc0635c8ecae
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-10 15:31:42 +0530

    changes

--- var/www/html/srp/slider/getCity.php
+++ var/www/html/srp/slider/getCity.php
@@ -7,11 +7,11 @@
 
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	$district = $_POST['district'];
+	$district = $_POST['district_id'];
 
 	$result = pg_query("SELECT * FROM city WHERE district_id=$district");
 
-	$cities = "<option disabled selected value=''>Select City</option>$district";
+	$cities = "<option disabled selected value=''>Select City</option>";
 
 	while($row = pg_fetch_assoc($result))
 	{
