faf2deaae06f29eeca4ab056f8bec618e4da21c4
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-11 01:35:25 +0530

    update

--- var/www/html/updateMailChimpCampaignsSRSQ.php
+++ var/www/html/updateMailChimpCampaignsSRSQ.php
@@ -2,8 +2,6 @@
 	
 	date_default_timezone_set('America/Los_Angeles');
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
-	// $ch = curl_init('https://us14.api.mailchimp.com/3.0/campaigns/');
-	// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey eecf4b5c299f0cc2124463fb10a6da2d-us14'));
 	$ch = curl_init('https://us12.api.mailchimp.com/3.0/campaigns/');
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey af5b50b9f714f9c2cb81b91281b84218-us12'));
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
@@ -11,7 +9,17 @@
 	$result = curl_exec($ch);
 	$result = json_decode($result);
 
-	foreach ($result->campaigns as $campaigns) {
+	$totalNumberOfCampaigns = $result->total_items;
+	$initialCount = 0;
+
+	while ( $totalNumberOfCampaigns >= $initialCount ) {
+	$ch = curl_init('https://us12.api.mailchimp.com/3.0/campaigns?offset='.$initialCount);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey af5b50b9f714f9c2cb81b91281b84218-us12'));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$result = curl_exec($ch);
+	$result = json_decode($result);
+		foreach ($result->campaigns as $campaigns) {
 
 
 		if ( isset($campaigns->report_summary) ){
@@ -41,6 +49,11 @@
 			print_r($query);
 		}
 	}
+
+	}
+
+
+
 ?>
 
 		
