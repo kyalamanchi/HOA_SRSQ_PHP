62d6227e127a088470290fa8b71b1a253bba2566
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-10 06:24:10 +0530

    update

--- var/www/html/updateMailChimpMembersSRSQ.php
+++ var/www/html/updateMailChimpMembersSRSQ.php
@@ -13,7 +13,7 @@
 	$result = curl_exec($ch);
 	$result = json_decode($result);
 	foreach ($result->members as $member) {
-	$query  = "INSERT INTO community_campaigns_lists_members (list_id,member_id,email_address,status,avg_open_rate,avg_click_rate,member_rating,last_changed,updated_on,updated_by) VALUES('".$row['list_id']."','".$member->id."','".$member->email_address."','".$member->status."',".round($member->stats->avg_open_rate,2).",".round($member->stats->avg_click_rate).",".$member->member_rating.",'".$member->last_changed."','".date('Y-m-d H:i:s')."',401) ON CONFLICT(member_id) DO UPDATE SET updated_on='".date('Y-m-d H:i:s')."',updated_by=401";
+	$query  = "INSERT INTO community_campaigns_lists_members (list_id,member_id,email_address,status,avg_open_rate,avg_click_rate,member_rating,last_changed,updated_on,updated_by) VALUES('".$row['list_id']."','".$member->id."','".$member->email_address."','".$member->status."',".round($member->stats->avg_open_rate*100,2).",".round($member->stats->avg_click_rate*100,2).",".$member->member_rating.",'".$member->last_changed."','".date('Y-m-d H:i:s')."',401) ON CONFLICT(member_id) DO UPDATE SET updated_on='".date('Y-m-d H:i:s')."',updated_by=401";
 	if ( !pg_query($query) ){
 		print_r($query);
 		print_r(nl2br("\n\n"));
