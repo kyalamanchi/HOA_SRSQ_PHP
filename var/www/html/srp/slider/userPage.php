dc469cb41e751ef216ca6002119381c95d8e9e4e
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-10 16:42:23 +0530

    changes

--- var/www/html/srp/slider/userPage.php
+++ var/www/html/srp/slider/userPage.php
@@ -1055,7 +1055,7 @@
 
 								</div>
 
-								<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+								<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-right'>
 
 									<hr class='small'>
 
