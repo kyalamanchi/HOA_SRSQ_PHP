1046b1c27be84002d7b01faa31af5b7db83bee42
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-14 17:12:13 +0530

    testing

--- var/www/html/srp/slider/assets/js/userPage.js
+++ var/www/html/srp/slider/assets/js/userPage.js
@@ -500,7 +500,7 @@ $('#edit_email_back').click(function(){
 
 $('#edit_person_close_modal').on('click', function(){
 
-	var person_id = ('edit_person_close_modal').val();
+	var person_id = ('#edit_person_close_modal').val();
 
 	alert(person_id);
 
