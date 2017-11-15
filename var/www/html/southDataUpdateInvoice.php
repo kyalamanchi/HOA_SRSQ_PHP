89d9080f1da09aa1dc6f276bf9f2aa6cf65dd9a6
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-16 04:51:02 +0530

    update

---
+++ var/www/html/southDataUpdateInvoice.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set('America/Los_Angeles');
+$connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
+$query = "SELECT * FROM COMMUNITY_STATEMENTS_MAILED WHERE ORDER_ID IS NOT NULL AND INVOICE_TOTAL IS NULL";
+$queryResult = pg_query($query);
+while ($row = pg_fetch_assoc($queryResult)) {
+	$req = curl_init();
+curl_setopt($req, CURLOPT_URL,"http://southdata.us-west-2.elasticbeanstalk.com/GetInvoiceDetails.aspx?id=".$row['order_id']);
+curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+$message = curl_exec($req);
+
+$message = explode('@', $message);
+if ( $message[0] != "Failed." ){
+$updateQuery = "UPDATE COMMUNITY_STATEMENTS_MAILED SET INVOICE_ID=".$message[1].",invoice_total=".$message[5].",invoice_date='".date('Y-m-d',strtotime($message[2]))."',invoice_unit_price=".$message[4].",quantity=".$message[3].",updated_on='".date('Y-m-d H:i:s')."',updated_by=401 WHERE statement_id=".$row['statement_id'];
+pg_query($updateQuery);
+}
+else {
+	echo $row['statement_id'];
+	print_r(nl2br("\n\n"));
+}
+
+}
+?>
---
+++ var/www/html/southDataUpdateOrderStatus.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+date_default_timezone_set('America/Los_Angeles');
+$connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
+$query = "SELECT * FROM COMMUNITY_STATEMENTS_MAILED WHERE ORDER_ID IS NOT NULL AND (order_status != 'Billed' OR order_status IS NULL)";
+$queryResult = pg_query($query);
+while ($row = pg_fetch_assoc($queryResult)) {
+	$req = curl_init();
+curl_setopt($req, CURLOPT_URL,"http://southdata.us-west-2.elasticbeanstalk.com/GetOrderStatus.aspx?id=".$row['order_id']);
+curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+$message = curl_exec($req);
+
+$message = explode('@', $message);
+if ( ($message[0] != "Failed." )&&($message[0] != "") ){
+$updateQuery = "UPDATE COMMUNITY_STATEMENTS_MAILED SET order_status='".$message[0]."', updated_on='".date('Y-m-d H:i:s')."',updated_by=401 WHERE statement_id=".$row['statement_id'];
+pg_query($updateQuery);
+}
+else {
+	echo $row['statement_id'];
+	print_r(nl2br("\n\n"));
+}
+
+}
+?>
