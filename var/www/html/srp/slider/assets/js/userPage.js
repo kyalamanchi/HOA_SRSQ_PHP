1dd0f5738d34a009d740776ca83de906d2d65c36
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-16 12:03:28 +0530

    CHANGES

--- var/www/html/srp/slider/assets/js/userPage.js
+++ var/www/html/srp/slider/assets/js/userPage.js
@@ -630,6 +630,40 @@ $('form.ajax5').on('submit', function(){
 
 	$.ajax({
 
+		url: 'resetRole.php',
+		method: 'POST',
+		data: {roleId: data['add_person_role']},
+		success: function(response1){
+
+			alert(response1);
+
+			alert("Ro "+response1);
+
+			ro = response1;
+
+		}
+
+	});
+
+	$.ajax({
+
+		url: 'resetRelationship.php',
+		method: 'POST',
+		data: {relationshipId: data['add_person_relationship']},
+		success: function(response1){
+
+			alert(response1);
+
+			alert("Re "+response1);
+
+			re = response1;
+
+		}
+
+	});
+
+	$.ajax({
+
 		url: url,
 		type: method,
 		data: data,
@@ -640,43 +674,9 @@ $('form.ajax5').on('submit', function(){
 			else
 			{
 
-				$.ajax({
-
-					url: 'resetRole.php',
-					method: 'POST',
-					data: {roleId: data['add_person_role']},
-					success: function(response1){
-
-						alert(response1);
-
-						alert("Ro "+response1);
-
-						ro = response1;
-
-					}
-
-				});
-
-				$.ajax({
-
-					url: 'resetRelationship.php',
-					method: 'POST',
-					data: {relationshipId: data['add_person_relationship']},
-					success: function(response1){
-
-						alert(response1);
-
-						alert("Re "+response1);
-
-						re = response1;
-
-					}
-
-				});
+				alert("Person added.");
 
 				$('#person_table').append('<tr><td name="person_'+response+'_firstname" id="person_'+response+'_firstname">'+data['add_person_firstname']+'</td><td name="person_'+response+'_lastname" id="person_'+response+'_lastname">'+data['add_person_lastname']+'</td><td name="person_'+response+'_email" id="person_'+response+'_email">'+data['add_person_email']+'</td><td name="person_'+response+'_cell_no" id="person_'+response+'_cell_no">'+data['add_person_cell_no']+'</td><td name="person_'+response+'_role" id="person_'+response+'_role">'+ro+'</td><td name="person_'+response+'_relationship" id="person_'+response+'_relationship">'+re+'</td><td></td><td></td></tr>');
-
-				alert("Person added.");
 
 			}
 
