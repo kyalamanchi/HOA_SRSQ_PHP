921b15a83a45a58321e4fc17ce2498a5df80c5c5
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-17 07:34:42 +0530

    testing primary email

--- var/www/html/AnnualReport/userPage3.php
+++ var/www/html/AnnualReport/userPage3.php
@@ -783,7 +783,12 @@
 
                                                     <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
 
-                                                        <input type='radio' name='primary_email' value='".$pid."'> <label style='color: black;'><strong>".$pemail."</strong></label>
+                                                        <input type='radio' name='primary_email' value='".$pid."'";
+
+                                                        if($pemail == $primary_email)
+                                                            echo " checked";
+
+                                                        echo "> <label style='color: black;'><strong>".$pemail."</strong></label>
 
                                                     </div>
 
