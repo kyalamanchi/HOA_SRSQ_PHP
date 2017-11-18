60609aa121551b6a7456198e15d6b00133566a41
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 04:05:58 +0530

    update

--- var/www/html/mailFile.php
+++ var/www/html/mailFile.php
@@ -73,10 +73,10 @@
 <script type="text/javascript">
   
   function getAddress(){
-      alert($("memberID").find("option:selected").text());
-      $("memberAddress").find('option').remove();
-      $("memberAddress").selectpicker('refresh');
-      var selectedMember = $("memberID").find("option:selected").text();
+      alert($("#memberID").find("option:selected").text());
+      $("#memberAddress").find('option').remove();
+      $("#memberAddress").selectpicker('refresh');
+      var selectedMember = $('#memberID').find("option:selected").text();
       if ( selectedMember ){
 
       }
