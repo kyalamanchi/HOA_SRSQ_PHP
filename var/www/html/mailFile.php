ce8d582da3955d1322bc3c83260ac6744b36e768
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-18 11:30:14 +0530

    test

--- var/www/html/mailFile.php
+++ var/www/html/mailFile.php
@@ -80,7 +80,7 @@
           
           <div class="row">
 
-            <section class="col-lg-12 col-xl-12 col-md-12 col-xs-12 col-sm-12">
+            <section class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-6">
 
               <div class="box">
 
