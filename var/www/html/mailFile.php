0780235f40491babd42237c24c681eaef3d94bfa
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-18 10:41:41 +0530

    test

--- var/www/html/mailFile.php
+++ var/www/html/mailFile.php
@@ -43,12 +43,11 @@
     <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
     <link rel="stylesheet" href="dist/css/alt/AdminLTE-select2.min.css">
     <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-    <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/select2.min.css" />
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.4/js/select2.min.js"></script>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/select2.min.js"></script>
+
   </head>
 
   <body class="hold-transition skin-blue sidebar-mini">
