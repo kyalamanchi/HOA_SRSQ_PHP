b06871073e7fd155d67b1d7b20cb623ef1e77702
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 04:52:47 +0530

    test

--- var/www/html/mailFile.php
+++ var/www/html/mailFile.php
@@ -55,8 +55,6 @@
     <link rel="stylesheet" href="dist/css/alt/AdminLTE-select2.min.css">
     <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
-    <!-- <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
-    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 
     <style type="text/css">
       body{
