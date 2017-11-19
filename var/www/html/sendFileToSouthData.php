404e51abd8e9a1650be72e424d423e153d2f498b
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 22:42:57 +0530

    update

--- var/www/html/sendFileToSouthData.php
+++ var/www/html/sendFileToSouthData.php
@@ -2,6 +2,25 @@
 $data = file_get_contents('php://input');
 $parseJSON = json_decode($data);
 
-echo $parseJSON[0]->file_data;
+$handler = fopen($parseJSON[0]->file_name, "w");
+fwrite($handler, $parseJSON[0]->file_data);
+fclose($handler);
+
+
+$url = 'https://content.dropboxapi.com/2/files/upload';
+    $pdfFileContent = $parseJSON[0]->file_data;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer xCCkLEFieJAAAAAAAAABUHpqfAcHsr24243JwXKp_A6jK_cKpN-9IFdm8QxGBjx9','Content-Type:application/octet-stream','Dropbox-API-Arg: {"path": "/'.$pdfFileName.'","mode": "overwrite","autorename": false,"mute": false}'));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $pdfFileContent); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($ch);
+
+
+
+unlink($parseJSON[0]->file_name);
+
+
+
 
 ?>
