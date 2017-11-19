d404dd914be73cc34cf3306016a57b2f13b9a2a4
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 22:47:41 +0530

    fix

--- var/www/html/sendFileToSouthData.php
+++ var/www/html/sendFileToSouthData.php
@@ -11,7 +11,7 @@ $url = 'https://content.dropboxapi.com/2/files/upload';
     $pdfFileContent = $parseJSON[0]->file_data;
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer xCCkLEFieJAAAAAAAAABUHpqfAcHsr24243JwXKp_A6jK_cKpN-9IFdm8QxGBjx9','Content-Type:application/octet-stream','Dropbox-API-Arg: {"path": "/'.$pdfFileName.'","mode": "overwrite","autorename": false,"mute": false}'));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer xCCkLEFieJAAAAAAAAABUHpqfAcHsr24243JwXKp_A6jK_cKpN-9IFdm8QxGBjx9','Content-Type:application/octet-stream','Dropbox-API-Arg: {"path": "/'.$parseJSON[0]->file_name.'","mode": "overwrite","autorename": false,"mute": false}'));
     curl_setopt($ch, CURLOPT_POSTFIELDS, $pdfFileContent); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $response = curl_exec($ch);
