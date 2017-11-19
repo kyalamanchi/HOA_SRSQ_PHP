dc4b74273d82dbbc4ac1cb8f08872ea2d4b67b17
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 20:11:11 +0530

    update

--- var/www/html/mailFile.php
+++ var/www/html/mailFile.php
@@ -59,6 +59,81 @@
   flex: 1;
   background-color: white;
 }
+
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+.switch input {display:none;}
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+input:checked + .slider {
+  background-color: #2196F3;
+}
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+[hidden] {
+  display: none !important;
+}
+.btn.outline {
+  background: none;
+  padding: 12px 22px;
+}
+    .notbold{
+    font-weight:normal
+}​
+body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+}
+input {
+    padding: 1px;
+    align-self: center;
+    min-width: 80px;
+    max-width: 500px;
+}
+input, label {
+    display:block;
+}
+
     </style>
 <script type="text/javascript">
 $(document).ready(function() {
