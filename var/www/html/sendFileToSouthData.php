6b2d6871de55d846a9e0e7eac8f7608f4b6fe5c3
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 22:51:40 +0530

    test

--- var/www/html/sendFileToSouthData.php
+++ var/www/html/sendFileToSouthData.php
@@ -3,12 +3,12 @@ $data = file_get_contents('php://input');
 $parseJSON = json_decode($data);
 
 $handler = fopen($parseJSON[0]->file_name, "w");
-fwrite($handler, $parseJSON[0]->file_data);
+fwrite($handler, base64_decode($parseJSON[0]->file_data);
 fclose($handler);
 
 
 $url = 'https://content.dropboxapi.com/2/files/upload';
-    $pdfFileContent = $parseJSON[0]->file_data;
+    $pdfFileContent = base64_decode($parseJSON[0]->file_data);
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer xCCkLEFieJAAAAAAAAABUHpqfAcHsr24243JwXKp_A6jK_cKpN-9IFdm8QxGBjx9','Content-Type:application/octet-stream','Dropbox-API-Arg: {"path": "/'.$parseJSON[0]->file_name.'","mode": "overwrite","autorename": false,"mute": false}'));
