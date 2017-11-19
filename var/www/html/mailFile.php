28b5c5ce4d3aabb07c6ae1abaf64e156312c98d9
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 06:31:10 +0530

    update

--- var/www/html/mailFile.php
+++ var/www/html/mailFile.php
@@ -64,19 +64,22 @@
   
 
   function getAddress(){
- var pleaseWaitData = '<div class="progress">\
-                      <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
-                      aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
-                      </div>\
-                    </div>';
-      $("#pleaseWaitDialog2").find('.modal-header').html('<h3>Please wait...</h3>');
-      $("#pleaseWaitDialog2").find('.modal-body').html(pleaseWaitData);
-      $("#pleaseWaitDialog2").modal("show");
+
 
       $("#memberAddress").find('option').remove();
       $("#memberAddress").selectpicker('refresh');
       var selectedMember = $('#memberID').find("option:selected").text();
       if ( selectedMember ){
+      
+
+        var pleaseWaitData = '<div class="progress">\
+                      <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
+                      aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
+                      </div>\
+                    </div>';
+        $("#pleaseWaitDialog2").find('.modal-header').html('<h3>Please wait...</h3>');
+        $("#pleaseWaitDialog2").find('.modal-body').html(pleaseWaitData);
+        $("#pleaseWaitDialog2").modal("show");
         var memberId = $('#memberID').val();
         json = [];
 
