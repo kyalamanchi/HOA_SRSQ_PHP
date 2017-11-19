32fa15b8a9c7c97e802e886bdf70cd5d922c1ab7
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 19:19:18 +0530

    update

--- var/www/html/getAddress.php
+++ var/www/html/getAddress.php
@@ -19,14 +19,14 @@ if ( $row['living_status'] == 'f') {
 
 	$returnArray = array();
 
-	array_push($returnArray, $row['home_id'].'#'.$row['address1']);
-	array_push($returnArray, $row2['home_id'].'#'.$row2['address1']);
+	array_push($returnArray, 'home'.'#'.$row['address1']);
+	array_push($returnArray, 'mailing'.'#'.$row2['address1']);
 	echo json_encode($returnArray);
 }
 
 else {
 	$returnArray = array();
-	array_push($returnArray, $row['home_id'].'#'.$row['address1']);
+	array_push($returnArray, 'home'.'#'.$row['address1']);
 	echo json_encode($returnArray);
 }
 
