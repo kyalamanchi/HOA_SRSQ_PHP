d4a4ab5ecc3436112051fdcf4b2b96020e3f8d81
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-19 22:19:50 +0530

    update

--- var/www/html/sendFileToSouthData.php
+++ var/www/html/sendFileToSouthData.php
@@ -3,8 +3,5 @@ $data = file_get_contents('php://input');
 $parseJSON = json_decode($data);
 
 echo $parseJSON[0]->file_data;
-echo $parseJSON[0]->file_name;
-echo $parseJSON[0]->hoa_id;
-echo $parseJSON[0]->address;
 
 ?>
