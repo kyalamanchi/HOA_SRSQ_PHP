b558fe2bc4904dc7a3d21dc6dd417a8f3fc2e1d0
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-21 01:04:52 +0530

    changes

--- var/www/html/AnnualReport/homeid.php
+++ var/www/html/AnnualReport/homeid.php
@@ -109,39 +109,39 @@
 
 			</header>
 
-            <!-- Page Header -->
-            <section class='module-page-title'>
-                                
-                <div class='container'>
-                                        
-                    <div class='row-page-title'>
-                                        
-                        <div class='page-title-captions'>
-                                            
-                            <ol class="breadcrumb">
-                                    
-                                <li class="breadcrumb-item">User Details</li>
-                                <li class="breadcrumb-item"><strong style='color: black;'>Home Details</strong></li>
-                                <li class="breadcrumb-item">Persons</li>
-                                <li class='breadcrumb-item'>Primary Email</li>
-                                <li class='breadcrumb-item'>SMS Notifications</li>
-                                <li class="breadcrumb-item">Agreements</li>
-                                <li class='breadcrumb-item'>Documents</li>
-                                <li class="breadcrumb-item">Payments</li>
-                                <li class="breadcrumb-item">HOA Fact Sheet</li>
-                                <li class="breadcrumb-item">Disclosures</li>
-
-                            </ol>
-                                        
-                        </div>
-                                    
-                    </div>
-                                    
-                </div>
-                            
-            </section>
-
 			<div class='wrapper'>
+
+				<!-- Page Header -->
+	            <section class='module-page-title'>
+	                                
+	                <div class='container'>
+	                                        
+	                    <div class='row-page-title'>
+	                                        
+	                        <div class='page-title-captions'>
+	                                            
+	                            <ol class="breadcrumb">
+	                                    
+	                                <li class="breadcrumb-item">User Details</li>
+	                                <li class="breadcrumb-item"><strong style='color: black;'>Home Details</strong></li>
+	                                <li class="breadcrumb-item">Persons</li>
+	                                <li class='breadcrumb-item'>Primary Email</li>
+	                                <li class='breadcrumb-item'>SMS Notifications</li>
+	                                <li class="breadcrumb-item">Agreements</li>
+	                                <li class='breadcrumb-item'>Documents</li>
+	                                <li class="breadcrumb-item">Payments</li>
+	                                <li class="breadcrumb-item">HOA Fact Sheet</li>
+	                                <li class="breadcrumb-item">Disclosures</li>
+
+	                            </ol>
+	                                        
+	                        </div>
+	                                    
+	                    </div>
+	                                    
+	                </div>
+	                            
+	            </section>
 
 				<!-- Content -->
 				<section class='module'>
