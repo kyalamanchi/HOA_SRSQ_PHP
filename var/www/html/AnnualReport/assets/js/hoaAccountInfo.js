f1db62922649b1029e56744f17561a59ca4bf548
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-21 01:58:13 +0530

    changes

--- var/www/html/AnnualReport/assets/js/hoaAccountInfo.js
+++ var/www/html/AnnualReport/assets/js/hoaAccountInfo.js
@@ -2,21 +2,21 @@ $('#set_password').keypress(function(){
 
 	data = $('#set_password').val();
 
-	alert(data);
+	//alert(data);
 
-	//$.ajax({
+	$.ajax({
 					
-	//	url: 'getBcrypt.php',
-	//	method: 'post',
-	//	data: {set_password:data},
-	//	success: function(response){
+		url: 'getBcrypt.php',
+		method: 'post',
+		data: {set_password:data},
+		success: function(response){
 
-	//		alert(response);
+			alert(response);
 						
-	//		$('#bcrypt_div').text("This is your encrypted password : "+response);
+			$('#bcrypt_div').text("This is your encrypted password : "+response);
 				
-	//	}
+		}
 
-	//});
+	});
 
 });
