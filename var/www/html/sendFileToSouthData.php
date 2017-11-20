b3c8221ee8b23518f304ca4937a150653750b241
Dhivysh <Dhivysh@users.noreply.github.com>
2017-11-20 14:47:22 +0530

    update

--- var/www/html/sendFileToSouthData.php
+++ var/www/html/sendFileToSouthData.php
@@ -7,7 +7,7 @@ fwrite($handler, base64_decode($parseJSON[0]->file_data));
 fclose($handler);
 
 
-$url = 'https://content.dropboxapi.com/2/files/upload';
+	$url = 'https://content.dropboxapi.com/2/files/upload';
     $pdfFileContent = file_get_contents($parseJSON[0]->file_name);
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
@@ -15,12 +15,18 @@ $url = 'https://content.dropboxapi.com/2/files/upload';
     curl_setopt($ch, CURLOPT_POSTFIELDS, $pdfFileContent); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $response = curl_exec($ch);
+    $response = json_decode($response);
+    if ( isset($response->error_summary) ){
+    	echo "An error occured.";
+    }
+    else {
+    	echo "File uploaded successfully";
+    }
 
-echo json_decode($response);
 
+
+
+
+//Deleting created file
 unlink($parseJSON[0]->file_name);
-
-
-
-
 ?>
