cc056df618c31e5797e4538679670e6f1e351f60
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-20 18:08:09 +0530

    changes

--- var/www/html/AnnualReport/assets/js/primaryEmail.js
+++ var/www/html/AnnualReport/assets/js/primaryEmail.js
@@ -4,37 +4,6 @@ $('#email_back').click(function(){
 
 });
 
-$('#email_radio_yes').change(function() {
-
-	if (document.getElementById('email_radio_yes').checked) {
-
-		$('#email_continue').show();
-
-	}
-
-});
-
-$('#email_radio_no').change(function() {
-
-	if (document.getElementById('email_radio_no').checked) {
-
-		$('#email_div').hide();
-		$('#edit_email_div').show();
-
-	}
-
-});
-
-$('#edit_email_back').click(function(){
-
-	$('#email_div').show();
-	$('#edit_email_div').hide();
-	$('#email_continue').show();
-	$('#email_radio_no').prop('checked', false);
-	$('#email_radio_yes').prop('checked', true);
-
-});
-
 $('#email_continue').click(function(){
 
 	window.location = "notifications.php";
