9d21da1eba8a395cd2f2c51d4171b1ea663e8729
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-20 19:20:47 +0530

    changes in agreements

--- var/www/html/AnnualReport/assets/js/agreements.js
+++ var/www/html/AnnualReport/assets/js/agreements.js
@@ -1,96 +1,11 @@
-$(document).ready(function(){
-
-	$('#documents_div').hide();
-	$('#payments_div').hide();
-	$('#hoa_fact_sheet_div').hide();
-	$('#disclosures_div').hide();
-	$('#documents_header').hide();
-	$('#payments_header').hide();
-	$('#hoa_fact_sheet_header').hide();
-	$('#disclosures_header').hide();
-
-});
-
 $('#agreements_continue').click(function(){
 
-	$('#agreements_div').hide();
-	$('#documents_div').show();
-	$('#documents_header').show();
-	$('#agreements_header').hide();
+	window.location = 'documents.php'
 
 });
 
 $('#agreements_back').click(function(){
 
 	window.location = ' notifications.php';
-
-});
-
-$('#documents_continue').click(function(){
-
-	$('#documents_div').hide();
-	$('#payments_div').show();
-	$('#payments_header').show();
-	$('#documents_header').hide();
-
-});
-
-$('#documents_back').click(function(){
-
-	$('#agreements_div').show();
-	$('#documents_div').hide();
-	$('#documents_header').hide();
-	$('#agreements_header').show();
-
-});
-
-$('#payments_continue').click(function(){
-
-	$('#payments_div').hide();
-	$('#hoa_fact_sheet_div').show();
-	$('#hoa_fact_sheet_header').show();
-	$('#payments_header').hide();
-
-});
-
-$('#payments_back').click(function(){
-
-	$('#payments_div').hide();
-	$('#documents_div').show();
-	$('#documents_header').show();
-	$('#payments_header').hide();
-
-});
-
-$('#hoa_fact_sheet_continue').click(function(){
-
-	$('#hoa_fact_sheet_div').hide();
-	$('#disclosures_div').show();
-	$('#hoa_fact_sheet_header').hide();
-	$('#disclosures_header').show();
-
-});
-
-$('#hoa_fact_sheet_back').click(function(){
-
-	$('#hoa_fact_sheet_div').hide();
-	$('#payments_div').show();
-	$('#hoa_fact_sheet_header').hide();
-	$('#payments_header').show();
-
-});
-
-$('#disclosures_continue').click(function(){
-
-	window.location = 'hoaAccountInfo.php';
-
-});
-
-$('#disclosures_back').click(function(){
-
-	$('#hoa_fact_sheet_div').show();
-	$('#disclosures_div').hide();
-	$('#hoa_fact_sheet_header').show();
-	$('#disclosures_header').hide();
 
 });
