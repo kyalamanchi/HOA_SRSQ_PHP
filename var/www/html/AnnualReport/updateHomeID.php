2773849d0adf01419a9ce540c18f423f7d4fb22b
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-20 16:59:16 +0530

    edit home id testing

--- var/www/html/AnnualReport/updateHomeID.php
+++ var/www/html/AnnualReport/updateHomeID.php
@@ -8,13 +8,7 @@
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
 	$address1 = $_POST['edit_mailing_address'];
-	$country_id = $_POST['edit_mailing_country'];
-	$state_id = $_POST['edit_mailing_state'];
-	$district_id = $_POST['edit_mailing_district'];
-	$city_id = $_POST['edit_mailing_city'];
-	$zip_id = $_POST['edit_mailing_zip'];
-	$home_id = $_SESSION['hoa_alchemy_home_id'];
-	$user_id = $_SESSION['hoa_alchemy_user_id'];
+	$csz = $_POST['edit_mailing_csz'];
 	$community_id = $_SESSION['hoa_alchemy_community_id'];
 	$today = date('Y-m-d');
 
@@ -23,21 +17,10 @@
 	if($address1 == $row['address1'])
 	{
 
-		$result = pg_query("UPDATE homeid SET living_status='t', address1='$address1', country_id=$country_id, state_id=$state_id, district_id=$district_id, city_id=$city_id, zip_id=$zip_id, updated_on='$today', updated_by=$user_id WHERE home_id=$home_id");
+		$result = pg_query("UPDATE homeid SET living_status='t', address2='$csz', updated_on='$today', updated_by=$user_id WHERE home_id=$home_id");
 
 		if($result)
-		{
-
-			$_SESSION['mailing_address'] = $address1;
-			$_SESSION['mailing_country'] = $country_id;
-			$_SESSION['mailing_state'] = $state_id;
-			$_SESSION['mailing_district'] = $district_id;
-			$_SESSION['mailing_city'] = $city_id;
-			$_SESSION['mailing_zip'] = $zip_id;
-
 			echo $address1;
-
-		}
 		else
 			echo "Some error occured. Plese try again.";
 
@@ -54,30 +37,18 @@
 			if($result)
 			{
 
-				$result = pg_query("UPDATE homeid SET living_status='t', address1='$address1', country_id=$country_id, state_id=$state_id, district_id=$district_id, city_id=$city_id, zip_id=$zip_id, updated_on='$today', updated_by=$user_id WHERE home_id=$home_id");
+				$result = pg_query("UPDATE home_mailing_address SET address1='$address1', address2='$csz', updated_on='$today', updated_by=$user_id WHERE home_id=$home_id");
 
 			}
 			else
 			{
 
-				$result = pg_query("INSERT INTO home_mailing_address (home_id, address1, zip_id, city_id, district_id, state_id, country_id, community_id, updated_by, updated_on, valid_address)VALUES($home_id, '$address1', $zip_id, $city_id, $district_id, $state_id, $country_id, $community_id, $user_id, '$today', 't')");
-
+				$result = pg_query("INSERT INTO home_mailing_address (home_id, address1, address2, community_id, updated_by, updated_on, valid_address)VALUES($home_id, '$address1', $csz, $community_id, $user_id, '$today', 't')");
 
 			}
 
 			if($result)
-			{
-
-				$_SESSION['mailing_address'] = $address1;
-				$_SESSION['mailing_country'] = $country_id;
-				$_SESSION['mailing_state'] = $state_id;
-				$_SESSION['mailing_district'] = $district_id;
-				$_SESSION['mailing_city'] = $city_id;
-				$_SESSION['mailing_zip'] = $zip_id;
-
 				echo $address1;
-
-			}
 			else
 				echo "Some error occured. Please try again.";
 
