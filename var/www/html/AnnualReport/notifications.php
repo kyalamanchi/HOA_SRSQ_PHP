a1172c7c2dfd06155443c989b0abd9d57b7fc35b
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-20 18:53:42 +0530

    changes

--- var/www/html/AnnualReport/notifications.php
+++ var/www/html/AnnualReport/notifications.php
@@ -937,7 +937,7 @@
 		<script src="assets/js/charts.js"></script>
 		<script src="assets/js/custom.min.js"></script>
 
-		<script src='assets/js/userPage4.js'></script>
+		<script src='assets/js/notifications.js'></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
         <!-- Datatable -->
