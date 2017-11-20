909c2bd52d81ad766225b78f97c2b8654cc0cf37
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-20 15:51:59 +0530

    script

--- var/www/html/AnnualReport/script.php
+++ var/www/html/AnnualReport/script.php
@@ -2,7 +2,7 @@
 	
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	$result = pg_query("SELECT * FROM hoaid");
+	$result = pg_query("SELECT * FROM hoaid ORDER BY hoa_id");
 
 	while($row = pg_fetch_assoc($result))
 	{
