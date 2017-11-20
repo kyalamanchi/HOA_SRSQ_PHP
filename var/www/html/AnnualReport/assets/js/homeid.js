0e302fc4a29d77892315ba69cae3a33a4205a792
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-20 17:05:59 +0530

    testing

--- var/www/html/AnnualReport/assets/js/homeid.js
+++ var/www/html/AnnualReport/assets/js/homeid.js
@@ -66,6 +66,8 @@ $('form.ajax2').on('submit', function(){
 		data: data,
 		success: function(response){
 
+			alert(response);
+
 			if(response == "Some error occured. Please try again.")
 				alert(response);
 			else
