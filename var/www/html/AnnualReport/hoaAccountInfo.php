96b6153baeb15427f85b300e72ad5a680c80badb
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-20 16:21:34 +0530

    changes

--- var/www/html/AnnualReport/hoaAccountInfo.php
+++ var/www/html/AnnualReport/hoaAccountInfo.php
@@ -195,16 +195,6 @@
 
                             </div>
 
-                            <br><br>
-
-                            <div class='row'>
-
-                                <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-
-                                </div>
-
-                            </div>
-
 						</div>
 
 					</div>
