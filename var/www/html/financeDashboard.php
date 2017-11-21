323786fb367138bca7efa730529ddc708d28609b
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-21 19:41:09 +0530

    purchase summary

--- var/www/html/financeDashboard.php
+++ var/www/html/financeDashboard.php
@@ -481,6 +481,34 @@
 
                 </div>
 
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12">
+
+                  <div class="row container-fluid text-left">
+
+                    <div class="row container-fluid">
+
+                      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+
+                        <a href='https://hoaboardtime.com/purchaseSummary.php'>
+
+                          <img src='purchase_summary.png'>
+
+                        </a>
+
+                      </div>
+
+                    </div>
+
+                    <div class="row container-fluid text-center">
+
+                      <h5><strong>Purchase Summary</strong></h5>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
               </div>
 
               <br><br>
