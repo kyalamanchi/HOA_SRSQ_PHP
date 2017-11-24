c46bc54ad2a52023aeafd69ae25a7e30f078014b
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-24 15:06:18 +0530

    testing

--- var/www/html/purchaseSummaryDetails.php
+++ var/www/html/purchaseSummaryDetails.php
@@ -83,201 +83,209 @@
           
           <div class="row">
 
-            <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 module'>
-            
-              <?php
+            <div class='box'>
 
-                        setlocale(LC_MONETARY, 'en_US');
-                        date_default_timezone_set('America/Los_Angeles');
-                        
-                        $purchaseID = $_GET['id'];
-                      
-                      $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/query');
-                      
-                      curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'POST');
-                      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json','Content-Type:application/text','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1509571171",oauth_nonce="XXAaTNNoZOx",oauth_version="1.0",oauth_signature="YqQesEYb0Fo%2FmAlv81W3UpT43bs%3D"'));
-                      curl_setopt($ch, CURLOPT_POSTFIELDS, "select * from purchase where id = '".$purchaseID."'");
-                      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            
-                      $result = curl_exec($ch);
-                      $purchaseResult  = json_decode($result);
-                      $purchaseResult  = $purchaseResult->QueryResponse->Purchase;
-                      $final = $purchaseResult[0];
-            
-                    ?>
+              <div class='box-body box-responsive'>
 
-                    <div class='row'>
+                <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 module'>
+                
+                  <?php
 
-                      <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
-
-                        <label><strong><?php echo $purchaseResult[0]->EntityRef->type; ?></strong></label>
-                        
-                        <br>
-
-                      <h4><?php echo $purchaseResult[0]->EntityRef->name; ?></h4>
-
-                      </div>
-
-                      <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
-
-                        <label><strong>Account</strong></label>
-                        
-                        <br>
-
-                      <h4><?php echo $purchaseResult[0]->AccountRef->name; ?></h4>
-
-                      </div>
-
-                      <div class='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-right'>
-
-                        <label><strong>Amount</strong></label>
-                        
-                        <br>
-
-                      <h2><?php echo money_format('%#10n',$purchaseResult[0]->TotalAmt); ?></h2>
-
-                      </div>
-
-                    </div>
-              
-              <br><br>
-
-                    <div class='row'>
-
-                      <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
-
-                        <label><strong>Payment Date</strong></label>
-                        
-                        <br>
-
-                      <h4><?php echo date('d F Y',strtotime($purchaseResult[0]->MetaData->CreateTime)); ?></h4>
-
-                      </div>
-
-                      <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
-
-                        <label><strong>Payment Type</strong></label>
-                        
-                        <br>
-
-                      <h4><?php echo $purchaseResult[0]->PaymentType; ?></h4>
-
-                      </div>
-
-                      <div class='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-right'>
-
-                        <label><strong>Reference Number</strong></label>
-                        
-                        <br>
-
-                      <h4>
-
-                        <?php
-
-                          if( $purchaseResult[0]->DocNumber )
-                                echo $purchaseResult[0]->DocNumber;
-                            else
-                                echo "<center>-</center>";
-
+                            setlocale(LC_MONETARY, 'en_US');
+                            date_default_timezone_set('America/Los_Angeles');
+                            
+                            $purchaseID = $_GET['id'];
+                          
+                          $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/query');
+                          
+                          curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'POST');
+                          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json','Content-Type:application/text','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1509571171",oauth_nonce="XXAaTNNoZOx",oauth_version="1.0",oauth_signature="YqQesEYb0Fo%2FmAlv81W3UpT43bs%3D"'));
+                          curl_setopt($ch, CURLOPT_POSTFIELDS, "select * from purchase where id = '".$purchaseID."'");
+                          curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                
+                          $result = curl_exec($ch);
+                          $purchaseResult  = json_decode($result);
+                          $purchaseResult  = $purchaseResult->QueryResponse->Purchase;
+                          $final = $purchaseResult[0];
+                
                         ?>
 
-                      </h4>
+                        <div class='row'>
 
-                      </div>
+                          <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
 
-                    </div>
-
-                    <br><br>
-
-                    <div class='row'>
-
-                      <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive'>
-
-                        <table id='example1' class='table table-striped' style='color: black;'>
-
-                          <thead>
+                            <label><strong><?php echo $purchaseResult[0]->EntityRef->type; ?></strong></label>
                             
-                            <th>#</th>
-                            <th>Account</th>
-                            <th>Description</th>
-                            <th>Amount</th>
+                            <br>
 
-                          </thead>
+                          <h4><?php echo $purchaseResult[0]->EntityRef->name; ?></h4>
 
-                          <tbody>
+                          </div>
+
+                          <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
+
+                            <label><strong>Account</strong></label>
                             
+                            <br>
+
+                          <h4><?php echo $purchaseResult[0]->AccountRef->name; ?></h4>
+
+                          </div>
+
+                          <div class='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-right'>
+
+                            <label><strong>Amount</strong></label>
+                            
+                            <br>
+
+                          <h2><?php echo money_format('%#10n',$purchaseResult[0]->TotalAmt); ?></h2>
+
+                          </div>
+
+                        </div>
+                  
+                  <br><br>
+
+                        <div class='row'>
+
+                          <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
+
+                            <label><strong>Payment Date</strong></label>
+                            
+                            <br>
+
+                          <h4><?php echo date('d F Y',strtotime($purchaseResult[0]->MetaData->CreateTime)); ?></h4>
+
+                          </div>
+
+                          <div class='col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6'>
+
+                            <label><strong>Payment Type</strong></label>
+                            
+                            <br>
+
+                          <h4><?php echo $purchaseResult[0]->PaymentType; ?></h4>
+
+                          </div>
+
+                          <div class='col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-right'>
+
+                            <label><strong>Reference Number</strong></label>
+                            
+                            <br>
+
+                          <h4>
+
                             <?php
 
-                                $value = 1;
-                                $IDS = array();
-                                
-                                foreach ($purchaseResult[0]->Line as $purchase) 
-                                {
-                                    
-                                    echo '<tr><td>'.$value.'</td><td>'.$purchase->AccountBasedExpenseLineDetail->AccountRef->name.'</td><td>'.$purchase->Description.'</td><td><style="float:right;">'.money_format('%#10n',$purchase->Amount).'</></td></tr>';
+                              if( $purchaseResult[0]->DocNumber )
+                                    echo $purchaseResult[0]->DocNumber;
+                                else
+                                    echo "<center>-</center>";
 
-                                    $value = $value + 1;
-
-                                }
-              
                             ?>
 
-                          </tbody>
-                          
-                        </table>
+                          </h4>
 
-                      </div>
+                          </div>
 
-                    </div>
+                        </div>
 
-                    <br><br>
+                        <br><br>
 
-                    <div class='row'>
+                        <div class='row'>
 
-                      <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                          <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive'>
 
-                        <label><strong>Memo</strong></label>
+                            <table id='example1' class='table table-striped' style='color: black;'>
 
-                        <br>
+                              <thead>
+                                
+                                <th>#</th>
+                                <th>Account</th>
+                                <th>Description</th>
+                                <th>Amount</th>
 
-                        <textarea class="form-control" rows="3" id="comment" style="width: 400px;" readonly="readonly"><?php print_r($final->PrivateNote); ?></textarea>
+                              </thead>
 
-                      </div>
+                              <tbody>
+                                
+                                <?php
 
-                      <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                                    $value = 1;
+                                    $IDS = array();
+                                    
+                                    foreach ($purchaseResult[0]->Line as $purchase) 
+                                    {
+                                        
+                                        echo '<tr><td>'.$value.'</td><td>'.$purchase->AccountBasedExpenseLineDetail->AccountRef->name.'</td><td>'.$purchase->Description.'</td><td><style="float:right;">'.money_format('%#10n',$purchase->Amount).'</></td></tr>';
 
-                        <label><strong>Attachment(s)</strong></label>
+                                        $value = $value + 1;
 
-                        <br>
+                                    }
+                  
+                                ?>
 
-                        <?php
+                              </tbody>
+                              
+                            </table>
 
-                          $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/query');
+                          </div>
 
-                          curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'POST');
-                          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json','Content-Type:application/text','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1509569266",oauth_nonce="8N0tvCVCsWK",oauth_version="1.0",oauth_signature="ZoQHffDGFCgQUgP8R5Owiix6pec%3D"'));
-                          curl_setopt($ch, CURLOPT_POSTFIELDS, "Select * from Attachable where AttachableRef.EntityRef.Type = 'purchase' AND AttachableRef.EntityRef.value = '".$purchaseID."'");
-                          curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                        </div>
 
-                          $result = curl_exec($ch);
-                          $result  = json_decode($result);
-                          $data = $result->QueryResponse;
-            
-                          if ( isset( $data->Attachable ) )
-                          {
+                        <br><br>
+
+                        <div class='row'>
+
+                          <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+
+                            <label><strong>Memo</strong></label>
+
+                            <br>
+
+                            <textarea class="form-control" rows="3" id="comment" style="width: 400px;" readonly="readonly"><?php print_r($final->PrivateNote); ?></textarea>
+
+                          </div>
+
+                          <div class='col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+
+                            <label><strong>Attachment(s)</strong></label>
+
+                            <br>
+
+                            <?php
+
+                              $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/query');
+
+                              curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'POST');
+                              curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json','Content-Type:application/text','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1509569266",oauth_nonce="8N0tvCVCsWK",oauth_version="1.0",oauth_signature="ZoQHffDGFCgQUgP8R5Owiix6pec%3D"'));
+                              curl_setopt($ch, CURLOPT_POSTFIELDS, "Select * from Attachable where AttachableRef.EntityRef.Type = 'purchase' AND AttachableRef.EntityRef.value = '".$purchaseID."'");
+                              curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+                              $result = curl_exec($ch);
+                              $result  = json_decode($result);
+                              $data = $result->QueryResponse;
                 
-                              foreach ($data->Attachable as $attachable)
-                                echo '<a target="_blank" href="'.$attachable->TempDownloadUri.'">'.$attachable->FileName.'</a><br>';
+                              if ( isset( $data->Attachable ) )
+                              {
+                    
+                                  foreach ($data->Attachable as $attachable)
+                                    echo '<a target="_blank" href="'.$attachable->TempDownloadUri.'">'.$attachable->FileName.'</a><br>';
 
-                          }
-                          else
-                            echo "No attachments found";
+                              }
+                              else
+                                echo "No attachments found";
 
-                      ?>
+                          ?>
 
-                      </div>
+                          </div>
 
-                    </div>
+                        </div>
+
+                </div>
+
+              </div>
 
             </div>
 
