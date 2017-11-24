ed42b5a61ce8ae740c780d97f1e1aac0699dc47b
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-24 19:23:47 +0530

    fact sheet

--- var/www/html/AnnualReport/factSheet3.php
+++ var/www/html/AnnualReport/factSheet3.php
@@ -420,7 +420,7 @@
 		<script src="assets/js/charts.js"></script>
 		<script src="assets/js/custom.min.js"></script>
 
-		<script src='assets/js/factSheet.js'></script>
+		<script src='assets/js/factSheet3.js'></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 	</body>
