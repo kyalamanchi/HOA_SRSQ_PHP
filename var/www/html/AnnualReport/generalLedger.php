5f5077b665745dbfd2eb271b75ffc873d61fe978
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-25 18:05:55 +0530

    general ledger

--- var/www/html/AnnualReport/generalLedger.php
+++ var/www/html/AnnualReport/generalLedger.php
@@ -1,4 +1,21 @@
 <?php
+  
+  session_start();
+
+  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
+  if(!$_SESSION['hoa_alchemy_hoa_id'])
+    header('Location: logout.php');
+
+  $email = $_SESSION['hoa_alchemy_email'];
+  $hoa_id = $_SESSION['hoa_alchemy_hoa_id'];
+  $home_id = $_SESSION['hoa_alchemy_home_id'];
+  $user_id = $_SESSION['hoa_alchemy_user_id'];
+  $username = $_SESSION['hoa_alchemy_username'];
+  $community_id = $_SESSION['hoa_alchemy_community_id'];
+  $community_code = $_SESSION['hoa_alchemy_community_code'];
+  $community_name = $_SESSION['hoa_alchemy_community_name'];
+
 date_default_timezone_set('America/Los_Angeles');
 ?>
 <!DOCTYPE html>
@@ -82,76 +99,101 @@ date_default_timezone_set('America/Los_Angeles');
 ?>
     <body>
 <div class="container">
-    <center><h3><span class="notbold">Stoneridge Square Association</span></h3></center>
     <br>
     <center><h4>GENERAL LEDGER</h4></center>
-     <center><h4><span class="notbold">January - December <?php echo date('Y')?></span></h4></center>
      <br>
-<table class="table table-responsive table-hover" id="example" style="font-size: 12px;">
-  <thead>
-        <tr><th style="width: 15%;">DATE</th><th>TRANSACTIONTYPE</th><th>NUM</th><th>NAME</th><th>MEMEO/DESCRIPTION</th><th>SPLIT</th><th>AMOUNT</th><th>BALANCE</th></tr>
-    </thead>
-    <tbody>
-            <?php
-            setlocale(LC_MONETARY, 'en_US');
-            error_reporting(E_ERROR | E_PARSE);
-            ini_set('display_errors', 1);
-            $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/reports/GeneralLedger?date_macro=This%20Fiscal%20Year');
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1509663029",oauth_nonce="VobfNZi3JwU",oauth_version="1.0",oauth_signature="4XCX1HNmLhF1DFp08eLyDXlwomI%3D"'));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            $result = curl_exec($ch);
-            $result = json_decode($result);
-            $idNum = 1;
-            foreach ($result->Rows->Row as $generalLedger) {
-                    $idVal = "row".$idNum;
-                  if ( isset($generalLedger->Header->ColData[0]->value) )
-                  {
-                    $printVal = '<tr class="clickable" data-toggle="collapse" id="'.$idVal.'" data-target=".'.$idVal.'">'.'<td><i class="glyphicon glyphicon-plus"></i>'.$generalLedger->Header->ColData[0]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[1]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[2]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[3]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[4]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[5]->value.'</td>'.'<td>'.money_format('%#10n',$generalLedger->Header->ColData[6]->value).'</td>'.'<td>'.money_format('%#10n',$generalLedger->Header->ColData[7]->value).'</td>'.'</tr>';
-                    echo $printVal;
-                    foreach ($generalLedger->Rows->Row as $childTrans) {
-                              if ( isset($childTrans->ColData[0]->value) ){
-                        echo '<tr class="collapse '.$idVal.'">
-                                <td>'.$childTrans->ColData[0]->value.'</td>
-                                <td>'.$childTrans->ColData[1]->value.'</td>
-                                <td>'.$childTrans->ColData[2]->value.'</td>  
-                                <td>'.$childTrans->ColData[3]->value.'</td>
-                                <td>'.$childTrans->ColData[4]->value.'</td>
-                                <td>'.$childTrans->ColData[5]->value.'</td>  
-                                <td>'.money_format('%#10n',$childTrans->ColData[6]->value).'</td>
-                                <td>'.money_format('%#10n',  $childTrans->ColData[7]->value).'</td>
-                                </tr>';
-                             
-                       }
-                       
-                  }
-                  if ( isset($generalLedger->Summary->ColData[0]->value)  ){
-                                    echo '<tr class="collapse '.$idVal.'">
-                                <td><b>'.$generalLedger->Summary->ColData[0]->value.'</b></td>
-                                <td><b>'.$generalLedger->Summary->ColData[1]->value.'</b></td>
-                                <td><b>'.$generalLedger->Summary->ColData[2]->value.'</b></td>  
-                                <td><b>'.$generalLedger->Summary->ColData[3]->value.'</b></td>
-                                <td><b>'.$generalLedger->Summary->ColData[4]->value.'</b></td>
-                                <td><b>'.$generalLedger->Summary->ColData[5]->value.'</b></td>  
-                                <td><b>'.money_format('%#10n',$generalLedger->Summary->ColData[6]->value).'</b></td>
-                                <td><b>'.money_format('%#10n',  $generalLedger->Summary->ColData[7]->value).'</b></td>
-                                </tr>';
-                    }
-                }
-                  else if ( isset($generalLedger->Summary->ColData[0]->value) ){
-                        $printVal = '<b><tr class="clickable" data-toggle="collapse" id="'.$idVal.'" data-target=".'.$idVal.'">'.'<td><b>'.$generalLedger->Summary->ColData[0]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[1]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[2]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[3]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[4]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[5]->value.'</b></td>'.'<td><b>'.money_format('%#10n', $generalLedger->Summary->ColData[6]->value).'</b></td>'.'<td><b>'.money_format('%#10n',  $generalLedger->Summary->ColData[7]->value).'</b></td>'.'</tr></b>';
-                    echo $printVal;
-                  }
-                  else {
-                                            
-                 }
-                 $idNum = $idNum +  1;
-                 
-            }
+                  <table id="example1" class="table table-bordered table-striped">
+                    
+                    <thead>
+                      
+                      <tr>
+                        
+                        <th>Date</th>
+                        <th>Transaction Type</th>
+                        <th>Num</th>
+                        <th>Name</th>
+                        <th>Memo / Description</th>
+                        <th>Split</th>
+                        <th>Amount</th>
+                        <th>Balance</th>
 
-        ?>
-    </tbody>
-  </table>
+                      </tr>
+
+                    </thead>
+
+                    <tbody>
+
+                      <?php
+
+                        setlocale(LC_MONETARY, 'en_US');
+                        error_reporting(E_ERROR | E_PARSE);
+                        ini_set('display_errors', 1);
+                          
+                        if($community_id == 2)
+                        {
+
+                          $ch = curl_init('https://quickbooks.api.intuit.com/v3/company/123145844183384/reports/GeneralLedger?date_macro=This%20Fiscal%20Year');
+              
+                          curl_setopt($ch, CURLOPT_CUSTOMREQUEST , 'GET');
+                          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/json','Authorization:OAuth oauth_consumer_key="qyprdRAm244oPXhP3miXslnVdpDfWF",oauth_token="qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1509663029",oauth_nonce="VobfNZi3JwU",oauth_version="1.0",oauth_signature="4XCX1HNmLhF1DFp08eLyDXlwomI%3D"'));
+                          curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+                          $result = curl_exec($ch);
+                          $result = json_decode($result);
+                          $idNum = 1;
+              
+                          foreach ($result->Rows->Row as $generalLedger) 
+                          {
+
+                            $idVal = "row".$idNum;
+
+                            if ( isset($generalLedger->Header->ColData[0]->value) )
+                            {
+
+                              $printVal = '<tr class="clickable" data-toggle="collapse" id="'.$idVal.'" data-target=".'.$idVal.'">'.'<td><i class="fa fa-plus"></i> '.$generalLedger->Header->ColData[0]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[1]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[2]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[3]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[4]->value.'</td>'.'<td>'.$generalLedger->Header->ColData[5]->value.'</td>'.'<td>'.money_format('%#10n',$generalLedger->Header->ColData[6]->value).'</td>'.'<td>'.money_format('%#10n',$generalLedger->Header->ColData[7]->value).'</td>'.'</tr>';
+
+                              echo $printVal;
+
+                              foreach ($generalLedger->Rows->Row as $childTrans) 
+                              {
+
+                                if ( isset($childTrans->ColData[0]->value) )
+                                {
+
+                                  echo '<tr class="collapse '.$idVal.'"><td>'.$childTrans->ColData[0]->value.'</td><td>'.$childTrans->ColData[1]->value.'</td><td>'.$childTrans->ColData[2]->value.'</td><td>'.$childTrans->ColData[3]->value.'</td><td>'.$childTrans->ColData[4]->value.'</td><td>'.$childTrans->ColData[5]->value.'</td><td>'.money_format('%#10n',$childTrans->ColData[6]->value).'</td><td>'.money_format('%#10n',  $childTrans->ColData[7]->value).'</td></tr>';
+                               
+                                }
+                         
+                              }
+
+                              if ( isset($generalLedger->Summary->ColData[0]->value)  )
+                              {
+
+                                echo '<tr class="collapse '.$idVal.'"><td><b>'.$generalLedger->Summary->ColData[0]->value.'</b></td><td><b>'.$generalLedger->Summary->ColData[1]->value.'</b></td><td><b>'.$generalLedger->Summary->ColData[2]->value.'</b></td><td><b>'.$generalLedger->Summary->ColData[3]->value.'</b></td><td><b>'.$generalLedger->Summary->ColData[4]->value.'</b></td><td><b>'.$generalLedger->Summary->ColData[5]->value.'</b></td><td><b>'.money_format('%#10n',$generalLedger->Summary->ColData[6]->value).'</b></td><td><b>'.money_format('%#10n',  $generalLedger->Summary->ColData[7]->value).'</b></td></tr>';
+
+                              }
+
+                            }
+                            else if ( isset($generalLedger->Summary->ColData[0]->value) )
+                            {
+
+                              $printVal = '<b><tr class="clickable" data-toggle="collapse" id="'.$idVal.'" data-target=".'.$idVal.'">'.'<td><b>'.$generalLedger->Summary->ColData[0]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[1]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[2]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[3]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[4]->value.'</b></td>'.'<td><b>'.$generalLedger->Summary->ColData[5]->value.'</b></td>'.'<td><b>'.money_format('%#10n', $generalLedger->Summary->ColData[6]->value).'</b></td>'.'<td><b>'.money_format('%#10n',  $generalLedger->Summary->ColData[7]->value).'</b></td>'.'</tr></b>';
+                                    
+                              echo $printVal;
+
+                            }
+                                
+                            $idNum = $idNum +  1;
+                   
+                          }
+
+                        }
+
+                      ?>
+                    
+                    </tbody>
+
+                  </table>
 </div>
   
     </body>
