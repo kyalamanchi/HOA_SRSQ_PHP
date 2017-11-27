54f1905e1cd07ab6d2f2b18873c7fc2c620b91c4
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-27 22:42:11 +0530

    changes

--- var/www/html/AnnualReport/addVolunteer.php
+++ var/www/html/AnnualReport/addVolunteer.php
@@ -8,8 +8,12 @@
 	$home_id = $_POST['home_id'];
 	$community_id = $_POST['community_id'];
 
-	print_r($_POST);
+	$result = pg_query("INSERT INTO volunteers (community_id, hoa_id, home_id, person_id, community_task_id) VALUES ($community_id, $hoa_id, $home_id, $person_id, $task)");
+
+	//print_r($_POST);
 
 	//echo $person_id." - - - ".$task;
+
+	header("Location: volunteers.php");
 
 ?>
