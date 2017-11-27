f024a14253174810bd6fa9dd450e7e5009be7260
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-27 23:19:47 +0530

    changes

--- var/www/html/AnnualReport/volunteers.php
+++ var/www/html/AnnualReport/volunteers.php
@@ -458,7 +458,7 @@
 		<script src="assets/js/charts.js"></script>
 		<script src="assets/js/custom.min.js"></script>
 
-		<script src='assets/js/disclosures.js'></script>
+		<script src='assets/js/volunteers.js'></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 	</body>
