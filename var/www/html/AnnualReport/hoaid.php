7493997c5e88cc361e45c99dceb4a07607816abd
GeethChadalawada <geethchadalawada@gmail.com>
2017-11-27 23:56:54 +0530

    testing

--- var/www/html/AnnualReport/hoaid.php
+++ var/www/html/AnnualReport/hoaid.php
@@ -16,6 +16,31 @@
 	$community_code = $_SESSION['hoa_alchemy_community_code'];
 	$community_name = $_SESSION['hoa_alchemy_community_name'];
 
+	function get_client_ip() {
+
+	    $ipaddress = '';
+
+	    if (getenv('HTTP_CLIENT_IP'))
+	        $ipaddress = getenv('HTTP_CLIENT_IP');
+	    else if(getenv('HTTP_X_FORWARDED_FOR'))
+	        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+	    else if(getenv('HTTP_X_FORWARDED'))
+	        $ipaddress = getenv('HTTP_X_FORWARDED');
+	    else if(getenv('HTTP_FORWARDED_FOR'))
+	        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+	    else if(getenv('HTTP_FORWARDED'))
+	       $ipaddress = getenv('HTTP_FORWARDED');
+	    else if(getenv('REMOTE_ADDR'))
+	        $ipaddress = getenv('REMOTE_ADDR');
+	    else
+	        $ipaddress = 'UNKNOWN';
+
+	    return $ipaddress;
+
+	}
+
+	echo get_client_ip();
+
 	$today = date('Y-m-d G:i:s');
 
 	$result = pg_fetch_assoc(pg_query("SELECT * FROM community_annual_report_pages WHERE community_id=$community_id"));
