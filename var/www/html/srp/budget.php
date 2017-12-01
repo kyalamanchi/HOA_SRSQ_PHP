e0d98689412e14e0f3e1d02b0030aa4df8c8dbde
GeethChadalawada <geethchadalawada@gmail.com>
2017-12-01 19:07:16 +0530

    changes

--- var/www/html/srp/budget.php
+++ var/www/html/srp/budget.php
@@ -120,7 +120,7 @@
 							
 						<div class='table-responsive col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
 						
-							<table id='example1' class='table table-striped' style="color: black;">
+							<table class='table table-striped' style="color: black;">
 										
 								<thead>
             <tr>
