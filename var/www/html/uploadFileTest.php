0d7b4e538bfce6b924b00ba159fb0abe9272d41d
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-01 16:04:13 +0530

    update

--- var/www/html/uploadFileTest.php
+++ var/www/html/uploadFileTest.php
@@ -265,7 +265,7 @@ function uploadFile(){
         request.open("POST", "https://hoaboardtime.com/uploadFileToDropbox.php", true);
         request.setRequestHeader("Content-type", "application/json");
         request.send(sendData);
-
+        
         var pleaseWaitData = '<div class="progress">\
                       <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
                       aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
@@ -276,7 +276,7 @@ function uploadFile(){
         $("#pleaseWaitDialog2").modal("show");
         request.onreadystatechange = function () {
           if (request.readyState == XMLHttpRequest.DONE) {
-
+            alert(request.responseText);
             $("#pleaseWaitDialog2").modal("hide");
           if (request.responseText == "An error occured."){
             swal("An error ocuured. Please try again. ","","error");
