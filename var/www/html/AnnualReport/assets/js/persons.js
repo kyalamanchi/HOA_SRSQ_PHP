554deca32e7f17b7adb8b4fd888267b22f8a9ca5
GeethChadalawada <geethchadalawada@gmail.com>
2017-12-05 21:30:43 +0530

    testing

--- var/www/html/AnnualReport/assets/js/persons.js
+++ var/www/html/AnnualReport/assets/js/persons.js
@@ -29,49 +29,12 @@ $('#person_edit_save').click(function(){
 				{
 					alert("Updated!");
 
-					//$('#user_cell_no').text(response);
-					//$('#user_information_radio_no').prop('checked', false);
-					//$('#edit_user_details_div').hide();
-					//$('#user_details_div').show();
-
 				}
 
 		}
 
 	});
 
-});
-
-$('form.ajax4').on('submit', function(){
-
-	var obj = $(this),
-	person_id = obj.attr('id'),
-	person_firstname = $('#edit_person_firstname_'+person_id).val(),
-	person_lastname = $('#edit_person_lastname_'+person_id).val(),
-	person_email = $('#edit_person_email_'+person_id).val(),
-	person_cell_no = $('#edit_person_cell_no_'+person_id).val(),
-	person_role = $('#edit_person_role_'+person_id).val(),
-	person_relationship = $('#edit_person_relationship_'+person_id).val();
-
-	data = {personId:person_id, personFirstname:person_firstname, personLastname:person_lastname, personEmail:person_email, personCellNo:person_cell_no, personRole: person_role, personRelationship: person_relationship};
-
-	$.ajax({
-
-		url: 'updatePerson.php',
-		method: "POST",
-		data: data,
-		success:function(response){
-
-			alert("Updated");
-
-			window.location = 'persons.php';
-
-		}
-
-	});
-
-	return false;
-	
 });
 
 $('form.ajax5').on('submit', function(){
@@ -106,49 +69,6 @@ $('form.ajax5').on('submit', function(){
 			{
 
 				alert("Person added.");
-
-				window.location = 'persons.php';
-
-			}
-
-		}
-
-	});
-
-	return false;
-	
-});
-
-$('form.ajax6').on('submit', function(){
-	
-	var obj = $(this),
-	url = obj.attr('action'),
-	method = obj.attr('method'),
-	id = obj.attr('id');
-
-	obj.find('[name]').each(function(index, value){
-
-		var input = $(this),
-		index = input.attr('name'),
-		value = input.val();
-
-		data[index] = value;
-
-	});
-
-	$.ajax({
-
-		url: url,
-		type: method,
-		data: {person_id:id},
-		success: function(response){
-
-			if(response == 'Some error occured. Please try again.')
-				alert(response);
-			else
-			{
-
-				alert("Person Removed.");
 
 				window.location = 'persons.php';
 
