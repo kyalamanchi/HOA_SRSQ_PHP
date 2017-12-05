0b0d13e56a03079d3c2eead7eb48a23c50cb4200
GeethChadalawada <geethchadalawada@gmail.com>
2017-12-05 22:25:43 +0530

    testing

--- var/www/html/boardDashboard.php
+++ var/www/html/boardDashboard.php
@@ -12,7 +12,7 @@
 	
     <?php
 
-      include "https://hoaboardtime.com/hoaResources/testing.php";                       
+      include "hoaResources/testing.php";                       
 
       pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
