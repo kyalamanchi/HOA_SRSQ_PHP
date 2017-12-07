6299725fc53f39da9db7c5f666a34d33397760d2
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-07 18:32:27 +0530

    changes

--- var/www/html/getFileCategoryDetails.php
+++ var/www/html/getFileCategoryDetails.php
@@ -28,9 +28,8 @@ else if ( $parseJSON[0]->type == "disclosure" ){
 
      $secondQuery = "SELECT * FROM community_disclosures where type_id=(select id from disclosure_type where name = '".$parseJSON[0]->sub_category."' and community_id=".$parseJSON[0]->community_id.") and community_id =".$parseJSON[0]->community_id;
      $secondQueryResult = pg_query($secondQuery);
-     $response = pg_fetch_assoc($queryResult);
-     echo 'Response is '.$response['notes'];
-
+     $row  = pg_fetch_assoc($secondQueryResult);
+     echo $row['notes'];
 }
 
 ?>
