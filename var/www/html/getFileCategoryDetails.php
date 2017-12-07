48c2133bc024b6cdfca2c335028f884a610ff766
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-07 21:44:08 +0530

    changes

--- var/www/html/getFileCategoryDetails.php
+++ var/www/html/getFileCategoryDetails.php
@@ -29,7 +29,7 @@ else if ( $parseJSON[0]->type == "disclosure" ){
      $secondQuery = "SELECT * FROM community_disclosures where type_id=(select id from disclosure_type where name = '".$parseJSON[0]->sub_category."' and community_id=".$parseJSON[0]->community_id.") and community_id =".$parseJSON[0]->community_id;
      }
      else {
-      $secondQuery = "SELECT * FROM community_disclosures where type_id=(select id from disclosure_type where name = '".$parseJSON[0]->sub_category."' and community_id=".$parseJSON[0]->community_id.") and community_id =".$parseJSON[0]->community_id." and valid_from='".$row['fiscal_year_start']."' and valid_until='".$row['fiscal_year_end']."'";
+      $secondQuery = "SELECT * FROM community_disclosures where type_id=(select id from disclosure_type where name = '".$parseJSON[0]->sub_category."' and community_id=".$parseJSON[0]->community_id.") and community_id =".$parseJSON[0]->community_id." and fiscal_year_start='".$row['valid_from']."' and fiscal_year_end='".$row['valid_until']."'";
      }
      echo $secondQuery;
      $secondQueryResult = pg_query($secondQuery);
