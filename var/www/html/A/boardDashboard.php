e0dabde2cf0f386098b20a9c5cdcb8e9ed7c566a
GeethChadalawada <geethchadalawada@gmail.com>
2017-12-07 17:01:50 +0530

    testing

--- var/www/html/A/boardDashboard.php
+++ var/www/html/A/boardDashboard.php
@@ -2055,7 +2055,7 @@
     </script>
 
     <script src='assets/js/home.js'></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script-->
 		
 	</body>
 
