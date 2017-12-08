c1f758f3f505c5ab95602e61b8bc62301f2d3080
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-08 21:09:42 +0530

    changes

--- var/www/html/sendSMS.php
+++ var/www/html/sendSMS.php
@@ -208,7 +208,8 @@ input, label {
                  <button type="button" class="btn btn-success" onclick="sendToAllSubscribers();" id="sendToAllButton" disabled="disabled">Send to all subscribers</button>
         </div>
         <div>
-              OR
+              <br>
+              <h4>OR</h4>
               <br>
               <label>Select a member</label>
               <select class="selectpicker" data-show-subtext="true" data-live-search="true" id="eventType">
