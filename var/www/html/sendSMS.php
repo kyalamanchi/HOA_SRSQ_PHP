0bc753ebb9979af1f5bab1f5e0cbdc7901e60e16
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-08 20:37:11 +0530

    update

--- var/www/html/sendSMS.php
+++ var/www/html/sendSMS.php
@@ -195,7 +195,8 @@ input, label {
         <div class="form-group" style="width: 25%">
                   <label for="comment">Message:</label>
                   <textarea class="form-control" rows="5" id="comment"></textarea>
-                  <button>Address</button>
+                  <label>Insert:</label>
+                  <button>Address</button><button>Name</button>
         </div>
         </div>
      
