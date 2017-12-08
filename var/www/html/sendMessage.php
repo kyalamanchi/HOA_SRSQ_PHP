896fa3a285362a292ae7f10776f9fecb4f904a00
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-09 00:54:19 +0530

    update

--- var/www/html/sendMessage.php
+++ var/www/html/sendMessage.php
@@ -7,8 +7,24 @@ date_default_timezone_set('America/Los_Angeles');
 pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
 
+
+
 $data = file_get_contents('php://input');
 $parseJSON = json_decode($data);
+
+
+$homeQuery = "SELECT * FROM HOMEID WHERE COMMUNITY_ID=".$parseJSON[0]->community_id;
+
+$homeQueryResult = pg_query($homeQuery);
+
+$homeIDS = array();
+
+while ($row23 = pg_fetch_assoc($homeQueryResult)) {
+	$homeIDS[$row23['home_id']] = $row23['address1'];
+}
+
+
+
 
 $message = $parseJSON[0]->message_body;
 
@@ -24,12 +40,25 @@ if ( $parseJSON[0]->mode == "all" ){
 	}
 
 	foreach ($personIDS as $key => $value) {
-		$query = "SELECT CELL_NO FROM PERSON WHERE ID=".$key;
+		
+		$query = "SELECT * FROM PERSON WHERE ID=".$key;
 		$queryResult = pg_query($query);
 		$row = pg_fetch_assoc($queryResult);
 		if ( $row['cell_no'] ){
-		print_r(base64_decode($row['cell_no']));
-		print_r(nl2br("\n"));
+		
+		if ( $parseJSON[0]->community_id == 1){
+			$accountID = '';
+			$authToken  = '';
+		}
+		else if ( $parseJSON[0]->community_id == 2){
+			$accountID = 'AC9370eeb4b1922b7dc29d94c387b3ab56';
+			$authToken  = '3b29450d9ce0e5ec7ba6b328f05525a2';
+		}
+
+		$message = str_replace('#address#', $homeIDS[$row['home_id']], $message);
+		$message = str_replace('#name#', $row['fname'].' '.$row['lname'], $message);
+		echo $message;
+
 		}
 		else {
 			echo "No Phone number found.";
@@ -38,11 +67,27 @@ if ( $parseJSON[0]->mode == "all" ){
 
 }
 else if ( $parseJSON[0]->mode == "single" ){
+	
 	$query = "SELECT * FROM HOAID WHERE HOA_ID=".$parseJSON[0]->hoa_id;
 	$queryResult = pg_query($query);
 	$row = pg_fetch_assoc($queryResult);
 	if ( $row['cell_no'] ){
-	echo ($row['cell_no']);
+		
+		if ( $parseJSON[0]->community_id == 1){
+			$accountID = '';
+			$authToken  = '';
+		}
+		else if ( $parseJSON[0]->community_id == 2){
+			$accountID = 'AC9370eeb4b1922b7dc29d94c387b3ab56';
+			$authToken  = '3b29450d9ce0e5ec7ba6b328f05525a2';
+		}
+
+		$message = str_replace('#address#', $homeIDS[$row['home_id']], $message);
+		$message = str_replace('#name#', $row['firstname'].' '.$row['lastname'], $message);
+		echo $message;
+
+
+
 	}
 	else 
 	{
