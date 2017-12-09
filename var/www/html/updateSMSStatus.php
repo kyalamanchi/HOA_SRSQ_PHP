739287470aa6b7a8ff66b6315e55d05300fefa98
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-09 17:47:41 +0530

    changes

--- var/www/html/updateSMSStatus.php
+++ var/www/html/updateSMSStatus.php
@@ -11,17 +11,19 @@ date_default_timezone_set('America/Los_Angeles');
 		$smsArray[$row['sid']]  = 1;
 	}
 
+	$accountID = 'AC9370eeb4b1922b7dc29d94c387b3ab56';
+	$authToken  = '3b29450d9ce0e5ec7ba6b328f05525a2';
 
-	$url = 'https://api.twilio.com/2010-04-01/Accounts/AC06019424f034503e8a7c67a8ddfcd490/Messages.json?PageSize=1000&Page=0';
+
+	$url = 'https://api.twilio.com/2010-04-01/Accounts/'.$accountID.'/Messages.json?PageSize=1000&Page=0';
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_POST, 0);
-	curl_setopt($ch, CURLOPT_USERPWD, "AC06019424f034503e8a7c67a8ddfcd490" . ":" . "a73768c36829436835653b51dd3c693c");
+	curl_setopt($ch, CURLOPT_USERPWD, $accountID . ":" . $authToken);
 	$res = curl_exec($ch);
 	$res = json_decode($res);
 	foreach ($res->messages as $message) {
-
 		if ( $smsArray[$message->sid] ){
 			$qr = "UPDATE SMS_SENT SET UPDATED_BY=401,UPDATED_ON='".date('Y-m-d H:i:s')."', STATUS='$message->status',date_updated='".date('Y-m-d H:i:s',strtotime($message->date_updated))."' WHERE SID='$message->sid'";
 			pg_query($qr);
