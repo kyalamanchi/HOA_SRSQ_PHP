bd85a168f3ae99e234d165ea9fe748568e31ef2a
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-10 20:54:00 +0530

    changes

--- var/www/html/genericNoticeCombine.php
+++ var/www/html/genericNoticeCombine.php
@@ -1,4 +1,7 @@
 <?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	
 	date_default_timezone_set("America/New_York");
 	header("Content-Type: text/event-stream\n\n");
  	$connection =  pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database.......");
