79a0a858d9d1525802a65aa6d4ff3835822fe39a
GeethChadalawada <geethchadalawada@gmail.com>
2017-12-10 10:22:22 +0530

    changes

--- var/www/html/boardDashboard.php
+++ var/www/html/boardDashboard.php
@@ -933,7 +933,7 @@
 
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
 
-                      <a href='boardCommunitySignedAgreements.php'>
+                      <a href='signedAgreements.php'>
 
                         <?php 
 
