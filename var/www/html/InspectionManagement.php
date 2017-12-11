a139c70d044bd7a1b74ab15115214ad958b5539e
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-12 02:28:01 +0530

    test

--- var/www/html/InspectionManagement.php
+++ var/www/html/InspectionManagement.php
@@ -225,7 +225,7 @@ function previewAndGenerate(button){
               }
               else {
                 var data = JSON.parse(request.responseText.split("@")[0]);
-
+                alert(data.pdf);
                 var fieldData = '<button type="button" class="btn btn-default" onclick="showPreview('+data.pdf+')">View</button>\
           <button type="button" class="btn btn-primary pull-right" onclick="" style="padding-left: 5px;">Send Notice(s) - SouthData</button>\
           <button type="button" class="btn btn-primary pull-right" onclick="" style="padding-right: 5px;">Email Notice(s)</button>';
