c1031dca8504238744a044a70e6882ecd87f5dac
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-13 00:08:19 +0530

    changes

--- var/www/html/mailFileToSouthData.php
+++ var/www/html/mailFileToSouthData.php
@@ -1,3 +1,9 @@
 <?php
 
+date_default_timezone_set('America/Los_Angeles');
+$data = file_get_contents('php://input');
+$parsJSON = json_decode($data);
+pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+
+echo $parsJSON[0];
 ?>
