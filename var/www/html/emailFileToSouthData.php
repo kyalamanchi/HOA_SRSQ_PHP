42f6a7bb9ccc6c295e7c8ff80e90233eb3d5b694
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-13 22:32:03 +0530

    changes

--- var/www/html/emailFileToSouthData.php
+++ var/www/html/emailFileToSouthData.php
@@ -5,22 +5,26 @@
 			$parsJSON = json_decode($data);
 			pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-			$url = "http://southdata.us-west-2.elasticbeanstalk.com/TestOrderMailing.aspx?file_id=".$parsJSON[0]->zip_id."&hoaid=".$parsJSON[0]->hoa_id."&type=0";
+            $hoaID = $parsJSON[0]->hoa_id;
 
-            $req = curl_init();
-            curl_setopt($req, CURLOPT_URL,$url);
-            curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
-            if(curl_exec($req) === false)
-            {
-                $message =  "An error occured.";
-                echo $message;
-                exit(0);
-            }
-            else 
-            {   
-                $message = "File uploaded to South Data.";
-                echo $message;       
-            }       
-            $query = "INSERT INTO files_sent(hoa_id,file_tech_id,sent_date,file_name) VALUES(".$parsJSON[0]->hoa_id.",'".$parsJSON[0]->zip_id."','".date('Y-m-d H:i:s')."','Inspection Notice')";
-            pg_query($query);
+            $fileID = $parsJSON[0]->pdf_id;
+
+            echo "An error occured.";
+
+            // $req = curl_init();
+            // curl_setopt($req, CURLOPT_URL,$url);
+            // curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+            // if(curl_exec($req) === false)
+            // {
+            //     $message =  "An error occured.";
+            //     echo $message;
+            //     exit(0);
+            // }
+            // else 
+            // {   
+            //     $message = "File uploaded to South Data.";
+            //     echo $message;       
+            // }       
+            // $query = "INSERT INTO files_sent(hoa_id,file_tech_id,sent_date,file_name) VALUES(".$parsJSON[0]->hoa_id.",'".$parsJSON[0]->zip_id."','".date('Y-m-d H:i:s')."','Inspection Notice')";
+            // pg_query($query);
 ?>
