f789469989b3cf354dfbbda49e9fd0c40b9a74f5
GeethChadalawada <geethchadalawada@gmail.com>
2017-12-15 19:15:33 +0530

    script

--- var/www/html/AnnualReport/script.php
+++ var/www/html/AnnualReport/script.php
@@ -2,6 +2,24 @@
 	
 	pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
-	
+	$res = pg_query("SELECT * FROM hoaid");
+
+	while($row = pg_fetch_assoc($res))
+	{
+
+		$cell_no = $row['cell_no'];
+		$hoa_id = $row['hoa_id'];
+
+		if($cell_no != '')
+		{
+
+			$ecell_no = base64_encode($cell_no);
+
+			$result = pg_query("UPDATE hoaid SET cell_no='$ecell_no' WHERE hoa_id=$hoa_id");
+
+			echo "<br>".$hoa_id." - - - ".$cell_no." - - - ".$ecell_no." - - - ".$result;
+
+		}
+	}
 
 ?>
