7a0f87224a89313ca173c59781bcb8aa0d41cda6
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-17 01:04:31 +0530

    changes

--- var/www/html/mobile/getMembers.php
+++ var/www/html/mobile/getMembers.php
@@ -13,7 +13,7 @@ $queryResult = pg_query($query);
 $homeID = array();
 
 while ($row = pg_fetch_assoc($queryResult)) {
-	$homeID['home_id'] = $row['address1'];
+	print_r($row);
 }
 
 print_r($homeID);
