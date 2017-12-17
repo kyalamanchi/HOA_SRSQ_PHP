9be0c9c814b5d7d2005b2932fa02c0962bd949f8
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-18 00:33:06 +0530

    changes

--- var/www/html/communityDocuments.php
+++ var/www/html/communityDocuments.php
@@ -151,7 +151,7 @@
                                 else
                                   $category = 'Others';
 
-                                echo "<tr><td><a href='https://hoaboardtime.com/getDocumentPreviewTest.php?path=$document_url&desc=$desc&cid=$community_id&t=-2' target='_blank'>$date_of_upload</a></td><td><a href='https://hoaboardtime.com/getDocumentPreviewTest.php?path=$document_url&desc=$desc&cid=$community_id' target='_blank'>$desc</a></td></tr>";
+                                echo "<tr><td><a href='https://hoaboardtime.com/getDocumentPreviewTest.php?path=$document_url&desc=$desc&cid=$community_id&t=-2' target='_blank'>$date_of_upload</a></td><td><a href='https://hoaboardtime.com/getDocumentPreviewTest.php?path=$document_url&desc=$desc&cid=$community_id&t=-2' target='_blank'>$desc</a></td></tr>";
 
                               }
 
