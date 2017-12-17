727b9d36ef1cbab0786ad9c2a191d342d7dbbe08
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-17 22:58:56 +0530

    changes

---
+++ var/www/html/mobile/sendViaMandrillEmails2.php
@@ -0,0 +1,29 @@
+<?php
+date_default_timezone_set('America/Los_Angeles');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
+$hoaID = $_GET['hoa_id'];
+$query = "SELECT EMAIL FROM HOAID WHERE HOA_ID=".$hoaID;
+$queryResult = pg_query($query);
+$row = pg_fetch_assoc($queryResult);
+
+if ( isset($row['email']) ){
+	$email  = $row['email'];
+	$email  = "dhivysh@gmail.com";
+	$url = "https://hoaboardtime.com/sendViaMandrill.php?hoaid=".$hoaID."&email=".$email;
+	$req = curl_init();
+	curl_setopt($req, CURLOPT_URL,$url);
+	curl_exec($req);
+	$response = array();
+	$response["response"] = "success";
+
+	echo json_encode($response);
+}
+else {
+	$response = array();
+	$response["response"] = "error";
+	echo json_encode($response);
+}
+
+?>
