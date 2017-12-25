504c23bb8171ac5038f1d5c0d1c7283b957023de
GeethChadalawada <geethchadalawada@gmail.com>
2017-12-26 00:02:57 +0530

    login db change

--- var/www/html/login.php
+++ var/www/html/login.php
@@ -11,7 +11,7 @@
 
 	include 'password.php';
 
-	pg_connect("host=srsq-only.crsa3tdmtcll.ussrsq-only.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
+	pg_connect("host=srsq-only.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy");
 
 	$query = "SELECT * FROM usr WHERE email='".$login_email."'";
 	$result = pg_query($query);
