a0e09c44deb1141c6056a00afbb93f568ee073b7
Dhivysh <Dhivysh@users.noreply.github.com>
2017-12-26 21:47:33 +0530

    changes

--- var/www/html/srp/pay/verifyUser.php
+++ var/www/html/srp/pay/verifyUser.php
@@ -5,7 +5,8 @@ $message  = "Please Wait...";
 echo 'data: '.$message."\n\n";  
 ob_end_flush();
 flush();
-$connection = pg_connect("host=srsq-only.crsa3tdmtcll.ussrsq-only.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
+// $connection = pg_connect("host=srsq-only.crsa3tdmtcll.ussrsq-only.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
+$connection = pg_connect("host=hoapgtest.crsa3tdmtcll.us-west-1.rds.amazonaws.com port=5432 dbname=SRP user=HOA_serviceID password=hoaalchemy") or die("Failed to connect to database");
 if ( $_GET['id'] ){
 $message  = $_GET['id'];
 echo 'data: '.$message."\n\n";  
