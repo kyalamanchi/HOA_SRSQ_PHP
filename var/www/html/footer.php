80df79b22be7887bb128514aab90a71782b114d8
kkolli1127 <kirankolli@gmail.com>
2017-12-29 17:22:32 +0530

    remove privacy policy

--- var/www/html/footer.php
+++ var/www/html/footer.php
@@ -11,6 +11,6 @@
 
     <div class="pull-right hidden-xs"></div>
         
-    <center><strong>Copyright &copy; <?php echo date('Y'); ?> - <a target='_blank' href="https://www.stoneridgesquare.org">Stoneridge Square Association</a> - <a target='_blank' href='https://termsfeed.com/privacy-policy/7803b3220cee25bd8bdb55c3d2a5dcb5'>Privacy Policy</a> - </strong> All rights reserved</center>
+    <center><strong>Copyright &copy; <?php echo date('Y'); ?> - </strong> All rights reserved</center>
 
 </footer>
