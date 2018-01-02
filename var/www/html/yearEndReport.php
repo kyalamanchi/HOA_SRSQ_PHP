1059654d78e9d3dc912c067bb5295967835f2913
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-03 05:03:20 +0530

    Year End Report

--- var/www/html/yearEndReport.php
+++ var/www/html/yearEndReport.php
@@ -32,7 +32,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     
-    <title><?php echo $_SESSION['hoa_community_name']; ?></title>
+    <title><?php echo $_SESSION['hoa_community_name'].' Year End Report 2017'; ?></title>
     
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
