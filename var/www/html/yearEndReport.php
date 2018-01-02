3b8aae46e416da968f8062c05c676580a7e59089
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-03 01:40:59 +0530

    test

--- var/www/html/yearEndReport.php
+++ var/www/html/yearEndReport.php
@@ -47,20 +47,27 @@
 
     <script>
 
-      $(document).ready(function() {
-        $('#example').DataTable( {
+      // $(document).ready(function() {
+      //   $('#example').DataTable( {
+      //     dom: 'Bfrtip',
+      //     buttons: [
+      //       'copy', 'csv', 'excel', 'pdf', 'print'
+      //     ],
+      //     "pageLength": 50
+      //     } );
+      // } );
+
+
+      $(function () {
+        $("#example1").DataTable({ 
+          "pageLength": 50,
           dom: 'Bfrtip',
           buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
-          ],
-          "pageLength": 50
-          } );
-      } );
+          ]
 
-
-      // $(function () {
-      //   $("#example1").DataTable({  });
-      // });
+         });
+      });
     </script>
 
     <script type="text/javascript">
@@ -768,21 +775,26 @@ function hidePleaseWait() {
 
     </div>
 
-    <script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!--     <script src="plugins/jQuery/jquery-2.2.3.min.js"></script> -->
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
-    <script src="//cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+<!--     <script src="plugins/datatables/jquery.dataTables.min.js"></script>
+ -->  
+   <!-- <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script> -->
+    <!-- <script src="//cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script> -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
     <script src="//cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
-    <script src="plugins/datatables/dataTables.bootstrap.min.js"></script>
+    <!-- <script src="plugins/datatables/dataTables.bootstrap.min.js"></script> -->
     <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
     <script src="plugins/fastclick/fastclick.js"></script>
     <script src="dist/js/app.min.js"></script>
     <script src="dist/js/demo.js"></script>
 
+
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.1.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.1.2/js/buttons.flash.min.js"></script>
 
 
   </body>
