f218694c5b920995b2ffcb0661af1bf5c937f5b2
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-03 01:29:33 +0530

    Revert "changes"
    This reverts commit 18e7b47b1a93ad658ad3fee0829a0702958b7e3f.

--- var/www/html/yearEndReport.php
+++ var/www/html/yearEndReport.php
@@ -761,6 +761,7 @@ function hidePleaseWait() {
     <script src="dist/js/demo.js"></script>
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
@@ -779,10 +780,15 @@ function hidePleaseWait() {
          
 
 =======
+=======
+>>>>>>> parent of 18e7b47b... changes
     <script>
       $(function () {
         $("#example1").DataTable({ "pageLength": 50 });
       });
+<<<<<<< HEAD
+>>>>>>> parent of 18e7b47b... changes
+=======
 >>>>>>> parent of 18e7b47b... changes
     </script>
 
