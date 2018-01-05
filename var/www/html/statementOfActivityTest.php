a8cc5c5cc3bd267aab9d49533bbadd24a4b2125b
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-05 22:05:11 +0530

    changes

--- var/www/html/statementOfActivityTest.php
+++ var/www/html/statementOfActivityTest.php
@@ -92,30 +92,7 @@
               $jsonprofitandloss = json_decode($profitandloss,TRUE);
               $data = $jsonprofitandloss['Rows']['Row'];
 
-              $case = 0;
 
-              foreach ($data as $profitAndLossAll) {
-                  if ( isset($profitAndLossAll['Header']['ColData'][0]['value']) ){
-                    $string = $profitAndLossAll['Header']['ColData'][0]['value'];
-                    $case = 1;
-                  }
-                  if ( isset($profitAndLossAll['Summary']['ColData'][0]['value']) ){
-                    $string = $profitAndLossAll['Summary']['ColData'][0]['value'];
-                    $case = 2;
-                  }
-                  if ( $case == 1 ){
-                  if ( isset($profitAndLossAll['Summary']['ColData'][1]['value']) ){
-                      print_r($string." ".$profitAndLossAll['Summary']['ColData'][1]['value']);
-                      print_r(nl2br("\n\n"));
-                  }
-                }
-                if ( $case == 2 ){
-                  if ( isset($profitAndLossAll['Summary']['ColData'][0]['value']) ){
-                      print_r($string." ".$profitAndLossAll['Summary']['ColData'][1]['value']);
-                      print_r(nl2br("\n\n"));
-                  }
-                }
-              }
 
 
 
@@ -365,7 +342,32 @@
                       <tr>
                         
                         <?php
-                          
+
+                           $case = 0;
+
+                          foreach ($data as $profitAndLossAll) {
+                                 if ( isset($profitAndLossAll['Header']['ColData'][0]['value']) ){
+                                      $string = $profitAndLossAll['Header']['ColData'][0]['value'];
+                                       $case = 1;
+                                  }
+                                if ( isset($profitAndLossAll['Summary']['ColData'][0]['value']) ){
+                                      $string = $profitAndLossAll['Summary']['ColData'][0]['value'];
+                                       $case = 2;
+                                 }
+                                if ( $case == 1 ){
+                                  if ( isset($profitAndLossAll['Summary']['ColData'][1]['value']) ){
+                                      print_r($string." ".$profitAndLossAll['Summary']['ColData'][1]['value']);
+                                      print_r(nl2br("\n\n"));
+                                 }
+                          }
+                         if ( $case == 2 ){
+                           if ( isset($profitAndLossAll['Summary']['ColData'][0]['value']) ){
+                             print_r($string." ".$profitAndLossAll['Summary']['ColData'][1]['value']);
+                              print_r(nl2br("\n\n"));
+                             }
+                          }
+                         }
+
                           echo '<td>Revenue</td>';
                           echo '<td>';
                           echo '<b>';
