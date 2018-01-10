a00c7d7eb8978b89c5eadc33d2f6144d183731d6
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-11 01:44:39 +0530

    changes

--- var/www/html/uploadFileTest.php
+++ var/www/html/uploadFileTest.php
@@ -436,6 +436,7 @@ function uploadFile(){
         request.onreadystatechange = function () {
           if (request.readyState == XMLHttpRequest.DONE) {
             $("#pleaseWaitDialog2").modal("hide");
+            alert(request.responseText);
             if ( request.responseText == "An error occured."){
               swal("An error ocuured. Please try again. ","","error");
             }
