0ea3b5daeef6b3c229dbd0d50eee7019dfe0090a
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-12 16:21:39 +0530

    changes

--- var/www/html/getFileCategoryDetails.php
+++ var/www/html/getFileCategoryDetails.php
@@ -21,7 +21,6 @@ if ( $parseJSON[0]->type == "legal" ){
      $secondQuery = "SELECT * FROM COMMUNITY_LEGAL_DOCS WHERE legal_docs_type_id=(SELECT ID FROM legal_docs_type WHERE name='".$parseJSON[0]->sub_category."' and community_id=".$parseJSON[0]->community_id.") AND COMMUNITY_ID=".$parseJSON[0]->community_id;
      }
      else {
-      // $secondQuery = "SELECT * FROM community_disclosures where type_id=(select id from disclosure_type where name = '".$parseJSON[0]->sub_category."' and community_id=".$parseJSON[0]->community_id.") and community_id =".$parseJSON[0]->community_id." and fiscal_year_start='".$row['valid_from']."' and fiscal_year_end='".$row['valid_until']."'";
      $secondQuery = "SELECT * FROM COMMUNITY_LEGAL_DOCS WHERE legal_docs_type_id=(SELECT ID FROM legal_docs_type WHERE name='".$parseJSON[0]->sub_category."' and community_id=".$parseJSON[0]->community_id.") AND COMMUNITY_ID=".$parseJSON[0]->community_id." and valid_from='".$row['valid_from']."' and valid_until='".$row['valid_until']."'";
      }
      $secondQueryResult = pg_query($secondQuery);
