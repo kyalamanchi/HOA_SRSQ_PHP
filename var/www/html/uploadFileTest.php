4a59815696126a4f53adda305fd6cde55d3ce6ff
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-13 02:02:50 +0530

    Revert "changes"
    This reverts commit bd8abd5b837459bcfef446f5f9fc389278ea75e1.

--- var/www/html/uploadFileTest.php
+++ var/www/html/uploadFileTest.php
@@ -71,11 +71,6 @@
   width: 60px;
   height: 34px;
 }
-.floating-box {
-    float: left;
-    margin: 10px;
-
-}
 .switch input {display:none;}
 .slider {
   position: absolute;
@@ -823,7 +818,7 @@ function uploadFile(){
                     if ( isset($row23) ){
                           if ( $row23['id'] ){
                           if ( $row23['document_id'] ){
-                            echo '<div class="floating-box"><a href="https://hoaboardtime.com/documentPreview.php?path='.$row23['document_id'].'&desc=preview" target="_blank">'.$row2['name'].'</a></div>';
+                            echo '<a style="display:inline-block; margin-right:20px;" href="https://hoaboardtime.com/documentPreview.php?path='.$row23['document_id'].'&desc=preview" target="_blank">'.$row2['name'].'</a>';
                           }
                           else {
                             echo "Not found";
