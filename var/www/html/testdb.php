9ab200238fe295f4d50b231864ae43ea5b4606c5
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-12 16:29:58 +0530

    changes

--- var/www/html/testdb.php
+++ var/www/html/testdb.php
@@ -8,14 +8,21 @@
 // $response = curl_exec($ch);
 // // header('Content-type: application/pdf'); header('Content-Disposition: inline; filename="a.pdf"'); 
 // echo $response;
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
- $consumer_key     = 'qyprdRAm244oPXhP3miXslnVdpDfWF';
-            $consumer_secret  = 'i3EglyzIw5eIUBXRttNUd7xBc5zqyxZxguHlYtdo';
-            $access_token     = 'qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v';
-            $access_secret    = '1XKRXuoXJeSKp84LmA1SF6aPFrywn4tOsUpB0Sn5';
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+//  $consumer_key     = 'qyprdRAm244oPXhP3miXslnVdpDfWF';
+//             $consumer_secret  = 'i3EglyzIw5eIUBXRttNUd7xBc5zqyxZxguHlYtdo';
+//             $access_token     = 'qyprdwVPs6UkPK3Xrpe9XMGvlGdJa6EUg0s65QPt2Cgsr14v';
+//             $access_secret    = '1XKRXuoXJeSKp84LmA1SF6aPFrywn4tOsUpB0Sn5';
 
-            $oauth = new OAuth($consumer_key,$consumer_secret,OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_AUTHORIZATION);
-            print_r($oauth);
+//             $oauth = new OAuth($consumer_key,$consumer_secret,OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_AUTHORIZATION);
+//             print_r($oauth);
+
+include 'includes/dbconn.php';
+
+$query = "SELECT * FROM COMMUNITY_LEGAL_DOCS WHERE legal_docs_type_id=(SELECT ID FROM legal_docs_type WHERE name='Voting & Election Rules' and community_id=2) AND COMMUNITY_ID=2 and valid_from='2017-01-01' and valid_until='2018-12-31'";
+$queryResult = pg_query($query);
+
+print_r($queryResult);
 
 ?>
