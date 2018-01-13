e3846392e6fc6d65ad0178148eadfd4d76e0fdfa
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-13 19:27:25 +0530

    changes

--- var/www/html/uploadFileTest.php
+++ var/www/html/uploadFileTest.php
@@ -666,6 +666,11 @@ function uploadFile(){
                       <option>Invoices</option>
               </select>
         </div>
+        <br>
+      <label class="btn btn-default">Select File<input type="file" id="fileInput" hidden disabled="disabled">      
+      </label>
+      <h5 id="label"></h5>
+      <button type="button" class="btn btn-success" onclick="uploadFile();" id="saveButton" disabled="disabled">Upload</button>
       <br>
       <div  class="row" id="legalContent" hidden="hidden">
       <div class="col-xs-6">
@@ -1009,11 +1014,6 @@ function uploadFile(){
       <input type="text" class="form-control daterange"  id="validUntil"/>
       <div id ="pdf"></div>
      </div>
-      <br>
-      <label class="btn btn-default">Select File<input type="file" id="fileInput" hidden disabled="disabled">      
-      </label>
-      <h5 id="label"></h5>
-      <button type="button" class="btn btn-success" onclick="uploadFile();" id="saveButton" disabled="disabled">Upload</button>
       </div>
 
 
