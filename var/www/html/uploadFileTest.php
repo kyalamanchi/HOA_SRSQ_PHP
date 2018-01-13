2edab5668460bdc63da52b8d75d3a0d77ddc8785
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-13 21:11:50 +0530

    changes

--- var/www/html/uploadFileTest.php
+++ var/www/html/uploadFileTest.php
@@ -1051,7 +1051,7 @@ function uploadFile(){
       <input type="text" class="form-control daterange"  id="validUntil"/>
       </div>
       <div class="col-xs-6">
-        <label id="preview"> Select a file to load preview</label>
+        <center><label id="preview"> Select a file to load preview</label></center>
         <embed width="100%" height="400px" name="plugin" id="embdLink" />
       </div>
       </div>
@@ -1067,7 +1067,7 @@ function uploadFile(){
           document.getElementById("name").value = res[res.length-2];
           document.getElementById("saveButton").disabled = false;
           document.getElementById("label").innerHTML = fileName;
-          document.getElementById("preview").value = "Preview";
+          document.getElementById("preview").innerHTML = "Preview";
           $('#embdLink')[0].src = window.URL.createObjectURL(new Blob([this.files[0]], {"type":"application/pdf"}));
           getFileData();
         };
