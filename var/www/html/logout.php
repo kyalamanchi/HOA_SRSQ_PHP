ac122ab70ad9d91d7ee508bce442c6c7e3eb485f
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-17 00:35:46 +0530

    test

--- var/www/html/logout.php
+++ var/www/html/logout.php
@@ -4,6 +4,14 @@
 	include 'includes/dbconn.php';
 	$hoa_id = $_SESSION['hoa_hoa_id'];
 
+    $ip = $_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']);
+	$userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+	$escapedAgent = pg_escape_string($userAgent);
+
+	$insertResult = pg_query("INSERT INTO user_access_log (ip_address, user_agent, hoa_id, access_date,access_page) VALUES ('$ip', '{$escapedAgent}', $hoa_id, '".date('Y-m-d H:i:s')."','Logout')");
+
+
 	session_start();
     session_unset();
     session_destroy();
@@ -11,12 +19,7 @@
     //setcookie(session_name(),'',0,'/');
     session_regenerate_id(true);
 
-    $ip = $_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']);
-	$userAgent = $_SERVER['HTTP_USER_AGENT'];
 
-	$escapedAgent = pg_escape_string($userAgent);
-
-	$insertResult = pg_query("INSERT INTO user_access_log (ip_address, user_agent, hoa_id, access_date,access_page) VALUES ('$ip', '{$escapedAgent}', $hoa_id, '".date('Y-m-d H:i:s')."','Logout')");
 	
 	header('Location: https://hoaboardtime.com/');
 	
