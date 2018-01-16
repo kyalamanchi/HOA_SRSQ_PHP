37f7c666deb82f9a985019e372b4e681a5de5b34
Dhivysh <Dhivysh@users.noreply.github.com>
2018-01-17 00:25:32 +0530

    test

--- var/www/html/login.php
+++ var/www/html/login.php
@@ -4,7 +4,7 @@
   	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
   	session_start();
-
+  	date_default_timezone_set('America/Los_Angeles');
 	$login_email = $_POST['login_email'];
 	$login_password = $_POST['login_password'];
 
