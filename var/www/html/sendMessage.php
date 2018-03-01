16de563b55a415f64225656eb2e2382fd3ad2ff5
Dhivysh <Dhivysh@users.noreply.github.com>
2018-03-01 23:05:05 +0530

    clean up

--- var/www/html/sendMessage.php
+++ var/www/html/sendMessage.php
@@ -86,7 +86,7 @@ if ( $parseJSON[0]->mode == "all" ){
 		$messageSub = str_replace('#address#', $homeIDS[$row['home_id']], $messageSub);
 		$messageSub = str_replace('#name#', $row['fname'].' '.$row['lname'], $messageSub);
 		$messageSub = str_replace('#month#', date('F'), $messageSub);
-		// $toNumber = '919603923649';
+	
 
 		$url  = 'https://api.twilio.com/2010-04-01/Accounts/'.$accountID.'/Messages.json';
 		$ch = curl_init();
@@ -136,7 +136,7 @@ else if ( $parseJSON[0]->mode == "single" ){
 		$messageSub = str_replace('#month#', date('F'), $messageSub);
 
 		$toNumber = $homeCountries[$row['home_id']].base64_decode($row['cell_no']);
-		// $toNumber = '919603923649';
+	
 		$url  = 'https://api.twilio.com/2010-04-01/Accounts/'.$accountID.'/Messages.json';
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
--- var/www/html/sendTestMessage.php
+++ var/www/html/sendTestMessage.php
@@ -9,13 +9,7 @@ $token = '3b29450d9ce0e5ec7ba6b328f05525a2';
 $client = new Client($sid, $token);
 
 
-// $client->messages->create(
-//     '+919603923649',
-//     array(
-//         'from' => '+19253996642',
-//         'body' => "Sample message using twilio SDK."
-//     )
-// );
+
 
 
 ?>
