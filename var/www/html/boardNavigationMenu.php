6ee630ea76f34672236362043de54ccd52e4f681
GeethChadalawada <geethchadalawada@gmail.com>
2018-03-03 22:45:00 +0530

    community logo

--- var/www/html/boardNavigationMenu.php
+++ var/www/html/boardNavigationMenu.php
@@ -4,6 +4,8 @@
 
   session_start();
 
+  include 'includes/globalvar.php';
+
 ?>
 
 <aside class="main-sidebar">
@@ -14,7 +16,7 @@
             
       <?php 
 
-        echo "<li class='header text-center'><img src='srsq_logo.JPG'></li>"; 
+        echo "<li class='header text-center'>$communtiy_logo</li>"; 
 
       ?>
             
